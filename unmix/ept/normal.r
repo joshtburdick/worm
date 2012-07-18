3aeb8d72be07506d40f870b04cbf5c7d856a5be8
Josh Burdick <jburdick@mail.med.upenn.edu>
2012-07-18 16:04:44 -0400

    removed n. Fixed one bug in converting between parameterizations.

--- unmix/ept/normal.r
+++ unmix/ept/normal.r
@@ -11,26 +11,19 @@ norm.var = function(n) {
   list(b = b, observed=FALSE)
 }
 
-# FIXME: these are not yet working
-
-# Converts from canonical parameterization to mean and s.d.
-canonical.to.mean.and.sd = function(a) {
-  p = -2 * (a[,"e2"] / a[,"n"])
-  cbind(n = rep(1, length(p)),
-    mu = a[,"e1"] / a[,"n"],
-    sd = sqrt(1/p))
+# Converts from mean and variance to canonical parameters.
+mean.and.variance.to.canonical = function(a) {
+  cbind(e1 = a[,"m"] / a[,"v"], e2 = -0.5 / a[,"v"])
 }
 
-# Converts from mean and s.d. to canonical parameters.
-mean.and.sd.to.canonical = function(a) {
-  p = 1 / (a[,"sd"]^2)
-  cbind(n = rep(1, length(p)),
-    e1 = a[,"n"] * (a[,"m"] / (a[,"sd"]^2)),
-    e2 = -0.5 * a[,"n"] * p)
+# Converts from canonical parameterization to mean and variance.
+canonical.to.mean.and.variance = function(a) {
+  v = -0.5 / a[,"e2"]
+  r = cbind(m = a[,"e1"] * v, v = v)
+  # to avoid NaN if variance is infinite, set mean to something arbitrary
+  r[ r[,"v"] == Inf, "m" ] = 0
+  r
 }
-
-
-# Factor constraining some variables to be linear transforms of each other.
 
 
 
