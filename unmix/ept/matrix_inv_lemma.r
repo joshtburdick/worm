5a3c470edd6c16c3f3061fd5b0c7c37d3d543b5d
Josh Burdick <jburdick@mail.med.upenn.edu>
2012-07-18 13:41:28 -0400

    minor formatting changes

--- unmix/ept/matrix_inv_lemma.r
+++ unmix/ept/matrix_inv_lemma.r
@@ -23,30 +23,25 @@ matrix.inv.diag.narrow = function(A.diag, X, B) {
   A.inv.diag - A.inv.diag * apply(X * t(solve(C, t(X))), 1, sum) * A.inv.diag
 }
 
-if (FALSE) {
 # Tests the above, by comparing with the naive function.
-# Args: A, X, and B, as for matrix.inv.diag.narrow()
-# Returns: mean-squared difference between the above
-#   function's output, and the same thing computed naively
-matrix.inv.diag.narrow.test = function(A.diag, X, B) {
-  r = matrix.inv.diag.narrow(A.diag, X, B)
+if (FALSE) {
 
-  r.naive = diag(chol2inv(chol( diag(A.diag) + X %*% B %*% t(X) )))
+  matrix.inv.diag.narrow.test = function(A.diag, X, B) {
+    r = matrix.inv.diag.narrow(A.diag, X, B)
+    r.naive = diag(chol2inv(chol( diag(A.diag) + X %*% B %*% t(X) )))
+    sum( (r - r.naive)^2 )
+  }
 
-  sum( (r - r.naive)^2 )
-}
+  set.seed(0)
+  t1 = list(A.diag = sample(10, 5),
+    X = matrix(runif(10), nrow=5, ncol=2),
+    B = matrix(c(2, -0.3, -0.3, 2), nrow=2))
 
-set.seed(0)
-t1 = list(A.diag = sample(10, 5),
-  X = matrix(runif(10), nrow=5, ncol=2),
-  B = matrix(c(2, -0.3, -0.3, 2), nrow=2))
+  t2 = list(A.diag = 0.1 + runif(1000),
+    X = matrix(runif(30000), nrow=1000, ncol=30),
+    B = { M = matrix(rnorm(900, sd=0.1), nrow=30); M + t(M) + diag( 1 + 5 * runif(30) ) } )
 
-t2 = list(A.diag = 0.1 + runif(1000),
-  X = matrix(runif(30000), nrow=1000, ncol=30),
-  B = { M = matrix(rnorm(900, sd=0.1), nrow=30); M + t(M) + diag( 1 + 5 * runif(30) ) } )
-
-cat(matrix.inv.diag.narrow.test(t1$A, t1$X, t1$B), "\n")
-cat(matrix.inv.diag.narrow.test(t2$A, t2$X, t2$B), "\n")
-
+  cat(matrix.inv.diag.narrow.test(t1$A, t1$X, t1$B), "\n")
+  cat(matrix.inv.diag.narrow.test(t2$A, t2$X, t2$B), "\n")
 }
 
