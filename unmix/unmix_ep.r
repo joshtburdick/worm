1f763f38fff64c4e9c1ddda2b7716980d30374e6
Josh Burdick <jburdick@mail.med.upenn.edu>
2012-07-20 18:06:55 -0400

    Works on toy examples. However, doesn't use faster "Ax~b" constraint, and hasn't been tested on actual unmixing.

--- unmix/unmix_ep.r
+++ unmix/unmix_ep.r
@@ -1,5 +1,7 @@
 # Unmixes using EP.
 
+library(Matrix)
+
 source("git/unmix/ept/normal.r")
 # source("git/unmix/ept/matrix_inv_lemma.r")   XXX um, don't think I need this
 
@@ -9,63 +11,93 @@ source("git/unmix/ept/normal.r")
 # "Moments of Truncated (Normal) Distributions".
 norm.moment.match = function(m, v) {
   m1 = -as.vector(m)
-v[v<0] = 1e8   # XXX hack
+# v[v<0] = 1e8   # XXX hack
   s = sqrt(v)
   z = -m1 / s
   a = dnorm(z) / pnorm(z)
 
   # hack to deal with when z is very negative
-  r = cbind(m = ifelse(z < -30, 0, - (m1 - s * a)),
-    v = ifelse(z < -30, 0, v * (1 - z*a - a^2)))
+#  r = cbind(m = ifelse(z < -30, 0, - (m1 - s * a)),
+#    v = ifelse(z < -30, 0, v * (1 - z*a - a^2)))
+  r = cbind(m = - (m1 - s * a), v = v * (1 - z*a - a^2))
   r
 }
 
 # Messages constraining a variable to be positive.
 positive.factor = function(x) {
   mv = canonical.to.mean.and.variance(x)
-  mm = norm.moment.match(mv[,"m"], m.and.v[,"v"])
+  mm = norm.moment.match(mv[,"m"], mv[,"v"])
   mean.and.variance.to.canonical(mm)
 }
 
-# Distribution of x ~ N(m,v) | Ax ~ N(b,b.var).
-lin.constraint = function(m, v, A, b, b.var) {
+# Original, slower, version of this.
+mvnorm.2.diag = function(m, v, A, b, b.var) {
+  V = Diagonal( x = v )
 
+  # first, compute (A V A^T + v I) ^ -1, which I'll call B
+  M = as.matrix( A %*% V %*% t(A) + Diagonal(x = b.var) )
+  B = pseudoinverse(M)
+
+  # ??? can we avoid computing the whole covariance here?
+  cbind(m = m - as.vector(V %*% t(A) %*% B %*% (A %*% m - b)),
+    v = as.vector(diag(V - V %*% t(A) %*% B %*% A %*% V)))
+}
+
+# Distribution of x ~ N(m,v) | Ax ~ N(b,b.var).
+lin.constraint.1 = function(m, v, A, b, b.var) {
+print("m =")
+print(m)
+print("v =")
+print(v)
   # first, compute (A V A^T + diag(v)) ^ -1, which I'll call B
   M = as.matrix( A %*% (v * t(A)) + Diagonal(x = b.var) )
   M.chol = chol(M)
-print(dim(M))
-print(dim(A))
-  cbind(m = m - v * t(A) %*% solve(M.chol, A %*% m - b),
+
+print("M =")
+print(M)
+  r = cbind(m = m - v * as.vector(t(A) %*% solve(M.chol, A %*% m - b)),
     v = v - v * apply(A * solve(M.chol, A), 2, sum) * v)
+print(colnames(r))
+  r
 }
 
 # Message constraining "Ax ~ N(b, b.var)".
 lin.constraint.factor = function(A, b, b.var) function(x) {
   mv = canonical.to.mean.and.variance(x)
-  r = lin.constraint(mv[,"m"], mv[,"v"], A, b, b.var)
+  r = mvnorm.2.diag(mv[,"m"], mv[,"v"], A, b, b.var)
+#  r = lin.constraint.1(mv[,"m"], mv[,"v"], A, b, b.var)
+print("r =")
+print(r)
+print(colnames(r))
   mean.and.variance.to.canonical(r)
 }
 
 # Does unmixing when the constraints are
 #   Ax ~ Normal(-,-), x >= 0.
-# (Assumes a flat prior.)
-unmix.ep = function(A, b, b.var) {
-  n = length(b)
+unmix.ep = function(A, b, b.var, prior.var=Inf) {
+  n = ncol(A)
 
-  # the term approximations (initially arbtrary, with nonzero variance)
-  terms = mean.and.variance.to.canonical(cbind(m=rep(0,n), v=rep(1,n)))
+  # prior
+  prior = mean.and.variance.to.canonical(cbind(m=rep(0,n), v=rep(prior.var,n)))
+
+  # the term approximations (initially flat)
+  terms = mean.and.variance.to.canonical(cbind(m=rep(0,n), v=rep(Inf,n)))
+
+  # the (soft) linear constraint
+  lin.constraint = lin.constraint.factor(A, b, b.var)
 
   # the posterior
-  q = terms
+  q = mean.and.variance.to.canonical(cbind(m=rep(0,n), v=rep(1,n)))
 
-  for(iter in 1:5) {
+  for(iter in 1:100) {
+    terms.1 = q - terms
+    mm = positive.factor(terms.1)
 
-
+    # update terms
+    terms = terms + (mm - q)
 
     # add in Ax ~ N(-,-) constraint
-
-
-
+    q = lin.constraint( prior + terms )
   }
 
   mv = canonical.to.mean.and.variance(q)
