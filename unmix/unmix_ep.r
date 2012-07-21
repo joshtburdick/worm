eb403267ca5d076e67570f9d67ddcb952e600355
Josh Burdick <jburdick@mail.med.upenn.edu>
2012-07-21 11:16:52 -0400

    moved EP region-approximating to a new file; sketched main EP routine.

--- unmix/unmix_ep.r
+++ unmix/unmix_ep.r
@@ -1,115 +1,27 @@
 # Unmixes using EP.
 
-library(Matrix)
-library(corpcor)
 
-source("git/unmix/ept/normal.r")
-# source("git/unmix/ept/matrix_inv_lemma.r")   XXX um, don't think I need this
 
-# Moment-matches a normal, truncated at x >= 0,
-# with mean and variance m and v, respectively.
-# following Phoebus Dhrymes'
-# "Moments of Truncated (Normal) Distributions".
-positive.moment.match = function(m, v) {
-  m1 = -as.vector(m)
-# v[v<0] = 1e8   # XXX hack
-  s = sqrt(v)
-  z = -m1 / s
-  a = dnorm(z) / pnorm(z)
 
-  # hack to deal with when z is very negative
-#  r = cbind(m = ifelse(z < -30, 0, - (m1 - s * a)),
-#    v = ifelse(z < -30, 0, v * (1 - z*a - a^2)))
-  r = cbind(m = - (m1 - s * a), v = v * (1 - z*a - a^2))
-  r
-}
 
-# Messages constraining a variable to be positive
-# (but using canonical parameterization.)
-positive.moment.match.canonical = function(x) {
-  mv = canonical.to.mean.and.variance(x)
-  mm = positive.moment.match(mv[,"m"], mv[,"v"])
-  mean.and.variance.to.canonical(mm)
-}
 
-# Original, slower, version of this.
-mvnorm.2.diag = function(m, v, A, b, b.var) {
-  V = Diagonal( x = v )
-
-  # first, compute (A V A^T + v I) ^ -1, which I'll call B
-  M = as.matrix( A %*% V %*% t(A) + Diagonal(x = b.var) )
-  B = pseudoinverse(M)
-
-  # ??? can we avoid computing the whole covariance here?
-  cbind(m = m - as.vector(V %*% t(A) %*% B %*% (A %*% m - b)),
-    v = as.vector(diag(V - V %*% t(A) %*% B %*% A %*% V)))
-}
-
-# Distribution of x ~ N(m,v) | Ax ~ N(b,b.var).
-lin.constraint.1 = function(m, v, A, b, b.var) {
-print("m =")
-print(m)
-print("v =")
-print(v)
-  # first, compute (A V A^T + diag(v)) ^ -1, which I'll call B
-  M = as.matrix( A %*% (v * t(A)) + Diagonal(x = b.var) )
-  M.chol = chol(M)
-
-print("M =")
-print(M)
-  r = cbind(m = m - v * as.vector(t(A) %*% solve(M.chol, A %*% m - b)),
-    v = v - v * apply(A * solve(M.chol, A), 2, sum) * v)
-print(colnames(r))
-  r
-}
-
-# Message constraining "Ax ~ N(b, b.var)".
-lin.constraint.factor = function(A, b, b.var) function(x) {
-  mv = canonical.to.mean.and.variance(x)
-  r = mvnorm.2.diag(mv[,"m"], mv[,"v"], A, b, b.var)
-#  r = lin.constraint.1(mv[,"m"], mv[,"v"], A, b, b.var)
-print("r =")
-print(r)
-print(colnames(r))
-  mean.and.variance.to.canonical(r)
-}
-
-# Does unmixing with a soft linear constraint.
+# Unmixes using EP.
 # Args:
-#   A, b, b.var - these give the constraint that
-#     Ax ~ Normal(b, b.var), x >= 0
-#     (For exact constraints, b.var can be 0.)
-#   prior.var - variance for the prior (can be Inf)
-# Returns: list with components
-#   m, v - the mean and variance of the posterior
-#   update.stats - matrix with mean and variance of update sizes
-unmix.ep = function(A, b, b.var, prior.var=Inf) {
-  n = ncol(A)
+#   m - the cell-sorting matrix
+#   x, x.var - mean and variance of the expression in each fraction,
+#     as matrices with one row per gene, one column per fraction
+#   output.dir - directory in which to write output
+# Side effects: writes one .Rdata file per gene, each containing:
+#   m, v - mean and variance of the posterior
+#   x, x.var - the per-fraction mean and variance that were used
+#   terms - the final term approximations
+#   update.stats - how much the term approximations changed at each step
+unmix.ep = function(m, x, x.var, output.dir) {
 
-  # prior (for now, restricted to be diagonal)
-  prior = mean.and.variance.to.canonical(cbind(m=rep(0,n), v=rep(prior.var,n)))
 
-  # the term approximations (initially flat)
-  terms = mean.and.variance.to.canonical(cbind(m=rep(0,n), v=rep(Inf,n)))
 
-  # the (soft) linear constraint
-  lin.constraint = lin.constraint.factor(A, b, b.var)
 
-  # the posterior
-  q = mean.and.variance.to.canonical(cbind(m=rep(0,n), v=rep(1,n)))
 
-  for(iter in 1:100) {
-    terms.1 = q - terms
-    mm = positive.moment.match.canonical(terms.1)
 
-    # update terms
-    terms = (mm - q) + terms
-
-    # add in Ax ~ N(-,-) constraint
-    q = lin.constraint( prior + terms )
-  }
-
-  mv = canonical.to.mean.and.variance(q)
-  list(m = mv[,"m"], v = mv[,"v"], update.stats = NULL)
 }
 
