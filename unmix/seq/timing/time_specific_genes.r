89d2304d444bb48c59854cdcde3884ee0b368e62
Josh Burdick <jburdick@mail.med.upenn.edu>
2012-07-26 13:50:49 -0400

    Added comparison between timeseries and our data.

--- unmix/seq/timing/time_specific_genes.r
+++ unmix/seq/timing/time_specific_genes.r
@@ -19,6 +19,7 @@ plot.pair = function(i, j) {
     log2(1+embryo.timeseries[,as.character(j)]),
     xlab=paste("log2(1+coverage at time", i, ")"),
     ylab=paste("log2(1+coverage at time", j, ")"))
+  abline(0,1, col="darkgrey")
 }
 
 # plots distribution of log-ratio of each pair of genes.
@@ -49,20 +50,50 @@ plot.pair(690,720)
 dev.off()
 
 # estimate mean and sd of when each gene is on
-w = log.ets / apply(log.ets, 2, sum)
+x1 = embryo.timeseries[ apply(embryo.timeseries, 1, mean) >= 10 , ]
+w = x1 / apply(x1, 1, sum)
+w[ is.na(w) ] = 0
+time.dist = {
+  t.mean = apply( t(w) * time.points, 2, sum)
+  t.s2 = apply( t(w) * (time.points^2), 2, sum)
+  t.sd = sqrt( t.s2 - (t.mean^2) )
+  cbind(mean = t.mean, sd = t.sd)
+}
 
-t.mean = apply( t(w) * time.points, 2, sum)
-t.s2 = apply( t(w) * (time.points^2), 2, sum)
-t.sd = sqrt( t.s2 - (t.mean^2) )
 
 # plot these
+pdf("git/unmix/seq/timing/timeMeanAndSD.pdf", width=6, height=6)
+smoothScatter(time.dist[,"mean"], time.dist[,"sd"],
+  main="Time when genes are expressed",
+  xlab="mean of time expressed", ylab="s.d. of time expressed")
+print(quantile(time.dist[,"sd"], 0.2))
+abline(h=quantile(time.dist[,"sd"], 0.2), col="#303030")
+dev.off()
 
+# Writes out all genes sorted by time. FIXME: use the above t.s2 and t.sd?
+write.sorted.by.time = function() {
+  #sorted.by.time = log.ets[ order(cor(t(log.ets), 1:23)) , ]
+  #write.table(round(sorted.by.time, 1), file="git/unmix/seq/timing/expr.by.time.tsv", sep="\t",
+  #  row.names=TRUE, col.names=NA)
+}
 
-#sorted.by.time = log.ets[ order(cor(t(log.ets), 1:23)) , ]
+# Plots expression of those genes.
+r = as.matrix(read.table(gzfile("git/unmix/seq/quant/readsPerMillion.tsv.gz"),
+  sep="\t", check.names=FALSE, header=TRUE, row.names=1, as.is=TRUE))
 
-#write.table(round(sorted.by.time, 1), file="git/unmix/seq/timing/expr.by.time.tsv", sep="\t",
-#  row.names=TRUE, col.names=NA)
+td1 = time.dist[ time.dist[,"sd"] <= quantile(time.dist[,"sd"], 0.2) , ]
+td1 = td1[ order(td1[,"mean"], decreasing=FALSE) , ]
 
-
+plot.time.profile = function(g, main) {
+  plot(td1[,"mean"], log2(1 + r[ rownames(td1), g ]),
+    main = main, xlab = "time", ylab="log2(1 + coverage)",
+    pch=20, cex=0.5)
+}
+pdf("git/unmix/seq/timing/perStageMarkersInSorted.pdf", width=9, height=8)
+par(mfrow=c(3,1))
+plot.time.profile("all", "singlets")
+plot.time.profile("pha-4", "pha-4")
+plot.time.profile("ceh-26", "ceh-26")
+dev.off()
 
 
