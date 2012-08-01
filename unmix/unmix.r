f6d53deafece261c6916107a9a02045c895c4d85
Josh Burdick <jburdick@mail.med.upenn.edu>
2012-08-01 18:36:26 -0400

    Soon, this should mostly aggregate the data for unmixing.

--- unmix/unmix.r
+++ unmix/unmix.r
@@ -1,12 +1,13 @@
-# Unmixing using the pseudoinverse.
+# Gets all the data sets for unmixing.
 
 library(corpcor)
 library(limSolve)
 
 load("git/unmix/image/sort_matrix.Rdata")
 
-# the read depth, with and without correction for sorting purity
+# the expression data as read depth, with correction for sorting purity
 source("git/unmix/seq/sortPurityCorrection.r")
+names(r.corrected) = c("m", "v")
 
 # limit sort matrix to only include things up to a certain time
 t.cutoff = 420
@@ -18,13 +19,47 @@ cell.time.on.off =
 # XXX note that this is not in lineage order
 cells.to.include =
   rownames(cell.time.on.off)[ cell.time.on.off$on < t.cutoff ]
-M = sort.matrix[ , cells.to.include ]
+M = sort.matrix[ colnames(r.corrected$m), cells.to.include ]
 
 # scale rows of this to add up to 1
 M = M / apply(M, 1, sum)
 
 # limit to cases in which we have measurements
-M = M[ colnames(r.corrected$r.mean) , ]
+M = M[ colnames(r.corrected$m) , ]
+
+# sorting matrix including temporal measurements
+load("git/unmix/image/time_sort_matrix.Rdata")
+M.t = time.sort.matrix[ , cells.to.include ]
+M.t = M.t / apply(M.t, 1, sum)
+
+# the sort matrix including the FACS data and the timeseries
+M.facs.and.ts = rbind(M, M.t)
+
+# the timeseries of embryonic expression data
+embryo.timeseries = as.matrix(
+  read.table("git/unmix/seq/timing/embryo.timeseries.tsv.gz",
+    sep="\t", header=TRUE, row.names=1, as.is=TRUE))
+time.points = paste("t.", c(0,30,60,90,120,140,180,240,270,300,330,360,390,
+  420,450,480,540,570,600,630,660,690,720), sep="")
+colnames(embryo.timeseries) = time.points
+
+# normalize to "ppm"
+embryo.timeseries =
+  t( t(embryo.timeseries) / (apply(embryo.timeseries,2,sum) / 1e6) )
+g1 = intersect(rownames(embryo.timeseries), rownames(r.corrected$m))
+
+# again, we assume that the variance in this is about the same as
+# expression on the ppm scale
+r.ts = list(m = embryo.timeseries[ g1, rownames(time.sort.matrix) ],
+  v = embryo.timeseries[g1 , rownames(time.sort.matrix) ])
+
+# get list of genes to unmix
+load("R/unmix/comp_paper/expr.cell.Rdata")
+enriched.fraction =
+  read.table("R/unmix/sort_paper/unmix/fraction/enriched.fraction.tsv",
+  sep="\t", header=TRUE, row.names=1, check.names=FALSE, as.is=TRUE)
+gene.list = c(rownames(expr.cell), rownames(enriched.fraction))
+gene.list = unique(intersect(gene.list, rownames(r.corrected$m)))
 
 # Removes cells in any fractions having zero expression.
 # Args:
@@ -42,13 +77,18 @@ remove.zeros = function(M, b, b.var) {
     nz = !z)
 }
 
+
+
+
 # Unmixes using the constraints that x >= 0.
 # Args:
 #   M - the cell-sorting matrix
 #   b, b.var - the mean and variance of the expression in each fraction,
 #     as matrices with one row per gene, and one column per fraction
+#   prior.var - variance of prior, as a constant times the maximum variance
+#     in any fraction (without this, result is spiky)
 # Returns: the estimated expression in each cell
-unmix.lsei = function(M, b, b.var) {
+unmix.lsei = function(M, b, b.var, prior.var = 1e3) {
   x = matrix(0, nrow = nrow(b), ncol=ncol(M))
   rownames(x) = rownames(b)
   colnames(x) = colnames(M)
@@ -59,9 +99,18 @@ unmix.lsei = function(M, b, b.var) {
       # remove zeros implied whenever b == 0
       a = remove.zeros(M, b[g,], b.var[g,])
 
+      p = prior.var * max(a$b.var, na.rm=TRUE)
+
+      # the original problem
+      A = a$M / sqrt(a$b.var)
+      B = a$b / sqrt(a$b.var)  # == 1
+      # tack on the prior
+      A = rbind(A, diag(ncol(A)) / sqrt(p))
+      B = c(B, rep(0, ncol(A)))
+
       # estimate just the non-zero cells
-      r = lsei(A = a$M / sqrt(a$b.var), B = a$b / sqrt(a$b.var),
-        G = Diagonal(sum(a$nz)), H = rep(0, sum(a$nz)), type=2)
+      r = lsei(A = A, B = B,
+        G = diag(ncol(A)), H = rep(0, ncol(A)), type=2)
 
       # only write the cells in non-zero fractions
       x[ g, a$nz ] = r$X
@@ -72,7 +121,7 @@ unmix.lsei = function(M, b, b.var) {
 }
 
 x.pseudoinverse = { 
-  x = r.corrected$r.mean %*% pseudoinverse(t(M))
+  x = r.corrected$m %*% pseudoinverse(t(M))
 
   # scaling to get "average read depth / cell"
 #  x = 1341 * t( t(x) / as.vector(M["all",]) )
@@ -96,19 +145,6 @@ test1 = function() {
   }
   r
 }
-
-# FIXME: move this elsewhere?
-embryo.timeseries = read.table("git/unmix/seq/timing/embryo.timeseries.tsv.gz",
-  sep="\t", header=TRUE, row.names=1, as.is=TRUE)
-time.points = c(0,30,60,90,120,140,180,240,270,300,330,360,390,
-  420,450,480,540,570,600,630,660,690,720)
-# colnames(embryo.timeseries) = time.points
-embryo.timeseries = embryo.timeseries[ rownames(r) , ]
-
-# normalize to "ppm"
-embryo.timeseries =
-  t( t(embryo.timeseries) / (apply(embryo.timeseries,2,sum) / 1e6) )
-g1 = intersect(rownames(embryo.timeseries), rownames(r.corrected$r.mean))
 
 pseudoinverse.with.time = function() {
   load("git/unmix/image/time_sort_matrix.Rdata")
