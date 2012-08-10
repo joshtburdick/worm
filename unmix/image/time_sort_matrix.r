2c3bcbe457ba1f0c60b18e8ec23dee7c9c1b0b62
Josh Burdick <jburdick@mail.med.upenn.edu>
2012-08-10 13:49:37 -0400

    now includes unweighted version.

--- unmix/image/time_sort_matrix.r
+++ unmix/image/time_sort_matrix.r
@@ -22,8 +22,8 @@ time.sort.matrix.row = function(cell.weights, a, b) {
     (t2 - t1) / (cell.weights$off - cell.weights$on),
     0)
 
-  # the weights of cells (weighted by overlap)
-  w.time.weighted = cell.weights$w * t.overlap
+  # the weights of cells (by overlap, not volume)
+  w.time.weighted = t.overlap    # cell.weights$w * t.overlap
   names(w.time.weighted) = rownames(cell.weights)
 
   r = w.time.weighted[lin.node.names]
@@ -32,12 +32,15 @@ time.sort.matrix.row = function(cell.weights, a, b) {
   r
 }
 
-time.sort.matrix = NULL
+time.sort.matrix.unweighted = NULL
 for(i in c(2:18)) {
-  time.sort.matrix = rbind(time.sort.matrix,
+  time.sort.matrix.unweighted = rbind(time.sort.matrix.unweighted,
     time.sort.matrix.row(cell.weights, time.points[i-1], time.points[i]))
 }
 rownames(time.sort.matrix) = paste("t.", time.points[2:18], sep="")
 
-save(time.sort.matrix, file="git/unmix/image/time_sort_matrix.Rdata")
+time.sort.matrix = t( t(time.sort.matrix.unweighted) * cell.weights[lin.node.names,"w"] )
+time.sort.matrix[ is.na(time.sort.matrix) ] = 0
+
+save(time.sort.matrix, time.sort.matrix.unweighted, file="git/unmix/image/time_sort_matrix.Rdata")
 
