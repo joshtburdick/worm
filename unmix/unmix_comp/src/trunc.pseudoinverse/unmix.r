c9610eb7747676782c7ffcf866fb4b51e0ee3ab1
Josh Burdick <jburdick@mail.med.upenn.edu>
2012-09-17 17:31:12 -0400

    not tested yet.

---
+++ unmix/unmix_comp/src/trunc.pseudoinverse/unmix.r
@@ -0,0 +1,30 @@
+# Does unmixing using lsei().
+
+source("unmix_test.r")
+
+# Does unmixing of one gene.
+# Args:
+#   si - the covariance matrix
+# Returns: function which, given a sort matrix and
+#   expression in each fraction, returns unmixed expression.
+unmix.lsei = function(si) {
+  A = chol(chol2inv(si))
+
+  function(m, x.fraction) {
+    num.cells = dim(m)[2]
+
+    # constraints to force everything to be positive
+    G = diag(num.cells)
+    H = rep(0, num.cells)
+
+    r = lsei(A=A, B=rep(0, num.cells), E=m, F=x.fraction, G=G, H=H, type=1)
+    x = as.vector(r$X)
+    names(x) = colnames(m)
+    x
+  }
+}
+
+
+run.unmix.all.data(unmix.lsei(diag(ncol(expr.cell))),
+  "trunc.pseudoinverse/")
+
