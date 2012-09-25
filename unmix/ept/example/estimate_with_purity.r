2a17b8b8181382d060737940af8507c4de1ab224
Josh Burdick <jburdick@mail.med.upenn.edu>
2012-09-25 14:33:06 -0400

    sketched this with a toy example.

--- unmix/ept/example/estimate_with_purity.r
+++ unmix/ept/example/estimate_with_purity.r
@@ -1,27 +1,37 @@
-
-library(corpcor)
+# Attempt at a particular case of unmixing.
+# Efficiency isn't a priority.
 
 source("git/unmix/ept/dirichlet.r")
 
+num.frac = 2
+num.cells = 5
+
+# a sample model; completely a toy example
+a = list(
+
+  # the measurements
+  f = matrix(0, nrow=2, ncol=num.frac),
+
+  # the known mixing proportions
+  m = matrix(0, nrow=num.frac, ncol=num.cells),
+
+  # the expression estimates
+  x = matrix(0, nrow=2, ncol=num.cells)
+)
+
+a$f = a$f + 1
+a$f[1,1] = 10
+a$f[1,2] = 5
+
+a$m[1,1:5] = 1
+a$m[2,1:2] = 1
+
+# add in messages from all factors
+a$m.from = 0 * a$x
 
 
 
 
-
-
-
-# Constrains one variable to be a linear function of another.
-lin.factor = function(A, b) {
-  Ap = pseudoinverse(A)
-  function(m) {
-    x1 = canonical.to.mean.and.variance(m$x)
-    y1 = canonical.to.mean.and.variance(m$y)
-    m.y = cbind(m = A %*% x1[,"m"] + b, v = t(A) %*% diag(x1[,"v"]) %*% A)
-    m.x = cbind(m = Ap %*% (y1[,"m"] - b), v = t(Ap) %*% diag(y1[,"v"]) %*% Ap)
-    list(m = list(x = mean.and.variance.to.canonical(m.x),
-      y = mean.and.variance.to.canonical(m.y)), le=0)
-  }
-}
 
 
 
