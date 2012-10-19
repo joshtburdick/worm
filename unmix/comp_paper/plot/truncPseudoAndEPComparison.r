a985a59b567380e86c64a3c8a57249506cc66d2f
Josh Burdick <jburdick@mail.med.upenn.edu>
2012-10-19 12:02:48 -0400

    added to this comparison.

--- unmix/comp_paper/plot/truncPseudoAndEPComparison.r
+++ unmix/comp_paper/plot/truncPseudoAndEPComparison.r
@@ -16,8 +16,8 @@ a = data.frame(
 
 plot.it = function() {
   pdf("git/unmix/comp_paper/plot/truncPseudoAndEPComparison.pdf",
-    width=10, height=5)
-  par(mfrow=c(1,2))
+    width=10, height=10)
+  par(mfrow=c(2,2))
 
   plot(a$tp.cor, a$ep.cor,
     main="Correlation",
@@ -33,6 +33,21 @@ plot.it = function() {
   g = rownames(a)[ abs(a$tp.auc - a$ep.auc) >= 0.1 ]
   text(a[g,"tp.auc"], a[g,"ep.auc"], labels=g, cex=0.65, pos=4, offset=0.2)
 
+  plot(a$tp.cor, a$tp.auc,
+    main="Constrained pseudoinverse",
+    xlab="Correlation", ylab="Area under curve",
+    pch=20, cex=0.5)
+  g = unique(c(rownames(a)[a$tp.cor < 0.7 | a$tp.auc < 0.85],
+    sample(rownames(a), 8)))
+  text(a[g,"tp.cor"], a[g,"tp.auc"], labels=g, cex=0.65, pos=4, offset=0.2)
+
+  plot(a$ep.cor, a$ep.auc,
+    main="Expectation propagation",
+    xlab="Correlation", ylab="Area under curve",
+    pch=20, cex=0.5)
+  g = unique(c(rownames(a)[a$ep.cor < 0.74 | a$ep.auc < 0.88],
+    sample(rownames(a), 8)))
+  text(a[g,"ep.cor"], a[g,"ep.auc"], labels=g, cex=0.65, pos=4, offset=0.2)
   dev.off()
 }
 
