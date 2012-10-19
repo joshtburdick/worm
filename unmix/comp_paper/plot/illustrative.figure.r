eeadff5c239e78e86f33387911060caee3e9f0f6
Josh Burdick <jburdick@mail.med.upenn.edu>
2012-10-19 12:02:17 -0400

    still hacking on this.

--- unmix/comp_paper/plot/illustrative.figure.r
+++ unmix/comp_paper/plot/illustrative.figure.r
@@ -1,12 +1,16 @@
 # Plots some actual expression patterns, and predictions
 # in a sub-lineage.
 
-# source("R/unmix/comp_paper/plot/load.unmixed.results.2.r")
+source("R/unmix/comp_paper/plot/load.unmixed.results.2.r")
 source("R/lineage/embryodb.r")
 source("git/plot/plot_expr.r")
 source("git/plot/smoothing.r")
 
-expr.prediction = unmix.r[["pseudo"]][["expr.cell"]][[30]][,lin.node.names]
+expr.prediction = unmix.r[["tp"]][["expr.cell"]][[30]][,lin.node.names]
+
+x.f = m.cell %*% t(expr.cell)
+x.f.pes1 = x.f[c("alr-1", "nhr-171", "tlp-1"), "pes-1"] / x.f["all", "pes-1"]
+x.f.pes1[4] = 0.08   # XXX not actually true, just a guess
 
 # Extends expression so that it's non-decreasing.
 # This also extends expression to child nodes.
@@ -29,8 +33,11 @@ extend.expression = function(cell, x) {
 # Args:
 #   expr - the expression pattern
 plot.expr.1 = function(expr, main, max.intensity, max.color) {
-  par(mar=c(2,4,1.8,2)+0.1)
+  par(mar=c(2,2,1.8,2)+0.1)
   expr[expr > max.intensity] = max.intensity
+
+  # XXX XXX hack
+#  expr[names(expr) %in% grep("^Ca", names(expr), value=TRUE)] = 0
 
   c1 = as.vector(col2rgb(max.color) / 255)
   r = rgb(scale.to.unit(expr) * c1[1],
@@ -39,6 +46,16 @@ plot.expr.1 = function(expr, main, max.intensity, max.color) {
   names(r) = names(expr)
   plot.segments.per.cell(r, main, root="C", lwd=3, yaxt="n")
   title(main)
+}
+
+# Draws barplot of fractions.
+fraction.barplot = function() {
+  mp = barplot(x.f.pes1,
+    col=hsv(c(0,0.1,1/3,0.69), 1,1), width=0.8,
+    ylim=c(0,0.7), xlab="FACS sorted fraction", xaxt="n",
+    yaxt="n", ylab="expression", main="pes-1 expression from RNA-seq")
+  axis(1, at=mp, tick=FALSE,
+    labels=c("alr-1", "nhr-171", "tlp-1", "lin-39"))
 }
 
 # Plots an image.
@@ -52,6 +69,10 @@ plot.image = function(series, main, max.intensity, hue) {
   expr = as.vector(scale.to.unit(expr))
 print(range(expr))
 
+  # XXX XXX hack
+  expr[scd$cell %in% grep("^Cpp", scd$cell, value=TRUE)] = 0
+
+
   r = data.frame(cell=scd$cell, time.1=scd$time, time.2=scd$time+1,
     col=hsv(hue,1,expr),
 #    col  par(mar=c(2,4,1.8,2)+0.1)=rgb(expr,0,0),
@@ -59,25 +80,24 @@ print(range(expr))
 print(r[1:5,])
 print(class(r$col))
 
-  par(mar=c(2,4,1.8,2)+0.1)
+  par(mar=c(2,2,1.8,2)+0.1)
   plot.segments(r, main, root="C", time=c(0,350), yaxt="n")
 }
 
 fig.1 = function() {
   pdf("git/unmix/comp_paper/plot/1. illustration of method.pdf",
-    width=8, height=8)
-  par(mfcol=c(4,2))
-  par(mar=c(2,4,1.8,2)+0.1)
+    width=5, height=10)
+  par(mfcol=c(4,1))
 
-  plot.image("20081016_alr-1_10A2_3_L2", "alr-1", 1200, 0)
-  plot.image("20100914_RW10594_nhr-171_L5", "nhr-171", 900, 0.1)
-  plot.image("20111003_tlp-1_L3", "tlp-1", 800, 1/3)
-  plot.image("20110906_RW10349_lin-39_L1", "lin-39", 500, 0.69)
+#  plot.image("20081016_alr-1_10A2_3_L2", "alr-1", 1500, 0)
+#  plot.image("20100914_RW10594_nhr-171_L5", "nhr-171", 1100, 0.1)
+#  plot.image("20111003_tlp-1_L3", "tlp-1", 1100, 1/3)
+#  plot.image("20110906_RW10349_lin-39_L1", "lin-39", 600, 0.69)
 
-  plot.image("20110615_pes-1_L4", "pes-1", 400, 0.75)
+  plot.image("20110615_pes-1_L4", "pes-1 expression from imaging", 20000, 0.75)
   plot.new()
-  plot.new()
-  plot.expr.1(expr.prediction["pes-1",], "pes-1 prediction", 1300,
+  fraction.barplot()
+  plot.expr.1(expr.prediction["pes-1",], "pes-1 prediction from unmixing", 3500,
     hsv(0.75,1,1))
 
   dev.off()
