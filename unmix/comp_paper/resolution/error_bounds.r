e3ee78a192adaa1d80e1ecbca4ce4f76b92937cb
Josh Burdick <jburdick@mail.med.upenn.edu>
2012-10-22 16:53:03 -0400

    slight color scale changes.

--- unmix/comp_paper/resolution/error_bounds.r
+++ unmix/comp_paper/resolution/error_bounds.r
@@ -3,16 +3,15 @@
 library("corpcor")
 
 source("R/unmix/comp_paper/plot/load.unmixed.results.2.r")
+source("git/plot/plot_expr.r")
 
+wd = getwd()
+setwd("~/gcb/git/unmix/unmix_comp/src/")
+source("unmix_test.r")
+source("pseudoinverse/unmix.r")
+setwd(wd)
 
-# expression, and predictions, normalized to sum to one for each gene
-# ??? rename this?
-rel.expr.cell = expr.cell / apply(expr.cell, 1, sum)
-rel.x.d = rel.expr.cell %*% t(m1) %*% pseudoinverse(t(m1))  # x.d / apply(x.d, 1, sum)
-rel.x.d.err = rel.x.d - rel.expr.cell
-
-
-# plot(as.vector(rel.x.d), abs(as.vector(rel.x.d.err)))
+m1 = m.cell[ c("all", reporters$picked[1:30]) , ]
 
 # FIXME: use a density plot?
 plot.it = function(a, b, a.name, b.name) {
@@ -52,42 +51,46 @@ if (s %% 50 == 0) cat(s, "")
 }
 
 # XXX shouldn't have hard-coded numbers
-# m.rotate = matrix.on.diagonal(1341)
+m.rotate = matrix.on.diagonal(1341)
 
 # Plots actual and simulated expression patterns, along with matrices
 # which may or may not be indicative of the structure of errors.
 plot.actual.predicted.and.resolution =
   function(x.actual, x.predicted, res.matrix, output.path) {
+system(paste("mkdir -p", output.path))
 
   library("grid")
 #  m.rotate = matrix.on.diagonal(dim(res.matrix)[1])
 
   for(i in 1:dim(x.actual)[1]) {
+    gene = rownames(x.actual)[i]
+
 cat(rownames(x.actual)[i], "")
     png(paste(output.path, "/", rownames(x.actual)[i], ".png", sep=""),
-      width=1500, height=1200)
+      width=1200, height=900)
     par(mfrow=c(4,1))
-
+    par(mar=c(3,2,2,1)+0.1)
     c1 = rgb(scale.to.unit(x.actual[i,]), 0, 0)
     names(c1) = colnames(x.actual)
-    plot.segments.per.cell(c1)
+    plot.segments.per.cell(c1, paste(gene, "measured expression"))
 
     c1 = rgb(scale.to.unit(x.predicted[i,]), 0, 0)
     names(c1) = colnames(x.actual)
-    plot.segments.per.cell(c1)
+    plot.segments.per.cell(c1, paste(gene, "prediction"))
 
     plot(0, 0, xlim=c(0,1), ylim=c(0,1), type="n",
-      xaxt="n", yaxt="n", xlab="", ylab="")
+      xaxt="n", yaxt="n", xlab="", ylab="",
+      main = "Resolution matrix, weighted by expression")
     x.p = x.predicted[i,]
     b = 1.5 * (x.p %o% x.p) * res.matrix
-    b = b / sd(as.vector(b))
+    b = b / (1 * sd(as.vector(b)))
     b[b<0] = 0
     b[b>1] = 1
     b = 1-b
     rasterImage(m.rotate(b), 0, 0, 1, 1, interpolate=FALSE)
 
     plot(0, 0, xlim=c(0,1), ylim=c(0,1), type="n",
-      xaxt="n", yaxt="n", xlab="", ylab="")
+      xaxt="n", yaxt="n", xlab="", ylab="", main="Resolution matrix")
     a = 0.5 * res.matrix / sd(as.vector(res.matrix))
     a[a<0] = 0
     a[a>1] = 1
@@ -98,15 +101,15 @@ cat(rownames(x.actual)[i], "")
   }
 }
 
-if (FALSE) {
+plot.it = function() {
   # XXX this should include row and column names!
-  expr.cell.d = 
-  rownames(expr.cell.d) = rownames(m)[-1]
-  colnames(expr.cell.d) = colnames(m)
-  expr.cell.d = expr.cell.d[rownames(expr.cell),]
+  expr.cell.d = unmix.r[["ep"]][["expr.cell"]][[30]]
+#  rownames(expr.cell.d) = rownames(m)[-1]
+#  colnames(expr.cell.d) = colnames(m)
+#  expr.cell.d = expr.cell.d[rownames(expr.cell),]
 
-
-  plot.actual.predicted.and.resolution(expr.cell, expr.cell.d, per.cell.res, "out/resolution")
+  plot.actual.predicted.and.resolution(expr.cell, expr.cell.d, per.cell.res, "git/unmix/comp_paper/resolution/expr_and_resolution_EP/")
 }
 
+plot.it()
 
