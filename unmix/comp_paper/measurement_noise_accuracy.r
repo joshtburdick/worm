afd573e85fc240051b3fe1fedf37005220f212b1
Josh Burdick <jburdick@mail.med.upenn.edu>
2012-10-24 11:24:28 -0400

    Now graphs this for naive pseudoinverse and EP.

--- unmix/comp_paper/measurement_noise_accuracy.r
+++ unmix/comp_paper/measurement_noise_accuracy.r
@@ -9,7 +9,7 @@ load("~/gcb/git/unmix/unmix_comp/data/tree_utils.Rdata")
 wd = getwd()
 setwd("~/gcb/git/unmix/unmix_comp/src/")
 source("unmix_test.r")
-# source("pseudoinverse/unmix.r")
+source("pseudoinverse/unmix.r")
 source("EP/unmix.r")
 setwd(wd)
 
@@ -55,50 +55,70 @@ run.unmix.perturbed.matrix = function(x, m, unmix.f, reporter.list, nr, noise.sd
 }
 
 # Computes accuracy for some noise level.
-compute.accuracy.with.noise = function(noise.sd) {
+compute.accuracy.with.noise = function(unmix.f, unmix.f.name)
+  function(noise.sd) {
 cat(noise.sd, "\n")
-  x.unmix = run.unmix.perturbed.matrix(expr.cell, m.cell, unmix.ep,
+  x.unmix = run.unmix.perturbed.matrix(expr.cell, m.cell, unmix.f,
     reporters$picked, 30, noise.sd)
 
-  c(noise.sd = noise.sd,
+  c(unmix.function = unmix.f.name, noise.sd = noise.sd,
     cor = mean(diag(cor(t(expr.cell), t(x.unmix))), na.rm=TRUE),
     auc = mean(auc(expr.cell >= 2000, x.unmix), na.rm=TRUE),
     num.non.na = sum(apply(!is.na(x.unmix), 1, sum) > 0))
 }
 
-accuracy.with.noise = NULL
-for(iter in 1:3)
-  accuracy.with.noise = rbind(accuracy.with.noise,
-    t( sapply(c(0:20)/20, compute.accuracy.with.noise)))
-
-write.table(accuracy.with.noise,
-  file="git/unmix/comp_paper/measurement_noise_accuracy.tsv",
-  col.names=NA, sep="\t")
+write.accuracy.table = function() {
+  accuracy.with.noise = NULL
+  for(iter in 1:3) {
+    accuracy.with.noise = rbind(accuracy.with.noise,
+      t( sapply(c(0:20)/20,
+        compute.accuracy.with.noise(unmix.pseudoinverse, "naive pseudoinverse"))))
+    accuracy.with.noise = rbind(accuracy.with.noise,
+      t( sapply(c(0:20)/20,
+        compute.accuracy.with.noise(unmix.ep, "EP"))))
+  }
+  write.table(accuracy.with.noise,
+    file="git/unmix/comp_paper/measurement_noise_accuracy.tsv",
+    col.names=NA, sep="\t")
+}
 
 plot.it = function() {
+  accuracy.with.noise = read.table(
+    "git/unmix/comp_paper/measurement_noise_accuracy.tsv",
+    header=TRUE, check.names=FALSE)
+
   pdf("git/unmix/comp_paper/accuracy with measurement noise.pdf",
     width=9, height=4)
   par(mfrow=c(1,2))
 
-  plot(accuracy.with.noise[,"noise.sd"], accuracy.with.noise[,"cor"],
-    ylim=c(0.5,1), pch=20, cex=0.75,
-    xlab="noise standard deviation", ylab="correlation",
-    main="Accuracy with measurement noise")
+  a.pseudo = accuracy.with.noise[
+    accuracy.with.noise$unmix.function == "naive pseudoinverse" , ]
+  a.ep = accuracy.with.noise[
+    accuracy.with.noise$unmix.function == "EP" , ]
 
-  plot(accuracy.with.noise[,"noise.sd"], accuracy.with.noise[,"auc"],
+  plot(a.ep[,"noise.sd"], a.ep[,"cor"],
     ylim=c(0.5,1), pch=20, cex=0.75,
-    xlab="noise standard deviation", ylab="area under the curve",
-    main="Accuracy with measurement noise")
+    xlab="Noise standard deviation", ylab="Correlation",
+    main="Accuracy with measurement noise", col="#00000080")
+  par(new=TRUE)
+  plot(a.pseudo[,"noise.sd"], a.pseudo[,"cor"],
+    ylim=c(0.5,1), pch=20, cex=0.75,
+    main="", xlab="", ylab="", col="#f0000080")
+  legend("bottomleft", legend = c("EP", "naive pseudoinverse"),
+    col=c("#00000080", "#f0000080"), seg.len=0, lwd=10)
+
+  plot(a.ep[,"noise.sd"], a.ep[,"auc"],
+    ylim=c(0.5,1), pch=20, cex=0.75,
+    xlab="Noise standard deviation", ylab="Area under the curve",
+    main="Accuracy with measurement noise", col="#00000080")
+  par(new=TRUE)
+  plot(a.pseudo[,"noise.sd"], a.pseudo[,"auc"],
+    ylim=c(0.5,1), pch=20, cex=0.75,
+    main="", xlab="", ylab="", col="#f0000080")
 
   dev.off()
 }
 
-#  write.table(missing.cells.separate.fractions, col.names=NA,
-#    file="git/unmix/missing/sim/missing_cells_separate_fractions.txt", sep="\t")
-
-# foo = run.unmix.perturbed.matrix(expr.cell, m.cell, unmix.pseudoinverse,
-#   reporters$picked, 30, 1)
-
-
+# write.accuracy.table()
 plot.it()
 
