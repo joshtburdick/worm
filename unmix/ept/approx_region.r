1dc6f9309856ce28b77136dcbd7050bea820f0dd
Josh Burdick <jburdick@mail.med.upenn.edu>
2012-12-19 10:08:19 -0500

    Added damped version of approx.region .

--- unmix/ept/approx_region.r
+++ unmix/ept/approx_region.r
@@ -6,7 +6,7 @@ library(corpcor)
 source("git/unmix/ept/normal.r")
 # source("git/unmix/ept/matrix_inv_lemma.r")   XXX um, don't think I need this
 
-backspace = cat(paste(rep("\b", 70), collapse=""))
+backspace = paste(rep("\b", 70), collapse="")
 
 # Moment-matches a normal, truncated at x >= 0,
 # with mean and variance m and v, respectively.
@@ -14,15 +14,15 @@ backspace = cat(paste(rep("\b", 70), collapse=""))
 # "Moments of Truncated (Normal) Distributions".
 positive.moment.match = function(m, v) {
   m1 = -as.vector(m)
-# v[v<0] = 1e10   # XXX hack
+  v[v<0] = 1e10    # XXX hack
   s = sqrt(v)
   z = -m1 / s
   a = dnorm(z) / pnorm(z)
 #  a = ifelse(z<= -30, -z, dnorm(z)/pnorm(z))
 
   # hack to deal with when z is very negative
-  r = cbind(m = ifelse(z < -30, 1e-4, - (m1 - s * a)),
-    v = ifelse(z < -30, 1e-10, v * (1 - z*a - a^2)))
+  r = cbind(m = ifelse(z < -30, 0, - (m1 - s * a)),
+    v = ifelse(z < -30, 0, v * (1 - z*a - a^2)))
 #  r = cbind(m = - (m1 - s * a), v = v * (1 - z*a - a^2))
   r
 }
@@ -184,17 +184,113 @@ if (FALSE) {
 }
     # ??? show change in mean and variance separately?
     diff = apply(abs(canonical.to.mean.and.variance(q) - canonical.to.mean.and.variance(q.old)), 2, max)
-cat(backspace, signif(diff, 2), " ")
+cat(backspace, damping, signif(diff, 2), " ")
     update.stats = rbind(update.stats, diff)
 
     # possibly stop early
     if (max(diff, na.rm=TRUE) <= converge.tolerance)
       break
   }
+cat(" \n")
 
   mv = canonical.to.mean.and.variance(q)
   list(m = mv[,"m"], v = mv[,"v"],
     t = canonical.to.mean.and.variance( prior + terms ),
     update.stats = update.stats)
 }
+
+# Also approximates a region constrained such that x >= 0,
+# but includes damping (which may help convergence.)
+# Args:
+#   A, b - these give the constraint that
+#     Ax = b, x >= 0
+#   converge.tolerance - determines when to stop
+#   max.iters - maximum number of iterations
+#   damping - initial damping factor (no damping = 1)
+#   damping.adjust - amount to multiply damping by,
+#     if an update leads to NaN / Inf
+# Returns: list with components
+#   m, v - the mean and variance of the posterior
+#   t - the prior times the terms (without the linear constraint)
+#   update.stats - matrix with mean and variance of update sizes
+approx.region.damping = function(A, b, converge.tolerance = 1e-9,
+    max.iters=100, damping=1, damping.adjust=0.5) {
+  n = ncol(A)
+
+  # the term approximations (initially flat)
+  terms = mean.and.variance.to.canonical(cbind(m=rep(0,n), v=rep(Inf,n)))
+
+  # the linear constraint (for now, this is exact)
+  lin.constraint = lin.constraint.factor(A, b, 0 * b)
+
+  # the posterior (??? initialize with pseudoinverse?)
+  q = mean.and.variance.to.canonical(cbind(m=rep(0,n), v=rep(1,n)))
+
+  # convergence statistics
+  update.stats = NULL
+
+  # definition of "numbers looking reasonably defined"
+  num.defined = function(x)
+    ((sum(is.na(x)) == 0) && (sum(is.nan(x)) == 0) && (sum(is.infinite(x)) == 0))
+
+  for(iter in 1:max.iters) {
+    error.flag = FALSE
+    update.diff = NULL
+
+    tryCatch({
+      q.old = q
+      terms.old = terms
+
+      terms.1 = q - terms
+      mm = positive.moment.match.canonical(terms.1)
+
+      # update terms, with damping. XXX not sure this is right.
+      terms.new = damping * (mm - q) + terms
+
+      # add in Ax ~ N(-,-) constraint
+      q.new = lin.constraint( terms.new )
+
+      # how much the posterior changed
+      update.diff <- apply(abs(canonical.to.mean.and.variance(q.new) -
+        canonical.to.mean.and.variance(q.old)), 2, max)
+
+    }, error = function(e) error.flag <<- TRUE)
+
+#    print(update.diff)
+#    print(class(update.diff))
+#    print(length(update.diff))
+#    cat("m:::::", update.diff["m"], update.stats[nrow(update.stats),"m"], "\n")
+#    cat("v=====", update.diff["v"], update.stats[nrow(update.stats),"v"], "\n")
+
+    # if that seemed to work, make the update
+    if ((!error.flag) && num.defined(terms.new) && num.defined(q.new) &&
+#        (min(canonical.to.mean.and.variance(q.new)[1,"m"]) >= 1e-6) &&
+        (!is.null(update.stats)) &&
+        (update.diff["m"] <= 2 * update.stats[nrow(update.stats),"m"]) &&
+        (update.diff["v"] <= 2 * update.stats[nrow(update.stats),"v"])) {
+      terms = terms.new
+      q = q.new
+    }
+    else {
+      if (iter >= 2)
+        damping = damping * damping.adjust
+    }
+
+ cat(backspace, damping, update.diff, backspace)   # signif(diff, 2)
+    update.stats = rbind(update.stats, c(damping=damping, update.diff))
+
+    # possibly stop early
+    if ((!error.flag) && (max(update.diff) <= converge.tolerance))
+      break
+  }
+cat(" \n")
+
+print(update.stats)
+
+  mv = canonical.to.mean.and.variance(q)
+  list(m = mv[,"m"], v = mv[,"v"],
+    t = canonical.to.mean.and.variance( terms ),
+    update.stats = update.stats)
+}
+
 
