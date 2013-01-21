58db9c2d57834dd1e6a4ab8604e0159078badb84
Josh Burdick <jburdick@mail.med.upenn.edu>
2013-01-21 16:46:47 -0500

    Now includes more samples.

--- unmix/seq/quant/get_coverage.pl
+++ unmix/seq/quant/get_coverage.pl
@@ -1,14 +1,33 @@
 #!/usr/bin/perl -w
 # Computes coverage for all BAM files.
 
-# confusingly, this is the "sense" strand quantification
-# compute_coverage("geneBounds_flip.tsv", "/murrlab/seq/tophat2/Murray050912/strand_flip", "Murray_050912");
-# compute_coverage("geneBounds_flip.tsv",
-#   "/murrlab/seq/tophat2/Murray_52831_092812/strand_flip",
-#   "Murray_52831_092812");
+# confusingly, "geneBounds_flip.tsv" is the "sense" strand quantification
+
+if (undef) {
+compute_coverage("geneBounds_flip.tsv",
+  "/murrlab/seq/tophat2/20110922/",
+  "20110922");
+compute_coverage("geneBounds.tsv",
+  "/murrlab/seq/tophat2/20110922/",
+  "20110922_as");
+}
+
+if (undef) {
+compute_coverage("geneBounds_flip.tsv",
+  "/murrlab/seq/tophat2/Murray050912/strand_flip",
+  "Murray_050912");
+compute_coverage("geneBounds.tsv",
+  "/murrlab/seq/tophat2/Murray050912/strand_flip",
+  "Murray_050912_as");
+}
+if (1) {
+compute_coverage("geneBounds_flip.tsv",
+  "/murrlab/seq/tophat2/Murray_52831_092812/strand_flip",
+  "Murray_52831_092812");
 compute_coverage("geneBounds.tsv",
   "/murrlab/seq/tophat2/Murray_52831_092812/strand_flip",
   "Murray_52831_092812_as");
+}
 
 sub compute_coverage {
   my($bed_file, $bam_dir, $out_dir) = @_;
@@ -23,6 +42,10 @@ sub compute_coverage {
     die if not $file =~ /\/?([^\/]+)\.bam/;
     my $sample_name = $1;
     my $output_file = "$out_dir/$sample_name.tsv.gz";
+
+    # XXX temporary hack
+print "$file\n";
+#    next if not (($file =~ "\/03_") || ($file =~ "\/04_"));
 
     print "[writing coverage for $file to $output_file]\n";
     #  system ("coverageBed -abam $file -b $bed_file > $output_file");
