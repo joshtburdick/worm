74b52b59653b0951939b7d035f0d7b40c3b6b234
Josh Burdick <jburdick@mail.med.upenn.edu>
2013-01-28 12:30:39 -0500

    WGCNA now seems to create clusters, and a dendrogram.

---
+++ unmix/seq/cluster/WGCNA/dendrograms.r
@@ -0,0 +1,18 @@
+# Plots dendrograms from WGCNA analysis.
+
+load("git/unmix/seq/cluster/WGCNA/wnet.Rdata")
+
+moduleColors = labels2colors(wnet$colors)
+
+pdf("git/unmix/seq/cluster/WGCNA/dendrograms.pdf",
+  title="WGCNA dendrograms", width=10, height=7.5)
+
+for(i in 1:length(wnet$dendrograms))
+plotDendroAndColors(wnet$dendrograms[[i]],
+  moduleColors[wnet$blockGenes[[i]] ],
+  "", main=paste("Gene dendrogram, block", i),
+  dendroLabels = FALSE, hang = 0.03,
+  addGuide = TRUE, guideHang = 0.05)
+
+dev.off()
+
---
+++ unmix/seq/cluster/WGCNA/wgcna.r
@@ -0,0 +1,25 @@
+# Clusters using the WGCNA methodology.
+
+library(WGCNA)
+# library(moduleColor)  # FIXME this should be a dependency
+
+options(stringsAsFactors = FALSE)
+
+read.counts = as.matrix(read.table("git/unmix/seq/quant/readsPerMillion_Murray_050912.tsv",
+  header=TRUE, row.names=1, check.names=FALSE, as.is=TRUE))
+
+r = t(log2(1 + read.counts))
+
+wnet = blockwiseModules(r, maxBlockSize = 5000,
+  power = 6, minModuleSize = 20,
+  reassignThreshold = 0, mergeCutHeight = 0.25,
+  numericLabels = TRUE,
+#  colors = hsv(1:255, 1,1),
+  saveTOMs = TRUE,
+  saveTOMFileBase = "git/unmix/seq/cluster/WGCNA/seq-blockwise",
+  verbose = 3)
+
+save(wnet, file="git/unmix/seq/cluster/WGCNA/wnet.Rdata")
+
+
+
