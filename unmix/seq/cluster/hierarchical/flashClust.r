4c4b6c0a51b39e85265ff400fa4c578d7aaf0662
Josh Burdick <jburdick@mail.med.upenn.edu>
2013-02-09 12:46:37 -0500

    Now reads "readsNormalized". Also uses hcluster.treeview().

--- unmix/seq/cluster/hierarchical/flashClust.r
+++ unmix/seq/cluster/hierarchical/flashClust.r
@@ -1,42 +1,39 @@
 
-library("flashClust")
+# library("flashClust")
 library("amap")
+library("ctc")
 
-# get data (FIXME should be in a separate function)
-count.path = "git/unmix/seq/quant/readsPerMillion"
-
-r1 = as.matrix(
-  read.table(paste(count.path, "readsPerMillion_Murray_050912.tsv", sep="/"),
-  header=TRUE, row.names=1, check.names=FALSE, as.is=TRUE))
-
-r2 = as.matrix(
-  read.table(paste(count.path, "readsPerMillion_092812.tsv", sep="/"),
-  header=TRUE, row.names=1, check.names=FALSE, as.is=TRUE))
-
-r = log2(1 + cbind(r1, r2))
-r = r[ , order(colnames(r)) ]
+r = read.table("git/unmix/seq/cluster/readsNormalized.tsv",
+  sep="\t", header=TRUE, row.names=1, check.names=FALSE, as.is=TRUE)
 
 # remove constant rows
 r = r[ apply(r, 1, var) > 0 , ]
 
-# if (FALSE) {
-print(date())
-  a = cor(t(r))
-print(date())
-  a = 1 - a
-  gc()
-print(date())
-  # XXX for whatever inane reason, this takes like eight minutes.
-  # not worrying about this for now.
-  d = as.dist(a)
-print(date())
-  a = NULL
-  gc()
-save(d, file="/var/tmp/exprCor.Rdata")
-print(date())
-# }
+# r = r[1:500,]     # XXX
 
-# h = hclust(d, method="complete")
-# print(date())
-save(h, file="/var/tmp/exprClustCorComplete.Rdata")
+# Does hierarchical clustering.
+# Args:
+#   x - data matrix
+#   base.filename - base filename
+#   method - which distance metric
+#   link - which clustering method
+# Modified from ctc::hclust2treeview() .
+# Side effects: writes .atr, .gtr, and .cdt files.
+# Returns: row and column clustering
+hcluster.treeview = function(x, basefile, method = "correlation", link = "complete") {
+  nbproc = 7
+
+  hr <- hcluster(x, method = method, link = link, nbproc = nbproc)
+  hc <- hcluster(t(x), method = method, link = link, nbproc = nbproc)
+  r2atr(hc, file = paste(basefile, ".atr", sep = ""))
+  r2gtr(hr, file = paste(basefile, ".gtr", sep = ""))
+  r2cdt(hr, hc, x, file = paste(basefile, ".cdt", sep = ""))
+
+  list(hr=hr, hc=hc)
+}
+
+cluster1 = hcluster.treeview(r,
+  "git/unmix/seq/cluster/hierarchical/cluster1",
+  method="correlation", link="complete")
+save(cluster1, file="git/unmix/seq/cluster/hierarchical/cluster1.Rdata")
 
