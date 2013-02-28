bada84bd08d588240eaab846a98813b36a7391b8
Josh Burdick <jburdick@mail.med.upenn.edu>
2013-02-28 10:15:14 -0500

    Now includes fractions separated out.

--- unmix/seq/cluster/enriched_in_fraction.r
+++ unmix/seq/cluster/enriched_in_fraction.r
@@ -2,18 +2,11 @@
 
 output.dir = "git/unmix/seq/cluster/"
 
-r = as.matrix(read.table(gzfile(
-    "git/unmix/seq/quant/readsPerMillion_pooled.tsv.gz"),
+r = as.matrix(read.table(
+    "git/unmix/seq/cluster/readsNormalized.tsv",
   header=TRUE, row.names=1, check.names=FALSE, as.is=TRUE))
 
-lr = log2(1 + r)
-le = lr - lr[,"all"]
-
-# tack on cases with negatives
-genes.with.neg = c("ceh-36", "cnd-1", "pha-4")
-le.neg = le[,genes.with.neg] - le[,paste(genes.with.neg, "_minus", sep="")]
-colnames(le.neg) = paste(genes.with.neg, "_rel_to_neg", sep="")
-le = cbind(le, le.neg)
+le = r
 
 enriched.and.depleted =
   data.frame(enriched = apply(le>=2, 2, sum),
@@ -21,17 +14,15 @@ enriched.and.depleted =
     stringsAsFactors=FALSE)
 
 write.table(enriched.and.depleted, sep="\t",
-  file=paste(output.dir, "/enriched.and.depleted.tsv", sep=""))
+  file=paste(output.dir, "/enriched.and.depleted_.tsv", sep=""))
 
 # write directory of files of which genes are enriched in each fraction
-system(paste("mkdir -p ", output.dir, "/enriched_genes/", sep=""))
+system(paste("mkdir -p ", output.dir, "/enriched_genes_2/", sep=""))
 for(g in colnames(le)) {
   enriched = rownames(le)[ le[,g] >= 2 ]
+  enriched = gsub(" ", "_", enriched)
+  enriched = gsub("/", "_", enriched)
   write.table(enriched, quote=FALSE, row.names=FALSE, col.names=FALSE,
     file=paste(output.dir, "/enriched_genes/", g, ".tsv", sep=""))
 }
-
-
-
-
 
