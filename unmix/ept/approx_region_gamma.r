f8ae6f9ede4a0d1d59e1d1b4d596b75a4552260e
Josh Burdick <jburdick@mail.med.upenn.edu>
2013-04-06 15:11:04 -0400

    hacked approx.region.gamma so that it, once again, sort-of-kind-of works; am wondering if what it's returning (q) is in fact the term approximation, and adding in the linear constraint would give a reasonable answer. It's also possible that I've just re-implemented essentially the Gaussian version, though...

--- unmix/ept/approx_region_gamma.r
+++ unmix/ept/approx_region_gamma.r
@@ -20,61 +20,64 @@ trunc.gamma.moment = function(m, x, a, b)
 #   x - prediction mean and variance
 approx.region.gamma = function(A, b, max.iters=100) {
 
-  # the posterior (initially something non-flat.)
-  # FIXME start with truncated pseudoinverse?
+  # the prior (initially something non-flat.)
   x1 = rbind(m = rep(1, ncol(A)), v = rep(1, ncol(A)))
-  x = gamma.mv2n(x1)
-  colnames(x) = colnames(A)
+  prior = gamma.mv2n(x1)
+  colnames(prior) = colnames(A)
+prior = prior / 1
 
   # the approximating terms
-  m = array(0, dim=c(2, ncol(A), nrow(A)),
+  term = array(0, dim=c(2, ncol(A), nrow(A)),
     dimnames = list(c("e1", "e2"), colnames(A), rownames(A)))
-  m["e1",,] = 0
-  m["e2",,] = -1
+  term["e1",,] = 0
+  term["e2",,] = -1
 
   # posterior is the sum of terms
-  x = apply(m, c(1,2), sum)
+  # FIXME start with truncated pseudoinverse?
+  q = prior + apply(term, c(1,2), sum)
 
   for (iter in 1:max.iters) {
 
-    # sum of messages from all other factors
-    m.to = - sweep(m, c(1,2), x, "-")
-print(gamma.n2mv(m.to))
+    # sum of messages from all other terms
+    q1 = - sweep(term, c(1,2), q, "-")
+# print(gamma.n2mv(m.to))
 
     # messages from each factor, conditional on constraint
-    m.from = gamma.conditional.approx(m.to, A, b)
+    q.new = gamma.conditional.approx(q1, A, b)
 
     # difference between those messages, and current posterior
-    m.change = sweep(m.from, c(1,2), x, "-")
+#    change = sweep(m, c(1,2), q, "-")
 
     # update messages from each factor (possibly damped)
-    m = m + 1 * m.change
+#    term = term + 1 * change
+    term = q.new - q1;
 
     # update posterior, as sum of messages
-    x = apply(m, c(1,2), sum)
+    q = prior + apply(term, c(1,2), sum)
 
     # FIXME check for convergence
 
   }
 
-  list(x = x, m = m)
+  list(x = q, term = term)
 }
 
+A0 = t(rep(1,4))
+x0 = array(gamma.s2n(rbind(a=c(2,3,4,5), b=rep(1,4))), dim=c(2,4,1))
+dimnames(x0)[[1]] = c("e1", "e2")
+# r = gamma.conditional.approx(x0, A0, 1)
 
-A = rbind(
+r = approx.region.gamma(A0, 1, max.iters=100)
+
+A1 = rbind(c(1,1,0), c(0,1,1))
+r1 = approx.region.gamma(A1, c(1,1), max.iters = 6)
+
+if (FALSE) {
+A2 = rbind(
   c(1,1,1,1,1,0),
   c(0,0,0,1,1,1))
 rownames(A) = c("ceh-6", "hlh-1")
 colnames(A) = c("ABal", "ABar", "ABpl", "ABpr", "EMS", "P2")   # XXX bogus
-
-A0 = t(rep(1,5))
-x0 = array(gamma.s2n(rbind(a=rep(1,5), b=rep(1,5))), dim=c(2,5,1))
-dimnames(x0)[[1]] = c("e1", "e2")
-# r = gamma.conditional(x0, A0, 1)
-
-
-
-# r = approx.region.gamma(A0, c(1), max.iters=10)
-
-
+r2 = approx.region.gamma(A2, c(1,1), max.iters=3)
+}
 
