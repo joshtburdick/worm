a419df851b1d249c80aa700fe9d9674d03dfdbe4
Josh Burdick <jburdick@mail.med.upenn.edu>
2013-04-08 12:33:00 -0400

    Initial version.

---
+++ tf/motif/motifCount/makeMergedRegions.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w
+# Makes a file containing the merged upstream regions.
+
+open IN, "sort -k1,1 -k2,2n upstreamRegionsWS220_1kb.bed | bedtools merge -i |" || die;
+open OUT, ">upstreamRegionsWS220_1kb_merged.bed" || die;
+
+while (<IN>) {
+  chomp;
+  my($chr, $a, $b) = split /\t/;
+  print OUT (join "\t", ($chr, $a, $b, "upstreamRegion", "0", "+"));
+  print OUT "\n";
+}
+
+close IN;
+close OUT;
+
