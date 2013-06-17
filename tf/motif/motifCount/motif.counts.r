6598af2fd42a9d8e0cca37aef3582c35d56acf96
Josh Burdick <jburdick@mail.med.upenn.edu>
2013-06-17 14:18:49 -0400

    added intergenic (up-to-5kb) regions, for known and de novo motifs.

--- tf/motif/motifCount/motif.counts.r
+++ tf/motif/motifCount/motif.counts.r
@@ -21,18 +21,29 @@ motifs.to.use = intersect(motifs.to.use, rownames(m))
 
 t(m[ motifs.to.use, ])
 }
-motif = known.motifs.5kb()
+# motif = known.motifs.5kb()
 
+load("git/tf/motif/clusterUsingMotIV.Rdata")
 
 # the motifs, in the intergenic (up to 5kb) regions
-#known.motifs.1 = read.table(
-#  gzfile("git/tf/motif/motifCount/motifs_5kb_nogenes.tsv.gz"),
-#  "git/tf/motif/motifCount/foo.tsv",
-#  sep="\t", header=TRUE, row.names=1, check.names=FALSE, as.is=TRUE)
-#de.novo.motifs.1 = read.table(
-#  gzfile("git/tf/motif/motifCount/denovo_motifs_20130615_5kb_nogenes.tsv.gz"),
-#  sep="\t", header=TRUE, row.names=1, check.names=FALSE, as.is=TRUE)
+# XXX for whatever reason, the MX000045 motif is truncated.
+# I have no idea why this is, but as it's part of PRODORIC
+# (a prokaryotic database) anyway, for now I'm just ignoring the
+# problem (it will be omitted anyway.) (The issue may be
+# because that motif occurs a lot.)
+known.motifs.1 = read.table(
+  gzfile("git/tf/motif/motifCount/motifs_5kb_nogenes.tsv.gz"),
+  sep="\t", header=TRUE, row.names=1, check.names=FALSE, as.is=TRUE,
+  fill=TRUE)
+de.novo.motifs.1 = read.table(
+  gzfile("git/tf/motif/motifCount/denovo_motifs_20130615_5kb_nogenes.tsv.gz"),
+  sep="\t", header=TRUE, row.names=1, check.names=FALSE, as.is=TRUE)
 
-# known.motifs = 
+# restrict known motif set to those which were clustered
+m1 = intersect(rownames(known.motifs.1), motif.clusters$label)
 
+known.motifs = t(known.motifs.1[m1,])
+de.novo.motifs = t(de.novo.motifs.1)
+
+save(known.motifs, de.novo.motifs, file="git/tf/motif/motifCount/motif.counts.Rdata")
 
