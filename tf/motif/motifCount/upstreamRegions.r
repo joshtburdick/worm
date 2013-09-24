5258d52c2b741a3327ebd9614fa149c24932bb1e
Josh Burdick <jburdick@mail.med.upenn.edu>
2013-09-24 14:42:12 -0400

    Now includes a minimum (as well as maximum) distance, which hopefully avoids some weirdness with very small genes.

--- tf/motif/motifCount/upstreamRegions.r
+++ tf/motif/motifCount/upstreamRegions.r
@@ -3,23 +3,21 @@
 # Deprecated -- switching to using bedtools.
 # Nope, this may actually be used
 
-out.file = "git/tf/motif/motifCount/upstreamRegionsWS220_5kb_nogenes.bed"
-
-upstream.dist = 5000
+out.file = "git/tf/motif/motifCount/upstream_liftOver_WS220.bed"
 
 # base annotation table
-g = read.table("git/unmix/seq/quant/geneBounds_WS220.bed",
+g = read.table("git/unmix/seq/quant/geneBounds_liftOver_WS220.bed",
   sep="\t", as.is=TRUE)
 colnames(g) = c("chr", "a", "b", "gene.id", "score", "strand")
-
 
 # Computes upstream intergenic regions.
 # Args:
 #   g - a BED file, as a data.frame
+#   min.dist - the minimum upstream distance to include
 #   max.dist - the maximum upstream distance to include
 # Returns: g, with extra columns "a1" and "b1", giving
 #   the coordinates of the relevant upstream region.
-compute.upstream.intergenic = function(g, max.dist) {
+compute.upstream.intergenic = function(g, min.dist, max.dist) {
 
   g$a1 = NA
   g$b1 = NA
@@ -43,7 +41,7 @@ cat("\n")
   g$a1[ g$strand == "-" ] = g$b[ g$strand == "-" ]
   g$b1[ g$strand == "+" ] = g$a[ g$strand == "+" ]
 
-  # trim length to no more than max.dist
+  # trim length to be between min and max
   i = g$strand=="+" & (g$b1 - g$a1) > max.dist
   g$a1[ i ] = g$a[ i ] - max.dist
   i = g$strand=="-" & (g$b1 - g$a1) > max.dist
@@ -56,6 +54,7 @@ cat("\n")
 }
 
 # compute upstream regions
+if (FALSE) {
 upstream.region = data.frame(chr=g$chr,
   a=ifelse(g$strand=="+", g$a - upstream.dist, g$b),
   b=ifelse(g$strand=="+", g$a, g$b + upstream.dist),
@@ -63,15 +62,17 @@ upstream.region = data.frame(chr=g$chr,
   score=0,
   strand=g$strand)
 upstream.region$a[ upstream.region$a < 1 ] = 1
+}
 
-upstream.region = compute.upstream.intergenic(g, 5000)
+upstream.region = compute.upstream.intergenic(g, 500, 5000)
 upstream.region$a = upstream.region$a1
 upstream.region$b = upstream.region$b1
 upstream.region = upstream.region[,c(1:6)]
 
+if (FALSE) {
 write.table(upstream.region,
   file=out.file,
   sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
 
 system(paste("bedSort", out.file, out.file))
-
+}
