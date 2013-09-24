a81eca6d23440f8f759cf9a822fbb4814ee8f8ec
Josh Burdick <jburdick@mail.med.upenn.edu>
2013-09-24 14:36:30 -0400

    Revised to use new upstream regions (with min and max distance bounds.)

--- tf/motif/motifCount/motif.counts.r
+++ tf/motif/motifCount/motif.counts.r
@@ -1,49 +1,38 @@
 # Computes motif counts.
 # Currently "countMotifs.pl" does most of the work.
 
-# the known motifs, 5kb upstream (without conservation filtering)
-known.motifs.5kb  = function() {
-m = read.table(gzfile("git/tf/motif/motifCount/motifs_5kbUpstream.tsv.gz"),
-  sep="\t", header=TRUE, row.names=1, check.names=FALSE, as.is=TRUE)
-
-motif.list = read.table("data/tf/meme/motifList.tsv", sep="\t",
-  header=TRUE, stringsAsFactors=FALSE)
-
-# only using a subset of these
-db.to.use = c("JASPAR_CORE_2009_insects",
-  "JASPAR_CORE_2009_nematodes",
-  "JASPAR_CORE_2009_vertebrates",
-  "jolma2013")
-
-motifs.to.use =
-  motif.list[ motif.list$database %in% db.to.use, "id" ]
-motifs.to.use = intersect(motifs.to.use, rownames(m))
-
-t(m[ motifs.to.use, ])
-}
-# motif = known.motifs.5kb()
-
 load("git/tf/motif/clusterUsingMotIV.Rdata")
 
-# the motifs, in the intergenic (up to 5kb) regions
-# XXX for whatever reason, the MX000045 motif is truncated.
-# I have no idea why this is, but as it's part of PRODORIC
-# (a prokaryotic database) anyway, for now I'm just ignoring the
-# problem (it will be omitted anyway.) (The issue may be
-# because that motif occurs a lot.)
-known.motifs.1 = read.table(
-  gzfile("git/tf/motif/motifCount/motifs_5kb_nogenes.tsv.gz"),
-  sep="\t", header=TRUE, row.names=1, check.names=FALSE, as.is=TRUE,
-  fill=TRUE)
-de.novo.motifs.1 = read.table(
-  gzfile("git/tf/motif/motifCount/denovo_motifs_20130615_5kb_nogenes.tsv.gz"),
-  sep="\t", header=TRUE, row.names=1, check.names=FALSE, as.is=TRUE)
+known.motifs = {
 
-# restrict known motif set to those which were clustered
-m1 = intersect(rownames(known.motifs.1), motif.clusters$label)
+  # the known motifs, 5kb upstream (without conservation filtering)
+  m = read.table(gzfile("git/tf/motif/motifCount/knownMotif_5kbUp.tsv.gz"),
+    sep="\t", header=TRUE, row.names=1, check.names=FALSE, fill=TRUE)
 
-known.motifs = t(known.motifs.1[m1,])
-de.novo.motifs = t(de.novo.motifs.1)
+  # omit cases in which motif search didn't work for all genes
+  # XXX for whatever reason, some motifs such as MX000045 are truncated.
+  # I have no idea why this is, but as it's part of PRODORIC
+  # (a prokaryotic database) anyway, for now I'm just ignoring the
+  # problem (it will be omitted anyway.) (The issue may be
+  # because that motif occurs a lot.)
+  m = m[ apply(is.na(m), 1, sum) == 0 , ]
+
+  # only include motifs which were clustered
+  m = m[ intersect(rownames(m), motif.clusters$label) , ]
+
+  t(m)
+}
+
+de.novo.motifs = {
+  m = read.table(
+    gzfile("git/tf/motif/motifCount/deNovoMotif_5kbUp.tsv.gz"),
+    sep="\t", header=TRUE, row.names=1, check.names=FALSE, as.is=TRUE)
+
+  # see above
+  m = m[ apply(is.na(m), 1, sum) == 0 , ]
+
+  t(m)
+}
 
 save(known.motifs, de.novo.motifs, file="git/tf/motif/motifCount/motif.counts.Rdata")
 
