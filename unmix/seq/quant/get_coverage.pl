297372bba1efdee1ad22395a1aa5b0cec32d0ff8
Josh Burdick <jburdick@mail.med.upenn.edu>
2013-12-10 15:38:58 -0500

    - Theoretically counts only in exons, on the correct strand. - Total read counts are now written to a separate file for each group of experiments.

--- unmix/seq/quant/get_coverage.pl
+++ unmix/seq/quant/get_coverage.pl
@@ -1,50 +1,57 @@
 #!/usr/bin/perl -w
 # Computes coverage for all BAM files.
 
-# confusingly, "geneBounds_flip.tsv" is the "sense" strand quantification
+# gene bounds
+my $gene_bounds = "../../../data/seq/merged_genes_WS220.bed";
 
-if (undef) {
-compute_coverage("geneBounds_flip.tsv",
-  "/murrlab/seq/tophat2/20110922/",
-  "rawCoverage/20110922", 1);
-compute_coverage("geneBounds.tsv",
-  "/murrlab/seq/tophat2/20110922/",
-  "rawCoverage/20110922_as", 1);
+# just the exons of those
+my $gene_bounds_split = "../../../data/seq/merged_genes_split_WS220.bed";
+
+if (1) {
+compute_coverage($gene_bounds,
+  "/murrlab/seq/tophat2/WS220/20110922/",
+  "rawCoverage/WS220/20110922/", "-s");
+compute_coverage($gene_bounds,
+  "/murrlab/seq/tophat2/WS220/20110922/",
+  "rawCoverage/WS220/20110922_as/", "-S");
 }
 
-if (undef) {
-compute_coverage("geneBounds_flip.tsv",
-  "/murrlab/seq/tophat2/Murray050912/strand_flip",
-  "rawCoverage/Murray_050912", 1);
-compute_coverage("geneBounds.tsv",
-  "/murrlab/seq/tophat2/Murray050912/strand_flip",
-  "rawCoverage/Murray_050912_as", 1);
+
+if (1) {
+compute_coverage($gene_bounds,
+  "/murrlab/seq/tophat2/WS220/Murray050912/",
+  "rawCoverage/WS220/Murray050912/", "-s");
+compute_coverage($gene_bounds,
+  "/murrlab/seq/tophat2/WS220/Murray050912/",
+  "rawCoverage/WS220/Murray050912_as/", "-S");
 }
 
-if (undef) {
-compute_coverage("geneBounds_flip.tsv",
-  "/murrlab/seq/tophat2/Murray_52831_092812/strand_flip",
-  "rawCoverage/Murray_52831_092812", 1);
-compute_coverage("geneBounds.tsv",
-  "/murrlab/seq/tophat2/Murray_52831_092812/strand_flip",
-  "rawCoverage/Murray_52831_092812_as", 1);
+if (1) {
+compute_coverage($gene_bounds,
+  "/murrlab/seq/tophat2/WS220/Murray_52831_092812/",
+  "rawCoverage/WS220/Murray_52831_092812/", "-s");
+compute_coverage($gene_bounds,
+  "/murrlab/seq/tophat2/WS220/Murray_52831_092812/",
+  "rawCoverage/WS220/Murray_52831_092812_as/", "-S");
 }
 
 # timeseries; this shouldn't be stranded
-if (1) {
+if (undef) {
   compute_coverage("geneBounds_WS220.tsv",
     "/media/disk2/jburdick/embryo_timeseries",
-    "rawCoverage/embryo_ts", undef);
-
+    "rawCoverage_WS220/embryo_ts", "");
 }
 
 sub compute_coverage {
-  my($bed_file, $bam_dir, $out_dir, $stranded) = @_;
-  my $str = $stranded ? " -s " : "";
+  my($bed_file, $bam_dir, $out_dir, $str) = @_;
 
   system("mkdir -p $out_dir");
 
-  open COUNTS, ">read_counts.tsv" || die;
+  my $read_counts_file = $out_dir;
+  $read_counts_file =~ s/\/+$//;
+  $read_counts_file = $read_counts_file . ".readCounts.tsv";
+
+  open COUNTS, ">$read_counts_file" || die;
   print COUNTS "sample\ttotal.reads\n";
 
   foreach (<$bam_dir/*.bam>) {
@@ -53,15 +60,19 @@ sub compute_coverage {
     my $sample_name = $1;
     my $output_file = "$out_dir/$sample_name.tsv.gz";
 
-    # XXX temporary hack
 print "$file\n";
+    # XXX temporary hack
 #    next if not (($file =~ "\/03_") || ($file =~ "\/04_"));
 
     print "[writing coverage for $file to $output_file]\n";
     #  system ("coverageBed -abam $file -b $bed_file > $output_file");
     # for now, only including uniquely-mapping reads
-    #  system ("samtools view -q 255 -b $file | coverageBed -s -split -abam stdin -b $bed_file | gzip -c > $output_file");
-    system ("samtools view -q 1 -b $file | coverageBed $str -split -abam stdin -b $bed_file | gzip -c > $output_file");
+#    system ("samtools view -q 1 -b $file | bedtools coverage $str -split -abam - -b $bed_file | gzip -c > $output_file");
+
+    # only include reads which intersect at least one exon
+    # XXX this is finicky
+    system ("samtools view -q 1 -b $file | bedtools intersect $str -u -abam - -b $gene_bounds_split | bedtools coverage $str -split -abam - -b $gene_bounds |cut -f1-6,13-16 | gzip -c > $output_file");
+
     my $read_count = `samtools view -q 1 -c $file`;
     print COUNTS "$sample_name\t$read_count";
   }
