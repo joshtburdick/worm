87a3c23100a1be28a8dd423423c3a5439f097b20
Josh Burdick <jburdick@mail.med.upenn.edu>
2014-01-07 14:56:59 -0500

    About to modify this to hopefully be slightly more sensitive.

--- perl/run_tophat2.pl
+++ perl/run_tophat2.pl
@@ -8,20 +8,18 @@ use File::Temp;
 
 my $xsq_file = $ARGV[0];
 
+# directory to use for temporary files
 my $csfasta_dir = $ARGV[1];
 
 my $output_dir = $ARGV[2];
-die if not defined $output_dir;
 
-# The iGenome build (with colorspace index.)
-my $bwt_index = "~/data/seq/Caenorhabditis_elegans/UCSC/ce6/Sequence/ColorspaceBowtieIndex/genome";
-my $gene_gtf = "~/data/seq/Caenorhabditis_elegans/UCSC/ce6/Annotation/Genes/genes.gtf";
+die if not (@ARGV + 0 == 3);
+
+# genome and transcriptome indices
+my $bwt_index = "/var/tmp/data/tophat2/WS220/genome";
+my $transcriptome_index = "/var/tmp/data/tophat2/WS220/transcriptome";
 
 my $bin_dir = "/murrlab/software/bin";
-
-# this index of the transcriptome should get reused across runs
-# my $transcriptome_index = "/var/tmp/Caenorhabditis_elegans_ce6_genes";
-my $transcriptome_index = "~/data/seq/Caenorhabditis_elegans/UCSC/ce6/TranscriptomeColorspaceBowtieIndex/genes";
 
 # extract the reads into a temporary directory
 # my $csfasta_dir = File::Temp->newdir( CLEANUP => undef );
@@ -38,21 +36,21 @@ foreach my $f (<$csfasta_dir/Libraries/*/F3/reads/*.csfasta>) {
   my $library_name = $1;
   my $base_name = $2;
 
-  next if (($library_name eq "Unassigned") || ($library_name eq "Unclassified"));
+  next if (($library_name eq "Unassigned") ||
+    ($library_name eq "Unclassified"));
 
   print "$library_name $base_name\n";
 
   my $output_name = $base_name;
   $output_name =~ s/ /_/g;   # avoid spaces in names, as they cause issues
 
-  system("mkdir -p $csfasta_dir/modified_csfasta/$output_name");
-
   rename_csfasta("$csfasta_dir/Libraries/$library_name/", $base_name, $csfasta_dir);
   run_tophat2($csfasta_dir, "$output_dir/$output_name");
 }
 
-# removing csfasta directory
-# system("rm -rf $csfasta_dir");
+# removing csfasta directory; otherwise we run out of disk space
+# (and potentially re-map a bunch of reads)
+system("rm -rf $csfasta_dir");
 
 # Extracts csfasta files, with read name.
 sub rename_csfasta {
@@ -70,8 +68,8 @@ sub rename_csfasta {
 
   system("sed -e 's/_F5-RNA\$//' < \"$in_dir/F5-RNA/reads/$base_name" .
     "_F5-RNA.csfasta\" > $out_dir/F5-RNA.csfasta");
-  system("sed -e 's/_F3\$//' < \"$in_dir/F5-RNA/reads/$base_name" .
-    "_F5-RNA.csfasta\" > $out_dir/F5-RNA.qual");
+  system("sed -e 's/_F5-RNA\$//' < \"$in_dir/F5-RNA/reads/$base_name" .
+    "_F5-RNA.QV.qual\" > $out_dir/F5-RNA.qual");
 }
 
 # Runs Tophat2 on one library.
@@ -86,9 +84,8 @@ sub run_tophat2 {
 
   system("mkdir -p $output_dir");
 
-# note: not including "--GTF $gene_gtf", as we're using a prebuilt transcriptome index
 system(<<END);
-$bin_dir/tophat2 --transcriptome-index $transcriptome_index --GTF $gene_gtf --no-novel-juncs --library-type fr-secondstrand --mate-inner-dist 200 --mate-std-dev 200 --color --integer-quals --bowtie1 --num-threads 7 --output-dir $output_dir $bwt_index $reads_dir/F3.csfasta $reads_dir/F5-RNA.csfasta $reads_dir/F3.qual $reads_dir/F5-RNA.qual
+$bin_dir/tophat2 --transcriptome-index $transcriptome_index --no-novel-juncs --library-type fr-secondstrand --mate-inner-dist 200 --mate-std-dev 200 --color --integer-quals --bowtie1 --no-sort-bam --num-threads 7 --output-dir $output_dir $bwt_index $reads_dir/F3.csfasta $reads_dir/F5-RNA.csfasta $reads_dir/F3.qual $reads_dir/F5-RNA.qual
 END
 ;
 
