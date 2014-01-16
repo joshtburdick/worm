8e25fe700aea72c3298e68a93ca7d7adfb617a34
Josh Burdick <jburdick@mail.med.upenn.edu>
2014-01-16 14:11:48 -0500

    Rewrote to split based on gene name; this fixes issue with, e.g., ace-3 and hot-7.

--- data/seq/gene_locs_WS220.pl
+++ data/seq/gene_locs_WS220.pl
@@ -5,7 +5,7 @@
 
 use strict;
 
-use Graph::UnionFind;
+# use Graph::UnionFind;
 
 # Reads in mapping from transcript names to human-readable
 # WormBase names (when available.)
@@ -25,55 +25,6 @@ sub get_wormbase_names {
 }
 
 my $wormbase_name = get_wormbase_names();
-
-# Creates a BED6 file containing merged exons, and the names of all
-# genes overlapping each exon.
-sub get_exons_old {
-
-  my $infile = "~/data/wormbase/c_elegans.WS220.annotations.gff2.gz";
-
-  open OUT, "| bedtools sort -i - > exons_WS220.bed" || die;
-
-  # first, the majority of genes (from Wormbase)
-  open IN, "gunzip -c $infile | egrep \"(Coding_transcript|Non_coding_transcript)\" | bedtools sort -i - |" || die;
-  while (<IN>) {
-    my @a = split /\t/;
-    $a[0] =~ s/^CHROMOSOME_//;
-
-    if (($a[1] eq "Coding_transcript" ||
-        $a[1] eq "Non_coding_transcript")
-        && $a[2] eq "exon") {
-      die @a if not $a[8] =~ /Transcript "(\S+)"/;
-      my $name = $1;
-      print OUT (join "\t", $a[0], $a[3], $a[4], $name, 0, $a[6]);
-      print OUT "\n";
-    }
-  }
-  close IN;
-
-  # then, the non-coding RNAs
-  my $dir = "../../../data/expression/lincRNA/";
-  open IN,
-    "cat $dir/ancRNAs_W3PSeq3_ce10.gtf $dir/lincRNAs_W3PSeq3_ce10.gtf |"
-    || die;
-  while (<IN>) {
-    my @a = split /\t/;
-    $a[0] =~ s/^chr//;
-
-    if ($a[2] eq "exon") {
-      die @a if not $a[8] =~ /gene_id "(\S+)"/;
-      my $name = $1;
-      print OUT (join "\t", $a[0], $a[3], $a[4], $name, 0, $a[6]);
-      print OUT "\n";
-    }
-  }
-  close IN;
-
-  close OUT;
-
-  system("bedtools merge -s -nms -i exons_WS220.bed " .
-    "| bedtools sort -i - > merged_exons_WS220.bed");
-}
 
 # Creates a BED6 file containing merged exons, and the names of all
 # genes overlapping each exon.
@@ -112,106 +63,48 @@ sub get_exons {
     "| bedtools sort -i - > merged_exons_WS220.bed");
 }
 
-# Given the set of exons, groups all the genes overlapping together.
+# Given a BED6 file, writes a BED6 file with one record per name
+# (covering the range of all records having that name.)
 # Args:
-#   exon_file - name of the file of exons to read (in BED6 format)
-#   gff_output_file - name of the GFF file to output
-# Side effects: writes a GFF file
-sub group_exons {
-  my($exon_file, $gff_output_file) = @_;
+#   in_file - the file to read
+#   out_file - the file to write
+sub get_gene_bounds {
+  my($in_file, $out_file) = @_;
 
-  my $uf = Graph::UnionFind->new;
+  # record of bounds, each in BED6 format
+  my %b = ();
 
-  # first, pick a "canonical" gene for each exon
-  open IN, "<$exon_file" || die;
+  # group records by name
+  open IN, "<$in_file" || die;
   while (<IN>) {
     chomp;
     my @a = split /\t/;
 
-    # this should be the names of all exons overlapping here
-    my @genes = split /;/, $a[3];
-    foreach my $g (@genes) {
-      $uf->union($genes[0], $g);
+    # have we seen this name before?
+    if (!defined($b{$a[3]})) {
+      # add initial record
+      $b{ $a[3] } = \@a;
+    }
+    else {
+      # update bounds
+      my @r = @{ $b{ $a[3] } };
+      $r[1] = ($a[1] < $r[1] ? $a[1] : $r[1]);
+      $r[2] = ($a[2] > $r[2] ? $a[2] : $r[2]);
+      die if not ($a[5] eq $r[5]);
+      $b{ $a[3] } = \@r;
     }
   }
   close IN;
 
-  # write out file as GFF format, with canonical name
-  open IN, "<$exon_file" || die;
-  open OUT, ">$gff_output_file" || die;
-  while (<IN>) {
-    chomp;
-    my @a = split /\t/;
-    my $name = $a[3];
-    my @genes = split /;/, $a[3];
-    my $canonical_name = $uf->find($genes[0]);
-    print OUT (join "\t", ($a[0], "WormBase", "exon",
-      $a[1], $a[2], 0, $a[4], ".", $canonical_name));
-    print OUT "\n";
-  }
-  close IN;
-  close OUT;
-}
-
-
-# Converts from GFF format to BED format.
-# Args:
-#   gff_in - GFF file to convert
-#   bed_out - BED file to write to
-sub gff_to_bed {
-  my($gff_in, $bed_out) = @_;
-
-
-  # first, read in all records, indexed by gene
-  open IN, "bedtools sort -i $gff_in |" || die;
-  my %g = ();
-  while (<IN>) {
-    chomp;
-    my @a = split /\t/;
-    push @{$g{$a[8]}}, \@a;
-  }
-  close IN;
-
-  # then, write out each gene
-  open OUT, "| bedtools sort -i - > $bed_out" || die;
-  foreach my $id (keys %g) {
-
-    # sort by start location (may not be necessary)
-    my @r = @{$g{$id}};
-    @r = sort { $a->[3] <=> $b->[3] } @r;
-
-    # aggregate all this information
-    my $a = $r[0]->[3];
-    my $b = $r[@r-1]->[4];
-    my @sizes = ();
-    my @starts = ();
-    foreach (@r) {
-      push @sizes, $_->[4] + 1 - $_->[3];
-      push @starts, $_->[3] + 0 - $a;
-    }
-
-    # if possible, convert ID to be a Wormbase gene name
-    if (defined $wormbase_name->{$id}) {
-      $id = $wormbase_name->{$id};
-    }
-
-    print OUT join "\t", ($r[0]->[0], $a-1, $b, $id, 0,
-      $r[0]->[6], $a-1, $b, "0,0,0", (@sizes+0),
-      (join ",", @sizes), (join ",", @starts));
+  # write the output (and sort it)
+  open OUT, "| bedtools sort -i - > $out_file" || die;
+  foreach (values(%b)) {
+    print OUT (join "\t", @{$_});
     print OUT "\n";
   }
   close OUT;
 }
 
 get_exons();
-group_exons("merged_exons_WS220.bed", "merged_genes_WS220.gff");
-gff_to_bed("merged_genes_WS220.gff", "merged_genes_WS220.bed");
-
-# XXX the coordinates for these are off-by-one; not worrying
-# about this for now, but still cleaning up these files
-# unlink("exons_WS220.bed");
-# unlink("merged_exons_WS220.bed");
-
-
-system("bedtools bed12tobed6 -i merged_genes_WS220.bed | bedtools sort -i - > merged_genes_split_WS220.bed");
+get_gene_bounds("exons_WS220.bed", "merged_genes_WS220.bed");
 
