25dd0ed0173dfeb4238aaea313922bdbddb0b551
Josh Burdick <jburdick@mail.med.upenn.edu>
2014-02-03 19:38:29 -0500

    Now reads in gene names not as factors.

--- data/name_convert.r
+++ data/name_convert.r
@@ -1,16 +1,22 @@
 # Utilities for converting names, such as gene names.
 
-gene.ids = read.csv(gzfile("data/wormbase/geneIDs.WS224.csv.gz"),
-  as.is=TRUE, header=FALSE)
-colnames(gene.ids) = c("wb.gene", "gene", "transcript")
-gene.ids = gene.ids[ gene.ids[,"gene"] != "" , ]
+# WormBase annotation
+gene.ids = read.table(gzfile("data/wormbase/c_elegans.PRJNA13758.WS240.xrefs.txt.gz"), sep="\t", as.is=TRUE)[,c(1,2,3,4)]
+colnames(gene.ids) = c("gene", "wb.gene", "gene.name", "transcript")
+# omit "unknown gene name" cases
+gene.ids = gene.ids[ gene.ids$gene.name != "." , ]
+gene.id.map = unique(rbind(
+  data.frame(id = gene.ids$gene, gene.name=gene.ids$gene.name),
+  data.frame(id = gene.ids$transcript, gene.name=gene.ids$gene.name)))
 
-# Renames the row names of a matrix (or data frame).
+# Renames a vector of gene names.
+# This tries to convert all gene identifiers to gene names,
+# where known.
 rename.gene.names = function(a) {
   r = rownames(a)
-  i = intersect(r, gene.ids[,"transcript"])
+  i = intersect(r, gene.id.map[,"id"])
   r[ match(i, r) ] =
-    gene.ids[ match(i, gene.ids[,"transcript"]), "gene" ]
+    gene.id.map[ match(i, gene.id.map[,"id"]), "gene.name" ]
 
   a = a[ !duplicated(r) , ]
   r = r[ !duplicated(r) ]
