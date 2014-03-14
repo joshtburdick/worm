181d486653831a431701c5af1afd1c5ffd27db6d
Josh Burdick <jburdick@mail.med.upenn.edu>
2014-03-14 14:09:03 -0400

    Slight changes; possibly not using this after all.

--- unmix/ml/pos_lsei.r
+++ unmix/ml/pos_lsei.r
@@ -1,16 +1,32 @@
 # Attempt at a simple greedy way of solving a particular
 # lsei-type problem (in which the constraints are all "x >= 0".)
 
+library(Matrix)
 library(corpcor)
 library(limSolve)
 
-# Efficiency be darned! Using slower, old, mvnorm.2.diag().
-source("git/unmix/ept/approx_region.r")
+# Conditions a normal distribution (with diagonal covariance)
+# on a linear constraint, somewhat as in the EP stuff.
+# Args:
+#   v - the (diagonal) variance
+#   A, b.var - these define the "Ax = N(b, b.var)" constraint
+#   b - also part of the constraint (separated out to avoid
+#     recomputing the pseudoinverse
+#   m - the current mean
+# Returns: the updated mean
+mvnorm.cond.diag = function(v, A, b.var) {
+  V = Diagonal( x = v )
 
+  # first, compute (A V A^T + v I) ^ -1, which I'll call B
+  M = as.matrix( A %*% V %*% t(A) + Diagonal(x = b.var) )
 
+  B = pseudoinverse(M)
+#  B = chol2inv(chol(M))
 
-
-
+  # we return a function which computes this
+  function(m, b)
+    m - as.vector(V %*% t(A) %*% B %*% (A %*% m - b))
+}
 
 # Goes "as far as possible" in some direction, without
 # any element of a vector going negative.
@@ -31,26 +47,60 @@ move.bounded = function(a, b) {
   a + m * delta
 }
 
-# ??? does this "get stuck" after one step?
-# Apparently not.
-find.mean.greedy = function(A, b, num.iters=10) {
-  P = pseudoinverse(A)
-
-  # start with the "truncated pseudoinverse"
-  x = P %*% b
-  x[ x < 0 ] = 0
-
-  # matrix to hold the results
-  X = matrix(NA, nrow=num.iters, ncol=length(x))
-  X[1,] = x
-  for(i in 2:num.iters) {
-    r = mvnorm.2.diag(x, 1+0*x, A, b, 0*b)
-    x.p = r[,1]
-    x = move.bounded(x, x.p)
-    x[x < 0] = 0
-    X[i,] = x
+# Finds a min-norm positive solution to Ax = b.
+# Args:
+#   A, b.var, b - these give the linear constriaint
+#   max.iters - maximum number of iterations to go
+#   converge.tol - if an update has a 2-norm smaller than
+#     this, then we consider that we've "converged"
+#     (even though the constraints may not be satisfied)
+# Returns: smallest |x|, with all x >= 0, matching the
+#   constraints (or the nearest non-negative point, if
+#   there is no such point)
+pos.lsei = function(A, b.var=NULL) {
+  if (is.null(b.var)) {
+    b.var = rep(0, nrow(A))
   }
 
-  X
+  # function which takes a point to the nearest point
+  # matching the constraints
+  f = mvnorm.cond.diag(rep(1, ncol(A)), A, b.var)
+
+  function(b, max.iters = 100, converge.tol = 1e-5) {
+
+
+    # start with the "truncated pseudoinverse"
+    x = pseudoinverse(A) %*% b
+    x[ x < 0 ] = 0
+
+    update.stats = cbind(err = max(abs(A %*% x - b)),
+      amount.moved = NA)
+
+    # matrix to hold the results
+  #  X = matrix(NA, nrow=max.iters, ncol=length(x))
+  #  X[1,] = x
+
+    err = max(abs(A %*% x - b))
+
+    for(i in 2:max.iters) {
+cat(i, "")
+      x1 = move.bounded(x, f(x, b))
+      x1[x1 < 0] = 0
+      err1 = max(abs(A %*% x1 - b))
+      update.stats = rbind(update.stats,
+        cbind(err = err1, amount.moved = max(abs(x1 - x))))
+      x = x1
+
+      if (abs(err1) <= converge.tol) {
+ cat("converged in", i, "\n")
+        return(x1)
+      }
+      x = x1
+      err = err1
+    }
+
+    x
+  }
 }
+
 
--- unmix/ml/pos_lsei_tests.r
+++ unmix/ml/pos_lsei_tests.r
@@ -4,25 +4,28 @@ source("git/unmix/ml/pos_lsei.r")
 
 
 
-r0 = find.mean.greedy(t(c(1,1,1)), 1)
+r0 = pos.lsei(t(c(1,1,1)))(1)
 
-set.seed(423)
-A1 = matrix(runif(50000), nrow=50)
-x1 = rgamma(1000, shape=0.5, 1)
+set.seed(12)
+# A1 = matrix(runif(50000), nrow=50)
+A1 = matrix(rbeta(50000, 0.1, 0.1), nrow=50)
+x1 = rgamma(1000, shape=0.1, scale=50)
+# x1 = rbeta(1000, 0.1, 0.1)
 b1 = A1 %*% x1
 
-print(system.time(r1 <- find.mean.greedy(A1, b1, num.iters=10)))
+print(system.time(r1 <- pos.lsei(A1)(b1)))
 
-print(system.time(r2 <- ldei(A1, b1, diag(ncol(A1)), rep(0, ncol(A1)))))
+err = (A1 %*% r1) - as.vector(b1)
 
-err = (A1 %*% t(r1)) - as.vector(b1)
-
-sse = apply(err^2, 2, sum)
+sse = apply(err^2, 2, max)
 
 # error in meeting the constraints
 print(sse)
 
+print(system.time(r2 <- ldei(A1, b1, diag(ncol(A1)), rep(0, ncol(A1)))))
+# print(system.time(r2 <- nnls(A1, b1)))
+
 # difference from the ldei() solution
-print(sum( (r1[nrow(r1),] - r2$X)^2 )) 
+print(sum( (r1 - r2$X)^2 )) 
 
 
