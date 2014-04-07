6d5a3c2f6476f109f176c35bbe762e35d9f8d413
Josh Burdick <jburdick@mail.med.upenn.edu>
2014-04-07 14:40:42 -0400

    Adjusting how specific the genes I use are.

--- sort_paper/enrichment/anatomyEnrichmentSmall.r
+++ sort_paper/enrichment/anatomyEnrichmentSmall.r
@@ -20,8 +20,26 @@ ao = data.frame(gene=rename.gene.name.vector(wb.anatomy$Gene.Public.Name),
 
 # subset to a smaller set of terms
 num.genes.per.anatomy.term = sort(table(ao$group.name), decreasing=TRUE)
-anatomy.term.1 = num.genes.per.anatomy.term[1:30]
+anatomy.term.1 = num.genes.per.anatomy.term[1:15]
 ao = ao[ao$group.name %in% names(anatomy.term.1) , ] 
+
+# clobber some anatomy terms with less-specific terms\
+ao[ao$group.name %in% c("head neuron", "nerve ring", "nervous system",
+  "neuron", "tail neuron", "ventral cord neuron"), "group.name"] = "neuron"
+ao[ao$group.name %in% c("body wall musculature", "vulval muscle"),
+  "group.name"] = "muscle"
+ao[ao$group.name %in% c("reproductive system", "spermatheca", "vulva"),
+  "group.name"] = "reproductive"
+
+
+
+
+# possibly restrict to genes only annotated with one term
+unique.gene = {
+  gene.count = table(ao$gene)
+  names(gene.count)[ gene.count == 1 ]
+}
+# ao = ao[ ao$gene %in% unique.gene , ]
 
 # information about clusters
 wb.cluster = read.table(gzfile("data/wormbase/gene_expr_cluster.tsv.gz"),
@@ -37,17 +55,6 @@ num.genes = 15683
 
 system("mkdir -p git/sort_paper/enrichment/anatomyEnrichment/")
 
-# enrichment of things in lineages
-if (TRUE) {
-  e = read.tsv("git/sort_paper/unmix/lineageEnriched.tsv")
-  e = e[ grep("enriched", e$set), ]
-  r = hyperg.test.groups.many(ao, e, num.genes)
-  r$p[ is.nan(r$p) ] = 1
-  r$p.corr = p.adjust(r$p, method="fdr")
-  r = r[ r$p.corr <= 0.05 , ]
-  write.tsv(r, "git/sort_paper/enrichment/anatomyEnrichment/lineageEnrichedAnatomyTermsSmall.tsv")
-}
-
 # ... and in tissues
 if (TRUE) {
   e = read.tsv("git/sort_paper/unmix/tissueEnriched.tsv")
@@ -57,5 +64,17 @@ if (TRUE) {
   r$p.corr = p.adjust(r$p, method="fdr")
   r = r[ r$p.corr <= 0.05 , ]
   write.tsv(r, "git/sort_paper/enrichment/anatomyEnrichment/tissueEnrichedAnatomyTermsSmall.tsv")
+}
+
+# enrichment of things in lineages
+if (TRUE) {
+  e = read.tsv("git/sort_paper/unmix/lineageEnriched.tsv")
+  lin.enriched = e
+  e = e[ grep("enriched", e$set), ]
+  r = hyperg.test.groups.many(ao, e, num.genes)
+  r$p[ is.nan(r$p) ] = 1
+  r$p.corr = p.adjust(r$p, method="fdr")
+  r = r[ r$p.corr <= 0.05 , ]
+  write.tsv(r, "git/sort_paper/enrichment/anatomyEnrichment/lineageEnrichedAnatomyTermsSmall.tsv")
 }
 
