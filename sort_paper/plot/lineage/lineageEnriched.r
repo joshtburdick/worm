3ca8c7cbbe8d2ed18e68d3ee41878a76153fa3cd
Josh Burdick <jburdick@mail.med.upenn.edu>
2014-04-15 12:35:09 -0400

    Added known tissue-type counts.

--- sort_paper/plot/lineage/lineageEnriched.r
+++ sort_paper/plot/lineage/lineageEnriched.r
@@ -6,8 +6,12 @@
 
 library(plotrix)
 
+source("git/utils.r")
 source("git/plot/plot_expr.r")
 source("git/sort_paper/enrichment/anatomyEnrichmentSmall.r")
+
+tissues.per.cell = read.table("data/worm/TissuesPerCell.tsv",
+  sep="\t", quote="", header=TRUE, row.names=1, as.is=TRUE)
 
 # the cells which are included as leaf nodes
 cells.23 = c("ABalaa", "ABalap", "ABalpa", "ABalpp", 
@@ -16,7 +20,23 @@ cells.23 = c("ABalaa", "ABalap", "ABalpa", "ABalpp",
   "ABpraa", "ABprap", "ABprpa", "ABprpp",
   "MSaa", "MSap", "MSpa", "MSpp", "E", "C", "P3")
 
-# Plots using ggplot.
+# Gets counts of known tissue annotations for a lineage.
+# XXX requires a certain amount of munging
+known.tissue.count = function(lineage) {
+  cells = names(which(cell.lineage.matrix[lineage,]>0))
+  tissues = tissues.per.cell[ cells , "Tissue" ]
+
+  tissues = tissues[ ! tissues %in% c("", "Death", "Other") ]
+  c(hypdermis = sum(tissues %in% c("Epidermis")),
+    intestine = sum(tissues %in% c("Intestine")),
+    muscle = sum(tissues %in% c("Muscle")),
+    neuron = sum(tissues %in% c("Glia", "Nervous")),
+    pharynx = sum(tissues %in% c("Pharynx")),
+    reproductive = sum(tissues %in% c("Gonad")),
+    "seam cell" = 0)
+}
+
+# Plots using ggplot (deprecated).
 plot.dendro.gg = function() {
   ddata = dendro_data(lin)
 
@@ -61,8 +81,7 @@ interior.cells =
   names(which(apply(cell.lineage.matrix[,cells.23] > 0, 1, any)))
 
 pdf("git/sort_paper/plot/lineage/lineageEnriched.pdf",
-  width=15, height=5)
-
+  width=15, height=7)
 r = data.frame(cell = as.character(rownames(cell.time.on.off)),
   time.1=cell.time.on.off$on, time.2=cell.time.on.off$off,
   stringsAsFactors=FALSE)
@@ -73,20 +92,31 @@ rownames(r) = r$cell
 
 # plot the tree
 plot.segments(r, "Anatomy annotations in lineages",
-  root="P0", c(0,120), lwd=3, yaxt="n",
+  root="P0", c(0,133), lwd=3, yaxt="n",
   int.n.to.label = cells.23, srt=90)
 
-pie.colors = hsv(0:(length(tissue.names)-1) / length(tissue.names), 0.7, 1)
+# rectangle to mark known annotation
+rect(-10000, 123, 10000, 150, lwd=0, col="lightgrey")
+
+pie.colors =
+  hsv(0:(length(tissue.names)-1) / length(tissue.names), 0.7, 0.9)
 
 for(cell in cells.23) {
   tl = tissue.lineage[ cell , ]
+
+  # first, the measured counts
   if (sum(tl) > 0) {
-  floating.pie(r[cell,"x"], r[cell,"time.2"],
-    tl + 1e-6,
-    col = pie.colors,
-    radius = 10)
+    floating.pie(r[cell,"x"], r[cell,"time.2"],
+      tl + 1e-6, col = pie.colors, radius = 9)
   }
+
+  # then, the known tissue types per lineage (from annotation)
+  floating.pie(r[cell,"x"], 130,
+    known.tissue.count(cell) + 1e-6,
+    col=pie.colors, radius = 9)
 }
+
+axis(2, at=c(100, 130), labels=c("predicted", "known"), tick = FALSE)
 
 legend("topleft", legend=tissue.names, fill = pie.colors, cex=0.84)
 
