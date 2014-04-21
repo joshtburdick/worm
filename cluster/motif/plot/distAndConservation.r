556433aeff4de44b454c18957fe96e0c46e835dd
Josh Burdick <jburdick@mail.med.upenn.edu>
2014-04-21 18:24:41 -0400

    About to try to modify this to plot individual graphs.

--- cluster/motif/plot/distAndConservation.r
+++ cluster/motif/plot/distAndConservation.r
@@ -5,7 +5,7 @@ source("git/utils.r")
 
 output.dir = "git/cluster/motif/plot/distAndConservation/"
 
-motif.gene.dir = "git/cluster/motif/distAndConservation/"
+motif.gene.dir = "git/cluster/motif/distAndConservation/5kb/"
 
 clusters = motifs.and.clusters = read.tsv(
   "git/cluster/hierarchical/hier.200.clusters/clusters.tsv")
@@ -14,58 +14,67 @@ motifs.and.clusters = read.table(
   "git/cluster/hierarchical/hier.200.clusters/uniqueKnownMotifEnrichment_5kb_0.5cons.tsv",
   header=TRUE, as.is=TRUE)
 
-# Does a Mann-Whitney test for different distribution
-# of upstream distance and conservation, between genes
-# in the cluster, and outside the cluster.
-compute.dist.conservation.wilcoxon = function() {
-  r.dist = NULL
-  r.conservation = NULL
+# data for histogram of amount of upstream conservation
+upstream.cons.hist = read.tsv(gzfile(
+  "git/tf/motif/conservation/upstreamRegionsWS220_conservationHist.txt.gz"))
 
-  for(i in 1:5) {    # nrow(motifs.and.clusters)) {
+# comparisons of upstream motif location and conservation
+dist.cons.wilcoxon = read.table(
+  "git/cluster/motif/plot/distConservationWilcoxon.tsv",
+  sep="\t", header=TRUE, row.names=NULL, as.is=TRUE)
 
-    m = motifs.and.clusters[i,"motif"]
-    cl = motifs.and.clusters[i,"cluster"]
-    cat(backspace.string, i, m, cl)
+# Computes histogram counts "by hand". This avoids the issue
+# that "hist(..., plot=FALSE)" is finicky about where the
+# buckets are. Also, it's useful for making cumulative histograms.
+hist.count = function(x, lim, num.buckets=20) {
+  x = x[ !is.na(x) ]
 
-    r = read.table(paste(motif.gene.dir, m, "_upstreamMotifCons.tsv.gz", sep=""),
-      as.is=TRUE)
-    colnames(r) = c("region.chr", "region.a", "region.b", "gene", "score",
-      "strand", "motif", "motif.a", "motif.b", "motif.id", "motif.cons")
-    r$upstream.dist = ifelse(r$strand=="+",
-      r$motif.a - r$region.b, r$region.a - r$motif.a)
-    r$in.cluster = clusters[r$gene,"cluster"]==cl
-    r = r[ !is.na(r$in.cluster) , ]
-    if (sum(r$in.cluster) > 0 && sum(!r$in.cluster) > 0) {
-      r0 = r[ !r$in.cluster , ]
-      r1 = r[ r$in.cluster , ]
+  # scale to [0,1]
+  x1 = floor( num.buckets * (x - lim[1]) / (lim[2] - lim[1]) )
+  x1[ x1 < 0 ] = 0
+  x1[ x1 >= num.buckets ] = num.buckets - 1
 
-      a = wilcox.test(r1$upstream.dist, r0$upstream.dist)
-      a1 = data.frame(motif = m, cluster = cl,
-        num.motifs.in.cluster = nrow(r1),
-        num.motifs.in.background = nrow(r0),
-        U = a$statistic, p = a$p.value, stringsAsFactors=FALSE)
-      r.dist = rbind(r.dist, a1)
+  counts = table(x1)
 
-      a = wilcox.test(r1$motif.cons, r0$motif.cons)
-      a1 = data.frame(motif = m, cluster = cl,
-        num.motifs.in.cluster = nrow(r1),
-        num.motifs.in.background = nrow(r0),
-        U = a$statistic, p = a$p.value, stringsAsFactors=FALSE)
-      r.conservation = rbind(r.conservation, a1)
-    }
-  }
+  r = rep(0, num.buckets)
+  r[as.numeric(names(counts))+1] = as.vector(counts)
 
-  r.dist$p.fdr = p.adjust(r.dist$p, method="fdr")
-  r.conservation$p.fdr = p.adjust(r.conservation$p, method="fdr")
+  data.frame(breaks = lim[1] + (c(0:(num.buckets-1)) / num.buckets) *
+    (lim[2] - lim[1]),
+    counts = r)
+}
 
-  list(r.dist = r.dist, r.conservation = r.conservation)
+# Gets a histogram of amount of conservation.
+# Args:
+#   genes - genes to get conservation for
+# Returns: data frame with columns
+#   conservation - bucketized amount of conservation
+#   bases - number of bases with that amount of conservation
+get.conservation.buckets = function(genes) {
+  g = intersect(genes, rownames(upstream.cons.hist))
+  cons = apply(upstream.cons.hist[g,], 2, sum)
+
+  data.frame(conservation = as.numeric(names(cons)),
+    bases = as.vector(cons))
+}
+
+# Gets the number of base pairs upstream of each gene.
+# (I'm using the conservation data here, which is admittedly
+# sort of a hack.)
+get.num.bp.upstream = function(genes) {
+  g = intersect(genes, rownames(upstream.cons.hist))
+  sizes = -apply(upstream.cons.hist[g,], 1, sum)
+
+  h = hist.count(sizes, c(-5000,0), num.buckets=20)
+
+  data.frame(bases = h$breaks, count = 250 * cumsum(h$counts))
 }
 
 # Plots all the graphs.
 plot.all = function(wilcox.results) {
   system(paste("mkdir -p", output.dir))
 
-  for(i in 1:5) {  # nrow(motifs.and.clusters)) {
+  for(i in 1:nrow(motifs.and.clusters)) {
 
     m = motifs.and.clusters[i,"motif"]
     cl = motifs.and.clusters[i,"cluster"]
@@ -81,31 +90,104 @@ plot.all = function(wilcox.results) {
 
     if (sum(r$in.cluster > 0)) {
 #      png(paste(output.dir, "/", m, " ", cl, ".png", sep=""),
-#        width=1050, height=700)
+#        width=1050, height=1200)
       pdf(paste(output.dir, "/", m, " ", cl, ".pdf", sep=""),
-        width=9, height=6)
+        width=9, height=12)
 
-      par(mfrow=c(2,3))
+      mat = matrix(c(1,1,1,2,3,4,5,6,7,8,9,10,11,12,13), nrow=5, byrow=TRUE)
+      layout(mat, heights=c(0.5,1,1,1))
+
+      # add labels
+      par(mar=c(0,0,0,0) + 0.1)     # XXX
+      plot(0,0, xlim=c(-1,1), ylim=c(0,5), type="n", bty="n",
+        xaxt="n", yaxt="n", xlab="", ylab="")
+      text(0,3, paste("Motif", m, "near genes in cluster", cl), cex=2.5)
+#      text(0,0, "testing")
+      text(0,1, paste("Motif enrichment p =",
+        signif(motifs.and.clusters[i,"p.fdr"], 2)), cex=1.5)
+
+      par(mar=c(5,4,4,2)+0.1)
+
+      # par(mfrow=c(2,3))
       r0 = r[ !r$in.cluster , ]
       r1 = r[ r$in.cluster , ]
 
-      s = paste("Motif", m, "near genes in cluster", cl)
-      plot(r1$upstream.dist, r1$motif.cons, col="#ff000080", pch=20, 
+      # numbers for motifs near genes in clusters
+      s = paste("Motif near genes in cluster")
+      plot(r1$upstream.dist, r1$motif.cons, col="#ff000040", pch=20, 
         xlim=c(-5000,0), ylim=c(0,1),
         main=s, xlab="Location relative to TSS", ylab="Conservation (PhastCons)")
-      hist(r1$upstream.dist, col="#ff0000a0", breaks=30, xlim=c(-5000,0),
+      hist(r1$upstream.dist, col="#ff0000a0", xlim=c(-5000,0),
         main=s, xlab="Location relative to TSS")
-      hist(r1$motif.cons, col="#ff0000a0", breaks=30, xlim=c(0, 1),
+      dist.p = dist.cons.wilcoxon[ dist.cons.wilcoxon$motif==m &
+        dist.cons.wilcoxon$cluster==cl &
+        dist.cons.wilcoxon$number=="upstream.dist", "p.fdr" ]
+      mtext(paste("Wilcoxon p =", signif(dist.p,2) ), cex=0.8)
+      hist(r1$motif.cons, col="#ff0000a0", xlim=c(0, 1),
+        main=s, xlab="Conservation (PhastCons)")
+      cons.p = dist.cons.wilcoxon[ dist.cons.wilcoxon$motif==m &
+        dist.cons.wilcoxon$cluster==cl &
+        dist.cons.wilcoxon$number=="conservation", "p.fdr" ]
+      mtext(paste("Wilcoxon p =", signif(cons.p,2)), cex=0.8)
+
+      # similarly, numbers for motifs in other clusters
+      s = paste("Motif near genes not in cluster")
+      plot(r0$upstream.dist, r0$motif.cons, col="#0000ff40", pch=20,
+        xlim=c(-5000,0), ylim=c(0,1),
+        main=s, xlab="Location relative to TSS", ylab="Conservation (PhastCons)")
+      hist(r0$upstream.dist, col="#0000ffa0", xlim=c(-5000,0),
+        main=s, xlab="Location relative to TSS")
+      hist(r0$motif.cons, col="#0000ffa0", xlim=c(0, 1),
         main=s, xlab="Conservation (PhastCons)")
 
-      s = paste("Motif", m, "near genes not in cluster", cl)
-      plot(r0$upstream.dist, r0$motif.cons, col="#0000ff80", pch=20,
-        xlim=c(-5000,0), ylim=c(0,1),
-        main=s, xlab="Location relative to TSS", ylab="Conservation (PhastCons)")
-      hist(r0$upstream.dist, col="#0000ffa0", breaks=30, xlim=c(-5000,0),
-        main=s, xlab="Location relative to TSS")
-      hist(r0$motif.cons, col="#0000ffa0", breaks=30, xlim=c(0, 1),
-        main=s, xlab="Conservation (PhastCons)")
+      # same, for all sequence upstream of genes near clusters
+      plot.new()
+
+      bp = get.num.bp.upstream(clusters[ clusters[ r$gene,"cluster"]==cl , "gene" ])
+      barplot(bp$count, col="#ff0000a0", space=0,
+        main="All sequence near genes in cluster",
+        xlab="Location relative to TSS", ylab="Frequency")
+      # XXX
+      par(new=TRUE)
+      plot(0,0, type="n", xlim=c(-5000,0), ylim=c(0,1), bty="n",
+        xaxt="n", yaxt="n", xlab="", ylab="")
+      axis(1, line=0.4)
+
+      cons = get.conservation.buckets(
+        clusters[ clusters[ r$gene,"cluster"]==cl , "gene" ])
+      barplot(cons$bases, col="#ff0000a0", space=0,
+        main="All sequence near genes in cluster",
+        xlab="Conservation (PhastCons)", ylab="Frequency")
+      # XXX
+      par(new=TRUE)
+      plot(0,0, type="n", xlim=c(0,1), ylim=c(0,1), bty="n",
+        xaxt="n", yaxt="n", xlab="", ylab="")
+      axis(1, line=0.4)
+
+
+      # and again, background numbers in all clusters
+      plot.new()
+
+      bp = get.num.bp.upstream(clusters[ clusters[ r$gene,"cluster"]!=cl , "gene" ])
+      barplot(bp$count, col="#0000ffa0", space=0,
+        main="All sequence near genes not in cluster",
+        xlab="Location relative to TSS", ylab="Frequency")
+      # XXX
+      par(new=TRUE)
+      plot(0,0, type="n", xlim=c(-5000,0), ylim=c(0,1), bty="n",
+        xaxt="n", yaxt="n", xlab="", ylab="")
+      axis(1, line=0.4)
+
+      cons = get.conservation.buckets(
+        clusters[ clusters[ r$gene,"cluster"]!=cl , "gene" ])
+      barplot(cons$bases, col="#0000ffa0", space=0,
+        main="All sequence near genes not in cluster",
+        xlab="Conservation (PhastCons)", ylab="Frequency")
+      # XXX
+      par(new=TRUE)
+      plot(0,0, type="n", xlim=c(0,1), ylim=c(0,1), bty="n",
+        xaxt="n", yaxt="n", xlab="", ylab="")
+      axis(1, line=0.4)
 
       dev.off()
     }
@@ -113,14 +195,7 @@ plot.all = function(wilcox.results) {
 }
 
 if (TRUE) {
-  r = compute.dist.conservation.wilcoxon()
 
-  write.table(rbind(cbind(number="upstream.dist", r$r.dist),
-    cbind(number="conservation", r$r.conservation)),
-    file="git/cluster/motif/plot/distConservationWilcoxon.tsv",
-    col.names=TRUE, row.names=FALSE)
-
-
-
-# plot.all()
+  plot.all()
 }
+
