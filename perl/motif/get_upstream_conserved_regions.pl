efeefdbd52d50ce9690052238ba2d5f9a9e3b40c
Josh Burdick <jburdick@mail.med.upenn.edu>
2014-06-23 11:44:27 -0400

    Changed input file to be a command-line arg.

--- perl/motif/get_upstream_conserved_regions.pl
+++ perl/motif/get_upstream_conserved_regions.pl
@@ -4,10 +4,12 @@
 #
 # Note that Bio::DB::BigWig can be somewhat painful to install.
 #
-# Input (from stdin): regions to get conserved portions of
+# Args:
+#   cutoff - the conservation cutoff to use
+#   regions - BED file of regions to compute conservation of
 # Output (to stdout): just the portions with at least that
 #   amount of conservation.
-# Note that if input regions overlap, then output regions may as well.
+# Note that if input regions overlap, then output regions will also.
 
 # use strict;
 
@@ -15,25 +17,26 @@ use Bio::DB::BigFile;
 use Bio::DB::BigFile::Constants;
 use Bio::DB::Fasta;
 
-my $cutoff = 0.5;
+my $cutoff = $ARGV[0];
 
-my $min_size = 20;
+my $min_size = 10;
 
 # the conservation track to use
 my $wig = Bio::DB::BigFile->bigWigFileOpen(
   "/murrlab/seq/igv/conservation/ce10.phastCons7way.bw");
 
-while (<>) {
+open IN, "<" . $ARGV[1] || die;
+while (<IN>) {
   chomp;
   my($chr, $a, $b, $name, $score, $strand) = split /\t/;
 
   $chr =~ s/chr//;
 
-  write_conserved_regions(0.5, $chr, $a, $b, $name, $score, $strand);
+  write_conserved_regions($cutoff, $chr, $a, $b, $name, $score, $strand);
 }
+close IN;
 
-# Prints FASTA-formatted DNA at a region, masked at some
-# amount of conservation.
+# Prints regions which have at least some amount of conservation.
 # Args:
 #   the fields from a .bed-formatted file
 # Side effects: prints the regions
@@ -44,6 +47,7 @@ sub write_conserved_regions {
 
   # get bins of statistical summary data
   # (this should be per-base, so we just use max)
+  # we compute the regions on the "+" strand
   my $r = $wig->bigWigSummaryArray("chr$chr",$a=>$b,bbiSumMax,$n);
 
   # if there were no results, don't print anything
@@ -55,10 +59,12 @@ sub write_conserved_regions {
   my @conservation = @$r;
 
   # the regions (as a list of regions to print)
-  my @regions = ();
+  my @block_sizes = ();
+  my @block_starts = ();
 
   my $region_start = undef;
 
+  my $count = 0;
   for(my $i=0; $i<$n; $i++) {
 
     # if it's undef, conservation is presumed to be zero
@@ -71,11 +77,13 @@ sub write_conserved_regions {
     }
 
     # if conservation has dropped below the cutoff,
-    # write out the region
+    # write out the region's start and end
     if ($c < $cutoff && defined($region_start)) {
-      if ($i - $region_start >= $min_size) {
-        push @regions, (join "\t",
-          ($chr, $a + $region_start, $a + $i, $name, $score, $strand)) . "\n";
+      my $size = $i - $region_start;
+      if ($size >= $min_size) {
+        $count++;
+        push @block_sizes, $size;
+        push @block_starts, $region_start;
       }
       $region_start = undef;
     }
@@ -83,23 +91,24 @@ sub write_conserved_regions {
 
   # possibly tack on the final region
   if (defined $region_start) {
-    if ($n - $region_start >= $min_size) {
-      push @regions, (join "\t",
-        ($chr, $a + $region_start, $a + $n, $name, $score, $strand)) . "\n";
+    my $size = $n - $region_start;
+    if ($size >= $min_size) {
+      $count++;
+      push @block_sizes, $size;
+      push @block_starts, $region_start;
     }
   }
 
-  # print the regions, if there were any
-  if (@regions > 0) {
-    if ($strand eq "+") {
-      print join "", @regions;
-    }
-    else {
-      print join "", (reverse @regions);
-    }
+  # print the regions as "exons" (if there were any)
+  if (@block_sizes > 0) {
+    my $n = @block_sizes + 0;
+    my $thick_start = $a + $block_starts[0];
+    my $thick_end = $a + $block_starts[$n-1] + $block_sizes[$n-1];
+
+    print join "\t", ($chr, $a, $b, $name, $score, $strand,
+      $thick_start, $thick_end, "0,0,0", $n,
+      join (",", @block_sizes), join (",", @block_starts));
+    print "\n";
   }
 }
-
-
-
 
