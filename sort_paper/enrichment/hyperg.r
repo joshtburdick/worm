925b9a9ff3441cd603c7b42f6f9dfd71c8e465ee
Josh Burdick <jburdick@jburdick-ThinkPad-X200.(none)>
2014-07-27 12:30:23 -0400

    About to try adding a faster version of this.

--- sort_paper/enrichment/hyperg.r
+++ sort_paper/enrichment/hyperg.r
@@ -15,14 +15,15 @@ hyperg.test.enrichment = function(g, group, num.genes) {
   # count genes in group
   num.in.group = length(intersect(g, group))
 
-# was:
-#  p = phyper(length(g) - num.in.group,
-#    num.genes - length(group), length(group), length(g),
-#    lower.tail=TRUE)
-# new version, based on
-# http://stackoverflow.com/questions/8382806/r-hypergeometric-test-phyper
-  p = 1-phyper(num.in.group-1, length(group),
-    num.genes-length(group), length(g))
+  # new version, based on
+  # http://stackoverflow.com/questions/8382806/r-hypergeometric-test-phyper
+  # p = 1-phyper(num.in.group-1, length(group),
+  #    num.genes-length(group), length(g))
+
+  # ...except, for reasons of numerical stability (not
+  # computing "1 - 0.999999999...whatever"), using this instead
+  p = phyper(num.in.group-1, length(group),
+    num.genes-length(group), length(g), lower.tail=FALSE)
 
   list(p = p, num.intersect = length(intersect(g, group)),
     num.in.group = length(group),
@@ -40,7 +41,6 @@ hyperg.test.groups = function(groups, gene.list, num.genes) {
   r = NULL
 
   for(gr in unique(groups$group)) {
-# cat(gr, "")
     g1 = groups[ groups$group==gr, ]
     p = hyperg.test.enrichment(gene.list, g1$gene, num.genes)
     r = rbind(r, data.frame(group=gr, group.name=g1$group.name[1],
@@ -48,7 +48,37 @@ hyperg.test.groups = function(groups, gene.list, num.genes) {
       num.in.group = p$num.in.group, p.corr=NA, genes = p$genes))
   }
 
-  r$p.corr = p.adjust(r$p, method="BH")
   r
 }
+
+# Tests for enrichment of any of several lists of genes,
+# in a large number of groups of genes.
+# Args:
+#   groups - a data frame of, e.g., anatomy terms, with
+#     columns "gene", "group", and "group.name"
+#   gene.set - a data.frame with columns "gene" and "set"
+#     (e.g. cluster)
+#   num.genes - the assumed total number of genes
+# Returns: a data frame of results
+# XXX somewhat slow
+hyperg.test.groups.many = function(groups, gene.set, num.genes) {
+  gene.set = gene.set[ !is.na(gene.set$set) , ]
+
+  r = NULL
+
+  for(s in sort(unique(gene.set$set))) {
+    write.status(s)
+    g = gene.set[ gene.set$set==s, "gene" ]
+    r1 = hyperg.test.groups(groups, g, num.genes)
+    r = rbind(r, data.frame(set = s, r1))
+  }
+
+  r$p.corr = p.adjust(r$p, method="fdr")
+  r = r[ order(r$p.corr, r$p) , ]
+  r
+}
+
+
+
+
 
