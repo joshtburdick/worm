50fd7cd08304b3e7635b3c3554cc44d79d5b71a4
Josh Burdick <jburdick@keyfitz.org>
2014-08-23 14:41:40 -0400

    Writes .svgz files instead (which avoids crashing Nautilus, yay.)

--- tf/motif/plot/motifSvg.r
+++ tf/motif/plot/motifSvg.r
@@ -1,10 +1,11 @@
 # Creates SVG files of each motif.
 # FIXME: possibly make these smaller?
 
-library("seqLogo")
-library("MotIV")
+# library("seqLogo")
+# library("MotIV")
 
 source("git/utils.r")
+source("git/plot/seqLogoSVG.r")
 
 load("git/tf/motif/meme.format.pwm.Rdata")
 
@@ -23,7 +24,7 @@ known.motifs.small =
 #   pwm - a position-weight matrix
 #   output.file - SVG file to write
 # Side effects: writes a file
-write.motif.svg = function(pwm, output.file) {
+write.motif.svg.orig = function(pwm, output.file) {
   svg(output.file, width=60, height=10)
   par(mar=c(0,0,0,0))
   seqLogo(pwm, xaxis=FALSE, yaxis=FALSE, xfontsize=0, yfontsize=0)
@@ -34,9 +35,11 @@ write.motif.svg = function(pwm, output.file) {
 output.dir = "git/tf/motif/plot/motifSvg/"
 system(paste("mkdir -p", output.dir))
 
-for (m in intersect(rownames(motif.filter), known.motifs)) {   #  known.motifs.small) {
+# for (m in "GATA3_DBD") {
+for (m in intersect(rownames(motif.filter), known.motifs)) {
+
   write.status(m)
-  write.motif.svg(meme.format.pwm[[m]],
-    paste0(output.dir, "/", m, ".svg"))
+  write.motif.svgz(meme.format.pwm[[m]],
+    paste0(output.dir, "/", m, ".svgz"))
 }
 
