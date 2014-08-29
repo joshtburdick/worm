2e143235490959f9cbede28aeedc3897e3f6c4ce
Josh Burdick <jburdick@mail.med.upenn.edu>
2014-08-29 14:06:51 -0400

    Now much more hypothetical; shows several sample genes, and their expression.

--- sort_paper/plot/figure/figure1.r
+++ sort_paper/plot/figure/figure1.r
@@ -11,74 +11,78 @@ source("git/plot/label_panel.r")
 # font size for panel labels
 gp = gpar(fontsize=12, col="black")
 
-# Draws a scatterplot, coloring genes which are far away from
-# a threshold.
-draw.scatter.diag.threshold.1 =
-    function(x, y, x.name, y.name, cutoff = 2) {
-  par(mar=c(4,4,3,3)+0.1)
-  lim = c(min(x,y), max(x, y))
-  main = expression("expression, log"[2](3+coverage))
+lineage.to.plot = "ABplpa"
+cells = get.node.names(get.lineage.by.name(lin, lineage.to.plot), 100)
+cells = colnames(m.unnormalized)[ colnames(m.unnormalized) %in% cells ]
 
-  colors = ifelse(y-x > cutoff, "#80000080",
-    ifelse(y-x < -cutoff, "#00008080", "#00000080"))
+m1 = m.unnormalized[ c("ceh-27", "hlh-16", "unc-130", "irx-1"), cells ]
 
-  par(font.axis=5)
-  plot(x, y, xlab=x.name, ylab=y.name,
-    main = main, xlim=lim, ylim=lim, col=colors,
-    pch=183, font=5, xaxt="n", yaxt="n",
-    cex=0.8, cex.axis=1, cex.lab=1.2, cex.main=1.2)
-  axis(1, font=1)
+# Plots a lineage in a particular color.
+plot.lineage.small = function(gene, main, hue) {
+
+#  par(mar=c(4,4,3,3)+0.1)
+  par(mar=c(0,0,0,0))
+  a = hsv(hue, 1, 0.8 * m.unnormalized[gene,])
+
+  names(a) = lin.node.names
+
+  plot.segments.per.cell(a, main, root="ABplpa", times=c(50,350),
+      lwd=6, yaxt="n")
   axis(2, font=1)
-  abline(0,1, lwd=2, col="#00000040")
-  abline(2,1, lwd=2, col="#80000040")
-  abline(-2,1, lwd=2, col="#00008040")
+#  mtext("Time (minutes)", side=2, line=2, cex=0.7)
+
 }
 
 pdf("git/sort_paper/plot/figure/figure1.pdf",
-  width=10, height=10)
+  width=8, height=8)
 
-layout(matrix(c(1:6), nrow=3, byrow=TRUE),
-  widths=c(2,1), heights=c(1,1,1))
+layout(matrix(c(1:10), nrow=5),
+  widths=c(2,1), heights=c(1,1,1,1,2))
 
-par(mar=c(4,4,3,3)+0.1)
-a = rgb(m.unnormalized["pha-4",], 0, 0)
-names(a) = lin.node.names
-plot.segments.per.cell(a, "pha-4 expression", root="P0", times=c(0,300),
-    lwd=1.5, yaxt="n")
-axis(2, font=1)
-mtext("Time (minutes)", side=2, line=2, cex=0.7)
-label.panel("a)", gp=gp)
+# plot the trees
+plot.lineage.small("ceh-27", "", 0)
+plot.lineage.small("hlh-16", "", 1/6)
+plot.lineage.small("unc-130", "", 2/6)
+plot.lineage.small("irx-1", "", 3/6)
 
-draw.scatter.diag.threshold.1(r[,"pha-4 9/1 (-)"], r[,"pha-4 9/1 (+)"],
-  "pha-4 9/1 (-)", "pha-4 9/1 (+)", 2)
-label.panel("d)", gp=gp)
+# made-up expression
+x = matrix(0, nrow=ncol(m1), ncol=5)
+x[1:20, 1:2] = 1
+x[5:8, 3] = 1
+x[34:49, 4:5] = 1
 
-par(mar=c(4,4,3,3)+0.1)
-a = rgb(m.unnormalized["ttx-3",], 0, 0)
-names(a) = lin.node.names
-plot.segments.per.cell(a, "ttx-3 expression", root="P0", times=c(0,300),
-    lwd=1.5, yaxt="n")
-axis(2, font=1)
-mtext("Time (minutes)", side=2, line=2, cex=0.7)
-label.panel("b)", gp=gp)
+x = x+rnorm(prod(dim(x)), mean=0, sd=0.2)
+x[x < 0] = 0
+x[x > 1] = 1
 
-draw.scatter.diag.threshold.1(r[,"ttx-3 (-)"], r[,"ttx-3 (+)"],
-  "ttx-3 (-)", "ttx-3 (+)", 2)
-label.panel("e)", gp=gp)
+par(mar=c(0,0,1,0) + 0.1)
+image(x, col=hsv(0,0,128:0/128), useRaster=TRUE,
+  xaxt="n", yaxt="n")
 
-par(mar=c(4,4,3,3)+0.1)
-a = rgb(m.unnormalized["hlh-16",], m.unnormalized["ceh-6",], 0)
-names(a) = lin.node.names
-plot.segments.per.cell(a, "hlh-16 and ceh-6 expression", root="ABp", times=c(0,300),
-    lwd=2.5, yaxt="n")
-axis(2, font=1)
-mtext("Time (minutes)", side=2, line=2, cex=0.7)
-label.panel("c)", gp=gp)
+# utility to color a slice of the heatmap
+color.slice = function(y1, y2, hue) {
+  rect(-1, (y1-0.5)/4, 2, (y2-0.5)/4, col=hsv(hue, 1, 1, 0.1), border=NA)
+}
 
-draw.scatter.diag.threshold.1(
-  r[,"ceh-6 (-) hlh-16 (-)"], r[,"ceh-6 (+) hlh-16 (+)"],
-  "ceh-6 (-) hlh-16 (-)", "ceh-6 (+) hlh-16 (+)", 2)
-label.panel("f)", gp=gp)
+color.slice(0, 2, 0.1)
+color.slice(2, 3, 0.3)
+color.slice(3, 5, 0.7)
+
+plot.new()
+plot.new()
+plot.new()
+plot.new()
+
+x.p = m1 %*% x
+x.n = (1-m1) %*% x
+x.f = x.p / (x.p + x.n)
+
+par(mar=c(0,1,1,0) + 0.1)
+image(x.f, col=hsv(0,0,128:0/128), useRaster=TRUE,
+  xaxt="n", yaxt="n")
+color.slice(0, 2, 0.1)
+color.slice(2, 3, 0.3)
+color.slice(3, 5, 0.7)
 
 dev.off()
 
