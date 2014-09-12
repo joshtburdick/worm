f1b8126622f369818ac4e120d977da49d56527f7
Josh Burdick <jburdick@mail.med.upenn.edu>
2014-09-12 16:45:00 -0400

    Added phenotypes, and added many links to WormBase.

--- sort_paper/plot/web/clusters.r
+++ sort_paper/plot/web/clusters.r
@@ -3,6 +3,8 @@
 library("hwriter")
 
 source("git/utils.r")
+source("git/tf/motif/motifName.r")
+source("git/plot/web/html.r")
 
 clustering.dir = "git/cluster/hierarchical/"
 output.dir = "git/sort_paper/plot/web/clusters"
@@ -10,12 +12,16 @@ output.dir = "git/sort_paper/plot/web/clusters"
 # which clustering to write out
 clustering.name = "hier.300.clusters"
 
-if (FALSE) {
+# WormBase gene IDs (for linking to there)
+wb.gene.id = read.csv(gzfile("data/wormbase/geneIDs.WS224.csv.gz"),
+  header=FALSE, as.is=TRUE)
+colnames(wb.gene.id) = c("wb.gene", "name", "clone.id")
+
 go.enriched = read.table(
   "git/sort_paper/enrichment/geneOntology/hier.300.clusters.tsv",
   sep="\t", quote="", header=TRUE, as.is=TRUE)
 go.enriched = go.enriched[ go.enriched$Count >= 2 , ]
-}
+
 system(paste0("mkdir -p ", output.dir, "/", clustering.name))
 
 # read in data to cluster
@@ -33,14 +39,38 @@ ao.enriched = read.tsv(paste0(
 cluster.enriched = read.tsv(paste0(
   "git/sort_paper/enrichment/summary/wormbaseCluster/",
   clustering.name, ".tsv"))
+phenotype.enriched = read.tsv(paste0(
+  "git/sort_paper/enrichment/summary/phenotype/",
+  clustering.name, ".tsv"))
 
 # enriched motif and ChIP signals
 motif.enriched = read.tsv(paste0("git/sort_paper/tf/summary/motif/",
   clustering.name, ".tsv"))
 chip.enriched = read.tsv(paste0("git/sort_paper/tf/summary/chip/",
   clustering.name, ".tsv"))
+colnames(chip.enriched)[1] = "experiment"
+chip.enriched$factor = sub("_.*$", "", chip.enriched$experiment)
 
+# information about orthologs
+motif.ortholog = read.table("git/tf/motif.ortholog.2.tsv",
+  as.is=TRUE, header=TRUE)
+motif.filter = read.tsv("git/tf/motif/motifFilter.tsv")
+# add representative motif name, from the motif clustering
+motif.ortholog$canonical.motif =
+  motif.filter[ motif.ortholog$motif, "canonical.name" ]
+motif.ortholog = motif.ortholog[ !is.na(motif.ortholog$canonical.motif) , ]
+# for each motif, list of potential orthologs
+orthologs.by.motif = by(motif.ortholog$gene, motif.ortholog$canonical.motif,
+  function(x) {
+    x = as.character(x)
+    nhrs = grep("nhr", x)
+    if (length(x) >= 5 && length(nhrs) >= 5) {
+      return(unique(c(grep("nhr", x, invert=TRUE, value=TRUE), paste(length(nhrs), "NHRs"))))
+    }
 
+    return(unique(as.character(x))) 
+  }
+)
 
 # Utility to convert numbers to colors.
 # Args:
@@ -60,7 +90,7 @@ number.to.color.hsv = function(neg.hue, pos.hue, max.val, x) {
 td.color = function(x) {
   # FIXME use HTML5
   paste0("<td title=\"", signif(x,3), "\" bgcolor=\"",
-    number.to.color.hsv(1/6, 4/6, 2, x),
+    number.to.color.hsv(194/360, 60/360, 2, x),
     "\" > </td>\n")
 }
 
@@ -69,56 +99,108 @@ td.color = function(x) {
 rotate.text = function(s) {
   paste0("<div class=\"vertical-text\">", s, "</div>", collapse="")
 
-
 }
 
 # HTML for a table of enriched anatomy terms / clusters.
-enriched.table = function(name, a) {
+enriched.table = function(a, link.f=NULL) {
   if (nrow(a) == 0)
-    return("")
+    return("none found")
 
-  a = a[ , c(2,3,4,6,7,8) ]
+#  a = a[ , c(2,3,4,6,7,8) ]
+
+  if (!is.null(link.f)) {
+    a[,"group.name"] =
+      html.link(a[,"group.name"], link.f(a[,"group"]))
+  }
+  a = a[ , c("group.name", "num.intersect", "enrichment", "p.corr") ]
   a = a[ order(a$p.corr) , ]
-  a[ , c(5,6) ] = signif(a[ , c(5,6) ], 2)
+
+#  a[ , c(5,6) ] = signif(a[ , c(5,6) ], 2)
 
   # XXX ideally hwrite should write the first line in <th>
-  colnames(a) = sapply(c("Group", "Group name", "Group size",
-    "Number in cluster", "p", "FDR corrected p"),
+  colnames(a) = sapply(c("Group name",
+    "Number in cluster", "Enrichment", "FDR corrected p"),
     function(a) paste0("<b>", a, "</b>"))
 
-  paste0("<h3>", name, "</h3>", hwrite(a, row.names=FALSE))
+  hwrite(a, row.names=FALSE)
 }
 
 # HTML for a table of enriched GO terms.
 go.enriched.table = function(a) {
+  if (nrow(a) == 0)
+    return("none found")
+
   a = a[ order(a$p.corr) , ]
-  results = data.frame("GO term" = a$Term, "GO ID" = a$GOID,
-    "Number of genes in cluster" = paste0(a$Count, "/", a$Size),
+  results = data.frame(   #"GO term" = a$Term, "GO ID" = a$GOID,
+    "GO term" = html.link(a$Term,
+      paste0("http://www.wormbase.org/db/get?name=",
+        a$GOID, ";class=go_term")),
+    "Number of genes" = a$Count,
+#    "Number of genes in cluster" = paste0(a$Count, "/", a$Size),
     "FDR-corrected p-value" = signif(a$p.corr, 2), check.names=FALSE)
+  colnames(results) = sapply(colnames(results),
+    function(a) paste0("<b>", a, "</b>"))
   hwrite(results)
 }
 
-# HTML for a table of enriched motifs / ChIP terms.
-motif.chip.table = function(a, motif.or.chip) {
+# HTML for a table of enriched motifs.
+motif.table = function(a) {
+  if (nrow(a) == 0)
+    return("none found")
+
   a = a[ order(a$p.corr) , ]
+
+  a$logo = sapply(a$motif,
+    function(m) paste0("<img src=\"../motifSvg/", m, ".svg\" ",
+      "alt=\"Logo for motif ", m, "\" width=\"120px\" height=\"27px\">"))
+  a$enrich = round(a$enrich, 2)
+  a$p.corr = signif(a$p.corr, 2)
+  a$ortholog = sapply(a$motif,
+    function(m) {
+      g = orthologs.by.motif[[ m ]]
+      # if there are many TFs, show the entire list as alt text
+      if (length(g) > 21) {
+        all.g = paste(g, collapse=" ")
+        g = c(g[1:20], paste("and", length(g)-20, "others"))
+
+        paste(paste(g, collapse=" "),
+          paste0("<br><span title=\"", all.g, "\">[full list]</span>"))
+      }
+      else {
+        paste(g, collapse=" ")
+      }
+    })
+
+  a = a[ , c("motif", "logo", "ortholog", "motifs.cluster", "enrich", "p.corr") ]
+
+  # use human-readable motif names
+  a$motif = motif.name[ a$motif ]
 
   # XXX ideally hwrite should write the first line in <th>
   colnames(a) = sapply(
-    c(ifelse(motif.or.chip=="motif", "Motif", "ChIP experiment"),
-    "Group", "Upstream distance (kb)", "Conservation",
-    "Motif score", "Motifs in cluster", "Motifs in background",
-    "Sequence in cluster (bp)",
-    "Sequence in background (bp)", "Enrichment", "Chi-squared value",
-    "p", "FDR corrected p"),
+    c("Motif", "Logo", "Possible orthologs",
+      "Number of motifs in cluster", "Enrichment", "FDR corrected p"),
     function(a) paste0("<b>", a, "</b>"))
 
-  a[ , c(10:11) ] = round(a[,c(10:11)], 1)
-  a[ , c(12:13) ] = signif(a[,c(12:13)], 2)
+  hwrite(a, row.names=FALSE)
+}
 
-  if (motif.or.chip == "chip") {
-    a = a[ , -5 ]
-  }
-  a = a[ , -2 ]
+
+# HTML for a table of ChIP terms.
+chip.table = function(a) {
+  if (nrow(a) == 0)
+    return("none found")
+
+  a = a[ order(a$p.corr) , ]
+
+  a = a[ , c("factor", "experiment", "motifs.cluster", "enrich", "p.corr") ]
+
+  a$enrich = round(a$enrich, 2)
+  a$p.corr = signif(a$p.corr, 2)
+  colnames(a) = sapply(
+    c("Gene", "Experiment", "Number of upstream peaks", "Enrichment",
+    "FDR corrected p"),
+    function(a) paste0("<b>", a, "</b>"))
 
   hwrite(a, row.names=FALSE)
 }
@@ -126,7 +208,19 @@ motif.chip.table = function(a, motif.or.chip) {
 # HTML for a formatted table of expression numbers.
 expr.table = function(x, cl) {
 
-  # FIXME this should include links
+# add link out to WormBase
+#  x[,3] = sapply(x[,3],
+#    function(g) html.link(g,
+#      paste0("http://www.wormbase.org/db/get?name=", g, ";class=Gene")))
+  wb.ids = x[,3]
+  i = x[,3] %in% wb.gene.id[,3]
+  wb.ids[i] = wb.gene.id[match(wb.ids[i], wb.gene.id[,3]), 1]  
+  i = x[,3] %in% wb.gene.id[,2]
+  wb.ids[i] = wb.gene.id[match(wb.ids[i], wb.gene.id[,2]), 1]  
+
+  x[,3] = html.link(x[,3],
+    paste0("http://www.wormbase.org/db/get?name=", wb.ids, ";class=Gene"))
+
   for(j in 1:11) {
     x[,j] = sapply(x[,j], function(a) paste0("<td>", a, "</td>"))
   }
@@ -137,7 +231,7 @@ expr.table = function(x, cl) {
   }
   x = x[ , c(12:51, 3, 4) ]
 
-  col.names = colnames(x)
+   col.names = colnames(x)
   col.names[1:40] = sapply(col.names[1:40], rotate.text)
 
   table.header = paste(c("<thead><tr>",
@@ -150,6 +244,21 @@ expr.table = function(x, cl) {
   table.rows = paste0("<tbody class=expr-tbody>", paste(table.rows.1, collapse=""), "</tbody>")
 
   paste0("<table class=expr-table>", table.header, table.rows, "</table>")
+}
+
+# JavaScript for "go to next page."
+next.page.js = function(cl) {
+  cl.next = cl + 1
+  if (cl.next == 301) {
+    cl.next = 1
+  }
+
+  paste0(
+  "function keyHandler(event) {",
+  " if (event.keyCode == 32) {",
+  "   window.location = \"", cl.next, ".html\"; ",
+  " }",
+  "}")
 }
 
 # Writes out one cluster.
@@ -165,28 +274,42 @@ write.cluster = function(x, cl) {
 
   h = paste0(
     "<h2>Cluster ", cl, "</h2>",
-    "<h3>Anatomy terms</h3>",
-      enriched.table("Anatomy terms enriched",
-        ao.enriched[ ao.enriched$cluster==cl , ]),
-    "<h3>GO terms enriched (FIXME)</h3>",
-      enriched.table("Expression clusters enriched",
-        cluster.enriched[ cluster.enriched$cluster==cl , ]),
-
+    "<h3>Expression</h3>", expr.table(x1),
+    "<h3>Anatomy terms enriched</h3>",
+    enriched.table(ao.enriched[ ao.enriched$cluster==cl , ],
+      function(ao)
+        paste0("http://www.wormbase.org/db/get?name=",
+        ao, ";class=Anatomy_term")),
+    "<h3>Phenotypes enriched</h3>",
+    enriched.table(phenotype.enriched[ phenotype.enriched$cluster==cl , ],
+      function(a)
+        paste0("http://www.wormbase.org/db/get?name=",
+        a, ";class=Phenotype")),
+    "<h3>GO terms enriched</h3>",
+    go.enriched.table(go.enriched[ go.enriched$cluster==cl , ]),
+    "<h3>Expression clusters enriched</h3>",
+    enriched.table(cluster.enriched[ cluster.enriched$cluster==cl , ],
+      function(ao)
+        paste0("http://www.wormbase.org/db/get?name=",
+        ao, ";class=Expression_cluster")),
     "<h3>Motifs enriched</h3>",
-    motif.chip.table(motif.enriched[motif.enriched$group == cl,], "motif"),
+    motif.table(motif.enriched[motif.enriched$group == cl,]),
     "<h3>ChIP peaks enriched</h3>",
-    motif.chip.table(chip.enriched[chip.enriched$group == cl,], "chip"),
-    "<h3>Expression clusters</h3>", expr.table(x1)
+    chip.table(chip.enriched[chip.enriched$group == cl,])
   )
 
   s = paste0("<!DOCTYPE html>",
     "<html><head><title>", "Cluster ", cl, "</title>",
-    "<style type=\"text/css\">", css, "</style>", "</head>",
-    "<body>", h, "</body></html>")
+    "<style type=\"text/css\">", css, "</style>",
+    "<script>", next.page.js(cl), "</script>", "</head>",
+    "<body onkeyup=\"keyHandler(event)\">",
+    h, "</body></html>")
   cat(s, file=paste0(output.dir, "/", clustering.name, "/",
     cl, ".html"))
 }
 
+# for(cl in c(1,2,3,52,286)) {
 for(cl in sort(unique(x$Cluster))) {
   write.cluster(x, cl)
 }
+
