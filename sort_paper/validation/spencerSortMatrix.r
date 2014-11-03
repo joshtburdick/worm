8391575f8e817d34d6530ef6d759cc0cea66f631
Josh Burdick <jburdick@mail.med.upenn.edu>
2014-11-03 12:45:01 -0500

    Writes (and draws) a sort matrix corresponding to Spencer et al.

--- sort_paper/validation/spencerSortMatrix.r
+++ sort_paper/validation/spencerSortMatrix.r
@@ -3,6 +3,8 @@
 
 library(Matrix)
 
+source("git/utils.r")
+
 load("R/lineage/tree_utils.Rdata")
 load("R/lineage/cell.names.Rdata")
 load("git/data/wormbase/anatomy.ontology.group.Rdata")
@@ -11,6 +13,9 @@ ao.name.to.term = c(tapply(names(ao.term.to.name), ao.term.to.name, function(a) 
 
 # tissue.by.lineage = read.table("data/worm/TissuesPerCell.tsv",
 #   sep="\t", header=TRUE, quote="", row.names=1, as.is=TRUE)
+# nervous system cells, obtained a different way
+# nerve.cells = rownames(tissue.by.lineage)[
+#   tissue.by.lineage[,"Tissue"] %in% c("Glia","Nervous") ]
 
 # Given the name of something, gets the associated cell names.
 # Args:
@@ -44,24 +49,58 @@ spencer.ao.terms = c("BAG",
   "germline precursor cell",
   "AVA",
   "GABAergic neuron",
-  "body wall muscle cell",       # or "body wall musculature"
+  "body wall muscle cell",     # or "body wall musculature"
   "coelomocyte",
   "dopaminergic neuron",
-  "intestinal cell",       # or "intestine"  
+  "intestinal cell",     # or "intestine"  
   "nervous system",
   "hypodermis",
   "AVE",
   "pharyngeal muscle cell")
 
+spencer.m = t(sapply(spencer.ao.terms, anatomy.term.to.lineage))
+
 # A-class motor neurons (unc-4 also is on elsewhere, esp. later)
-# "DA neuron", "VA neuron",
+spencer.m = rbind(spencer.m, "A-class motor.neuron" =
+  anatomy.term.to.lineage("DA neuron") | anatomy.term.to.lineage("VA neuron"))
 
-# nervous system cells, obtained a different way
-nerve.cells = rownames(tissue.by.lineage)[
-  tissue.by.lineage[,"Tissue"] %in% c("Glia","Nervous") ]
+# Adds in cells, both of whose daughter cells are on.
+# Args:
+#   x - a boolean vector, indexed by cell name
+# Returns: x, with parent lineages filled in.
+fill.in.lineage = function(x) {
+  x.names = names(x)[x]
+  p1 = lin.triples[ lin.triples$daughter.1 %in% x.names , "parent" ]
+  p2 = lin.triples[ lin.triples$daughter.2 %in% x.names , "parent" ]
+  p = intersect(p1, p2)
+  x1 = lin.node.names %in% union(x.names, p)
+  names(x1) = lin.node.names
+  if (all(x == x1))
+    x
+  else
+    fill.in.lineage(x1)
+}
 
+# do this for all these sets of cells
+spencer.m = t(apply(spencer.m, 1, fill.in.lineage))
 
+spencer.m = spencer.m[ order(apply(spencer.m, 1, sum)) , ]
 
+write.tsv(spencer.m, "git/sort_paper/validation/spencerSortMatrix.tsv")
 
+# pdf("git/sort_paper/validation/spencerSortMatrix.pdf", width=10, height=5)
+png("git/sort_paper/validation/spencerSortMatrix.png", width=1000, height=500)
 
+par(mar=c(0.1,11,1,0.1)+0.1)
+
+image(t(spencer.m),
+  useRaster=TRUE,
+  xaxt="n", yaxt="n", zlim=c(0,1),
+  col=hsv(1/6, 1, 0:128/128))
+
+axis(2, at=c(0:12)/12,
+  labels=rownames(spencer.m),
+  tick=FALSE, las=2)
+
+dev.off()
 
