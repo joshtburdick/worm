cf1420dbca968ab53277776393c979b29e60c453
Josh Burdick <jburdick@mail.med.upenn.edu>
2014-11-05 16:01:57 -0500

    Init.

--- sort_paper/FACS/timing/exprByTime.r
+++ sort_paper/FACS/timing/exprByTime.r
@@ -4,6 +4,8 @@
 source("git/utils.r")
 source("git/data/name_convert.r")
 
+source("git/sort_paper/FACS/timing/proportionOnFromImaging.r")
+
 # genes expressed at particular times
 gene.expr.time =
   read.tsv("git/sort_paper/FACS/timing/geneExprTime.tsv")
@@ -27,16 +29,21 @@ s1 = as.matrix(s1[ , 4:33 ])
 singlet.average = (r[,"cnd-1 singlets"] + r[,"pha-4 singlets"]) / 2
 
 # Plots profile of enrichment.
-plot.time.profile = function(a, b, main, ylab="enrichment") {
+plot.time.profile = function(a, b, main, ylab="Enrichment") {
   write.status(main)
   plot(td1[,"mean"],
     a[ rownames(td1) ] - b[ rownames(td1) ],
-    main = main, xlab = "time", ylab=ylab,
-    pch=20, cex=0.5, col="#00000080")
+    main = main, xlab = "Time (minutes)", ylab=ylab,
+    ylim=c(-5,5), xaxt="n", yaxt="n",
+    pch=183, font=5, cex=1, col="#00000080")
+  axis(1)
+  axis(2)
   abline(h = 0, col="#606060")
 }
 
-if (TRUE) {
+# Plots expression versus time for all the sorting experiments,
+# and experiments from Spencer et al.
+plot.all.expr.by.time = function() {
   pdf("git/sort_paper/FACS/timing/exprByTime.pdf",
     width=7.5, height=10)
   par(mfrow=c(4,2))
@@ -45,9 +52,16 @@ if (TRUE) {
   samples = sort(grep("ceh-6.*hlh-16", samples, value=TRUE, invert=TRUE))
 
   for(s in samples) {
-
     plot.time.profile(r[,s], singlet.average,
       paste(s, "vs singlets"))
+  }
+
+  # positive vs. negative
+  for(s in samples) {
+    neg = sub("\\(\\+\\)", "\\(\\-\\)", s)
+    if (neg %in% colnames(r)) {
+      plot.time.profile(r[,s], r[,neg], paste(s, "vs", neg))
+    }
   }
 
   # Spencer data
@@ -78,6 +92,68 @@ if (TRUE) {
   dev.off()
 }
 
+# simple smoothing
+sm = function(p) {
+  m = loess(proportion.on ~ t, p, alpha = 0.1)
+  p$proportion.on = predict(m, p)
+  p$proportion.on[ p$proportion.on < 0 ] = 0
+  p
+}
 
+# Plots the expression profile, and how many cells were expressing
+# at particular times, for a few genes.
+#plot.expr.and.percent.on = function() {
 
+on.off.c = "#0000ffb0"
+
+  # cnd-1
+  pdf("git/sort_paper/FACS/timing/exprByTime_cnd1.pdf",
+    width=8, height=4.5)
+  par(mar=c(5,4,4,4) + 0.1)
+
+  plot.time.profile(r[,"cnd-1 8/19 (+)"], singlet.average, "")
+
+  p = sm(get.proportion.on("SCD20110731_RW10434_L1_smooth", 50, 1000))
+  par(new=TRUE)
+  ymax=signif(max(p$proportion.on), 1)
+  p = p[p$t <= 400,]   # only show part including data
+  plot(p$t, p$proportion.on, type="l", lwd=3, col=on.off.c,
+    xlim=range(td1$mean), ylim=c(-ymax, ymax), xaxt="n", yaxt="n",
+    xlab="", ylab="")
+  axis(4, at = c(0, 0.5, 1) * ymax, col=on.off.c, col.axis=on.off.c)
+  mtext("Proportion of expressing cells", col=on.off.c, side=4, line=2)
+  dev.off()
+
+  # pros-1
+  pdf("git/sort_paper/FACS/timing/exprByTime_pros1.pdf",
+    width=8, height=4.5)
+  par(mar=c(5,4,4,4) + 0.1)
+
+  plot.time.profile(r[,"pros-1 (+)"], singlet.average, "")
+
+  p = sm(get.proportion.on("SCD20120426_JIM122_L2_smooth", 50, 1000))
+  par(new=TRUE)
+  ymax=signif(max(p$proportion.on), 1)
+  p = p[p$t <= 400,]   # XXX only show part including data
+  plot(p$t, p$proportion.on, type="l", lwd=3, col=on.off.c,
+    xlim=range(td1$mean), ylim=c(-ymax, ymax), xaxt="n", yaxt="n",
+    xlab="", ylab="")
+  axis(4, at = c(0, 0.5, 1) * ymax, col=on.off.c, col.axis=on.off.c)
+  mtext("Proportion of expressing cells", col=on.off.c, side=4, line=2)
+  dev.off()
+
+if (FALSE) {
+  pdf("git/sort_paper/FACS/timing/proportionOn.pdf",
+    width=8, height=4.5)
+  par(mar=c(5,4,4,4) + 0.1)
+
+  p = sm(get.proportion.on("SCD20110726_RW10713_L1_smooth", 50, 1000))
+  plot(p$t, p$proportion.on, type="l")
+
+  dev.off()
+}
+
+#}
+
+# plot.all.expr.by.time()
 
