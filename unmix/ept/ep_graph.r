5b916ea7a8504727f68cb1bbc69006bfdf9b3191
Josh Burdick <jburdick@mail.med.upenn.edu>
2014-12-03 10:58:59 -0500

    Still tweaking how to represent stuff.

--- unmix/ept/ep_graph.r
+++ unmix/ept/ep_graph.r
@@ -7,86 +7,110 @@
 
 # Makes a model.
 # Args:
-#   vars - list of variables
-#   factors - list of factors
+#   vars - list of variables (this is just a list of
+#     exponential-family variables)
+#   factors - list of factors (see make.factors())
 make.model = function(vars, factors) {
   # XXX this admittedly looks pretty silly
   r = list(vars = vars, factors = factors)
-#  attr(r, "class") = "ep.model"
+  class(x) = "epModel"
   r
 }
 
+# XXX this is deprecated
 # A variable is a list with at least these elements:
 #   b - the total messages to this variable (as a matrix)
 #   observed - TRUE iff this variable is observed
 #   log.evidence - function which computes log-evidence
-
 # Makes a variable.
-make.var = function(b, log.evidence) {
+make.var.old = function(b, log.evidence) {
   r = list(b = b, observed = FALSE, log.evidence = log.evidence)
 #  attr(r, "class") = "ep.var"
   r
 }
 
+# Adds a scaled amount of some variable to another variable.
+# Args:
+#   x, y - lists of numeric variables (y should only have
+#     a subset of x's variables)
+#   a - scalar
+# Returns: x + ay
+add.scaled = function(x, y, a) {
+  for(i in names(y))
+    x[[i]] = x[[i]] + a * y[[i]]
+  x
+}
+
 # A factor is a list with:
-#   from.f - list of messages from this factor
-#   update - function which, given messages to this factor,
-#     sends messages from this factor
-#   log.evidence - function which computes log-evidence for this factor
+#   m - list of messages from this factor
+#   f - list of methods, including:
+#     update - function which, given messages to this factor,
+#       computes messages from this factor
+#     logEvidence - function which computes log-evidence for this factor
 
 # Constructs a factor with some set of methods.
 # Args:
 #   x - the variables connected to this factor
-#   f - list containing the "update" and "log.evidence" functions
+#   f - list containing the "update" and "logEvidence" functions
 #     for this factor
 make.factor = function(x, f) {
   # the messages start at the belief, but zeroed out
   z = lapply(x, function(a) 0 * a$b )
   names(z) = names(x)
 
-  list(from.f = z,
-    update = f$update, log.evidence = f$log.evidence)
+  a = list(m = z, f = f)
+#    update = f$update, logEvidence = f$logEvidence)
+#  class(a) = "ep.factor"
+  a
 }
 
-
-# An equality factor (maybe.)
-eq.f = function(log.partition) function(a) {
+# An equality factor.
+eq.f = function(a) {
   a1 = a[[1]]
   a2 = a[[2]]
   
   list(update = list(a2, a1),
-    log.evidence = log.partition(a1+a2) - (log.partition(a1) + log.partition(a2)))
+    logEvidence = logPartition(a1+a2) - (logPartition(a1) + logPartition(a2)))
 }
 
 # Factor which constrains a variable (almost the same thing.)
-# ??? this may be all wrong.
-constrain.f = function(log.partition, x) function(a) {
+# ??? should this care about the message passed to it?
+constrain.f = function(x) function(a) {
   a1 = a[[1]]
 
   list(update = list(x),
-    log.evidence = log.partition(x+a1) - (log.partition(x) + log.partition(a1)))
+    logEvidence = log.partition(x+a1) - (log.partition(x) + log.partition(a1)))
 }
 
-
-
-
-
-
-# FIXME not working
 # Updates a model once, using "parallel" updating.
 # Currently, no convergence testing.
 #   m - a model, as a list with elements "vars" and "factors"
 # Returns: m, after one round of parallel updates
 update.ep = function(m) {
 
+  f1 = list()  
+
+  for(i in 1:length(m$factors)) {
+
+    # message to that factor
+    m.to = m$factors[[i]]$f$
+
+    # compute updated messages
+    m.from = m$factors[[i]]$f$update( m.to )
+
+    f1[[i]] = list(m = m.from, f = m$factors[[i]]$f)
+
+
+  }
+
+
+  # accumulate messages to that factor
+
+
 
   # get messages from each factor
   f1 = sapply(m$factors, function(f)
     for(i in 1:length(f$x)) {
-# cat("factor ", i, "\n")
-# cat("dim(to.f) =", dim(f$to.f[[i]]), "\n")
-# cat("dim(f$x...) =", dim(f$x[[i]]$b), "\n")
-# cat("dim(from.f) =", dim(f$from.f[[i]]), "\n")
         f$to.f[[i]] = f$x[[i]]$b - f$from.f[[i]]
       })
 
@@ -94,9 +118,11 @@ update.ep = function(m) {
   v1 = lapply(m$vars, function(v) { v$b = 0 * v$b; v })
 
   # send messages from each factor
-
+  
 
 
   list(vars = v1, factors = f1)
 }
+
+
 
