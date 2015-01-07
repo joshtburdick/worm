8842a56e7bb858f96e3b42cc80eff1b0773a3cd4
Josh Burdick <jburdick@mail.med.upenn.edu>
2015-01-07 18:34:03 -0500

    Rewritten, so that a model is just a list.

--- unmix/ept/ep_graph.r
+++ unmix/ept/ep_graph.r
@@ -1,17 +1,39 @@
 # Attempt at a more general library for EP.
 # For now, avoiding object-oriented stuff.
 
-# A model is represented by a list with at least:
-#   vars - a named list of variables (the names should
-#     be unique)
-#   factors - a list of factors
-# Each factor has a list consisting of:
-#   m - the messages from the factor
-#   update - function which takes a named list of messages,
-#     and returns a named list of messages.
-#   (later, maybe add log-evidence.)
+# Constructs a model.
+# Args:
+#   vars - a named list of variables. These should be
+#     in terms of "natural parameters", so that adding
+#     them multiplies the actual density functions.
+#     (You may want to initialize these somehow.)
+#   factors - a named list of lists, specifying factors.
+#     Each element of this should have a factor, followed
+#     by the names of the variables on which it depends.
+# Returns: an EP model (represented as a list.)
+#   (The messages are initially flat; I don't know if
+#   there's a reason to alter them, but you can.)
+ep.model = function(vars, factors) {
+  
+  # messages (initially flat)
+  m = NULL
+  for(i in names(factors)) {
+    f1 = factors[[i]]
+    m1 = NULL
+    for(j in 2:length(f1)) {
+      m1[[ f1 [[j]] ]] = 0 * vars[[ f1[[j]] ]]
+    }
+    m[[i]] = m1
+  }
 
+  # factors
+  f = NULL
+  for(i in names(f)) {
+    f[[i]] = factors[[i]][[1]]
+  }
 
+  list(vars = vars, messages = m, factors = f)
+}
 
 # Adds in a message. The variable names in y should
 # be a subset of the names in x.
@@ -28,19 +50,36 @@ message.subtract = function(x, y) {
   y
 }
 
-# Constructs a factor with some set of methods.
-# Args:
-#   x - the variables connected to this factor
-#   f - the "update" message for this factor
-make.factor = function(x, f) {
-  # the messages start at the belief, but zeroed out
-  z = lapply(x, function(y) 0 * y )
-  names(z) = names(x)
+# Updates a model once, using "parallel" updating.
+# Currently, no convergence testing.
+#   m - a model, as constructed above
+# Returns: m, after one round of parallel updates
+update.ep = function(m) {
 
-  a = list(m = z, f = f)
-#    update = f$update, logEvidence = f$logEvidence)
-#  class(a) = "ep.factor"
-  a
+  m1 = list()
+
+  # loop through names of factors
+  for(i in names(m$messages)) {
+
+    # message to that factor
+    m.to = message.subtract(m$vars, m$messages[[i]])
+
+    # compute factor value
+    m.f = m$factors[[i]](m.to)
+
+    # compute message from this factor
+    m1[[i]] = message.subtract(m.f, m$messages[[i]])
+  }
+
+  # clear the variables
+  v1 = lapply(m$vars, function(v) 0 * v)
+
+  # sum messages from each factor
+  for(i in 1:length(f1)) {
+    v1 = message.add(v1, m1[[i]])
+  }
+
+  list(vars = v1, messages = m1, factors = m$factors)
 }
 
 # An equality factor.
@@ -55,40 +94,8 @@ eq.f = function(a) {
 
 # Factor which constrains a variable (almost the same thing.)
 # ??? should this care about the message passed to it?
-constrain.f = function(x) function(a) {
-  a1 = a[[1]]
-
-  list(update = list(x),
-    logEvidence = log.partition(x+a1) - (log.partition(x) + log.partition(a1)))
-}
-
-# Updates a model once, using "parallel" updating.
-# Currently, no convergence testing.
-#   m - a model, as a list with elements "vars" and "factors"
-# Returns: m, after one round of parallel updates
-update.ep = function(m) {
-
-  f1 = list()  
-
-  for(i in 1:length(m$factors)) {
-
-    # message to that factor
-    m.to = message.subtract(m$vars, m$factors[[i]]$m)
-
-    # compute updated messages from this factor
-    m.from = m$factors[[i]]$f$update(m.to)
-
-    f1[[i]] = list(m = m.from, f = m$factors[[i]]$f)
-  }
-
-  # clear the variables
-  v1 = lapply(m$vars, function(v) 0 * v)
-
-  # send messages from each factor
-  for(i in 1:length(f1)) {
-    v1 = message.add(v1, f1[[i]]$m)
-  }
-
-  list(vars = v1, factors = f1)
+constrain.f = function(a) function(x) {
+  x[[1]] = a
+  x
 }
 
