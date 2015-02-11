ca864c1a31bf6cdb7b8a2d725c463f1a2693d6f9
Josh Burdick <jburdick@mail.med.upenn.edu>
2015-02-11 15:50:25 -0500

    About to update these to use new motif enrichment.

---
+++ sort_paper/network/networkStats.r
@@ -0,0 +1,62 @@
+# Computes various network stats.
+
+if (FALSE) {
+load("git/sort_paper/tf/allResults/motif/hier.300.clusters.Rdata")
+motif1.min.p = apply(enrich[,,,,,"p.corr"], c(1,2), min)
+load("git/sort_paper/tf/motif/allResults/hughes_motif/hier.300.clusters.Rdata")
+hughes.min.p = apply(enrich[,,,,,"p.corr"], c(1,2), min) 
+
+motif.min.p = rbind(hughes.min.p, motif1.min.p)
+
+load("git/sort_paper/tf/allResults/chip_incl_HOT/hier.300.clusters.Rdata")
+chip.min.p = apply(enrich[,,,,,"p.corr"], c(1,2), min) 
+}
+
+pdf("git/sort_paper/network/networkStats.pdf",
+  width=10, height=6)
+par(mfrow=c(2,4))
+
+hist(apply(motif.min.p <= 0.05, 1, sum),
+  main="Clusters per motif",
+  xlab="Number of clusters",
+  breaks=50, col="red")
+mtext("p <= 0.05", 3, cex=0.7)
+hist(apply(motif.min.p <= 1e-5, 1, sum),
+  main="Clusters per motif",
+  xlab="Number of clusters",
+  breaks=50, col="red")
+mtext("p <= 1e-5", 3, cex=0.7)
+hist(apply(chip.min.p <= 0.05, 1, sum),
+  main="Clusters per ChIP signal",
+  xlab="Number of clusters",
+  breaks=25, col="red")
+mtext("p <= 0.05", 3, cex=0.7)
+hist(apply(chip.min.p <= 1e-5, 1, sum),
+  main="Clusters per ChIP signal",
+  xlab="Number of clusters",
+  breaks=25, col="red")
+mtext("p <= 1e-5", 3, cex=0.7)
+
+hist(apply(motif.min.p <= 0.05, 2, sum),
+  main="Motifs per cluster",
+  xlab="Number of motifs",
+  breaks=50, col="blue")
+mtext("p <= 0.05", 3, cex=0.7)
+hist(apply(motif.min.p <= 1e-5, 2, sum),
+  main="Motifs per cluster",
+  xlab="Number of motifs",
+  breaks=50, col="blue")
+mtext("p <= 1e-5", 3, cex=0.7)
+hist(apply(chip.min.p <= 0.05, 2, sum),
+  main="ChIP signals per cluster",
+  xlab="Number of ChIP signals",
+  breaks=25, col="blue")
+mtext("p <= 0.05", 3, cex=0.7)
+hist(apply(chip.min.p <= 1e-5, 2, sum),
+  main="ChIP signals per cluster",
+  xlab="Number of ChIP signals",
+  breaks=25, col="blue")
+mtext("p <= 1e-5", 3, cex=0.7)
+
+dev.off()
+
---
+++ sort_paper/network/scaleFree.r
@@ -0,0 +1,44 @@
+# Tests if transcriptional network is scale-free, at least
+# superficially.
+
+if (TRUE) {
+load("git/sort_paper/tf/motif/hyperg/allResults/5kb/hier.300.Rdata")
+motif1.min.p = apply(enrich[,,"p.corr",,,], c(1,2), min)
+
+load("git/sort_paper/tf/motif/hyperg/allResults/5kb/hier.300.Rdata")
+hughes.min.p = apply(enrich[,,"p.corr",,,], c(1,2), min) 
+
+motif.min.p = rbind(hughes.min.p, motif1.min.p)
+
+load("git/sort_paper/tf/motif/hyperg/allResults/chip/hier.300.Rdata")
+chip.min.p = apply(enrich[,,"p.corr",,,], c(1,2), min) 
+}
+
+
+
+scale.free.regress = function(x, main) {
+
+  h = hist(apply(x, 1, sum), breaks=15, col="grey", main=main, xlab="degree")
+
+  ld = log(h$breaks[-1])
+  lc = log(h$counts+1)
+  plot(ld, lc, pch=20, xlab="log(degree distribution)", ylab="log(count+1)")
+  r = lm(lc ~ ld)
+  abline(r$coefficients, lwd=2, col="#aa000080")
+  mtext(paste("p = ", signif(summary(r)$coef[2,4], 2)), cex=0.7)
+
+  list(h = h, r = r)  
+}
+
+
+pdf("git/sort_paper/network/scaleFree.pdf", width=10, height=5)
+par(mfcol=c(2,4))
+for (min.p in 10^c(-1:-7)) {
+# cutoffs were 1e-3, 1e-3, 1e-4, and 1e-2, respectively
+  scale.free.regress(motif.min.p <= min.p, "Motifs")
+  scale.free.regress(t(motif.min.p <= min.p), "Clusters (motifs)")
+  scale.free.regress(chip.min.p <= min.p, "ChIP signals")
+  scale.free.regress(t(chip.min.p <= min.p), "Clusters (ChIP signals)")
+}
+dev.off()
+
