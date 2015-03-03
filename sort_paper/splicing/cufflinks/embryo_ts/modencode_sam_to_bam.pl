9b54811e076c8a2e091891f7007f2ae8a8f4c495
Josh Burdick <jburdick@mail.med.upenn.edu>
2015-03-03 14:46:24 -0500

    Now includes strand info for SL sequences; Cufflinks is still complaining about "spliced sequences without XS tag", but not (I think) as often (although I haven't quantified that.)

--- sort_paper/splicing/cufflinks/embryo_ts/modencode_sam_to_bam.pl
+++ sort_paper/splicing/cufflinks/embryo_ts/modencode_sam_to_bam.pl
@@ -3,16 +3,65 @@
 
 chdir("/media/jburdick/disk2/data/ftp/");
 
+# Converts a SAM file to a BAM file, with these alterations:
+# - any D in the CIGAR string is converted to an N (as
+#   these seem to correspond to introns, and that's what
+#   Cufflinks, at least, expects)
+# - any read with a Y1 tag gets a corresponding XS tag
+#   added, indicating the strand (based on alignment)
+# Args:
+#   sam_gz_in  - the input file
+#   bam_out - the output file
+sub modencode_sam_gz_to_bam {
+  my($sam_gz_in, $bam_out) = @_;
+
+  die if not $bam_out =~ /(^.+)\.bam$/;
+  my $bam_base = $1;
+
+  open IN, "gunzip -c $sam_gz_in |" || die;
+  open OUT, "| samtools view -bS - | samtools sort -m 1000000000 - $bam_base" || die;
+
+  while (<IN>) {
+
+    # pass through header unchanged
+    if (/^@/) {
+      print OUT;
+      next;
+    }
+
+    chomp;
+    my @a = split /\t/;
+
+    # change "D" to "N" in the CIGAR string
+    $a[5] =~ s/D/N/g;
+
+    print OUT (join "\t", @a);
+
+    # possibly add an XS tag
+    if (defined $a[12] && 
+        $a[12] =~ /Y1:Z:.*_\d+_\d+_([+-])_wb220/) {
+      print OUT "\tXS:A:$1\n";
+    }
+    else {
+      print OUT "\n";
+    }
+  }
+
+  close IN;
+  close OUT;
+
+  system "samtools index $bam_out";
+}
+
 foreach my $sam (<sam_gz_1/*.sam.gz>) {
   print "$sam\n";
 
-  my $bam_base = $sam;
-  $bam_base =~ s/^sam_gz_1/bam/;
-  $bam_base =~ s/\.sam\.gz$//;
-  my $bam = "$bam_base.bam";
+  my $bam = $sam;
+  $bam =~ s/^sam_gz_1/bam/;
+  $bam =~ s/\.sam\.gz$/\.bam/;
 
-  system "gunzip -c $sam | samtools view -bS - " .
-    "| samtools sort -m 200000000 - $bam_base";
+  modencode_sam_gz_to_bam($sam, $bam);
+
   system "samtools index $bam";
 }
 
