40acb02b4367acc1b32ddaab635246395d530a63
Josh Burdick <jburdick@mail.med.upenn.edu>
2015-03-04 14:01:22 -0500

    Faint glimmering of success: now merges channels.

--- python/fiji/mergeStacks.py
+++ python/fiji/mergeStacks.py
@@ -5,6 +5,9 @@
 
 import sys
 from ij import IJ, ImagePlus, ImageStack, CompositeImage
+
+from java.awt import Color
+
 # from ij import ImageStack
 
 if 0:
@@ -20,35 +23,44 @@ outputPath = "mergedImages";
 # read in images
 img1 = IJ.openImage(imgPath + "/" + "alexa005.tif")
 img2 = IJ.openImage(imgPath + "/" + "dapi005.tif")
-print "img1 = " + img1.toString() + "\n"
 
 
+ip1 = ImagePlus("ip1", img1.getImageStack())
+ip2 = ImagePlus("ip2", img2.getImageStack())
 
 # create stack containing the images
-stack1 = img1.getImageStack()
-stack2 = img2.getImageStack()
+stack1 = ip1.getImageStack()
+stack2 = ip2.getImageStack()
+
+
 
 stackNew = ImageStack(img1.width, img1.height)
 
 for i in xrange(1, 4):     # img1.getNSlices()+1):  
+
   # Extract the channels as FloatProcessor  
   i1 = stack1.getProcessor(i).toFloat(0, None)
-  i2 = stack2.getProcessor(i).toFloat(1, None)
+  i2 = stack2.getProcessor(i).toFloat(0, None)
 
   # Add both to the new stack
   stackNew.addSlice("ch1", i1)
   stackNew.addSlice("ch2", i2)
 
 imp2 = ImagePlus("merged", stackNew)
+# imp2.setColor(Color.RED)   runs, but does nothing
 imp2.setCalibration(img1.getCalibration().copy())  
-# IJ.run(imp2, "Green", "")
-print(imp2.toString())
+# IJ.run(imp2, "Red", "")
 
+print(imp2.toString())
 nChannels = 2              # two color channels  
 nSlices = stackNew.getSize() # the number of slices of the original stack  
-nFrames = 2                # only one time point   
-imp2.setDimensions(nChannels, nSlices, nFrames)  
+nFrames = 1                # only one time point   
+imp2.setDimensions(nChannels, 3, nFrames)
+print(imp2.toString())
+
 comp = CompositeImage(imp2, CompositeImage.COMPOSITE)  
 comp.show()  
 
+print comp.getImageStackSize()
+# print comp.getChannelLut(1)
 
