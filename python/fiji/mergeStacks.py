5ae2e54d636292b82a38e928d2d4db05053b7b11
Josh Burdick <jburdick@mail.med.upenn.edu>
2015-03-05 15:29:14 -0500

    This creates a merged image, which I can open in fiji, and select particular colors; however, it doesn't use the LUTs I give it.

--- python/fiji/mergeStacks.py
+++ python/fiji/mergeStacks.py
@@ -33,16 +33,15 @@ ip1 = ImagePlus("ip1", img1.getImageStack())
 ip2 = ImagePlus("ip2", img2.getImageStack())
 
 # XXX temp hack
-ip1.setCalibration(img1.getCalibration().copy())  
+# ip1.setCalibration(img1.getCalibration().copy())  
 
 # create stack containing the images
 stack1 = ip1.getImageStack()
 stack2 = ip2.getImageStack()
 
-
 stackNew = ImageStack(img1.width, img1.height)
 
-for i in xrange(1, img1.getNSlices()+1):  
+for i in xrange(1, 4):  # img1.getNSlices()+1):  
 
   # Extract the channels as FloatProcessor  
   i1 = stack1.getProcessor(i).toFloat(0, None)
@@ -53,37 +52,49 @@ for i in xrange(1, img1.getNSlices()+1):
   stackNew.addSlice("ch2 " + str(i), i2)
 
 imp2 = ImagePlus("merged", stackNew)
-imp2.setCalibration(img1.getCalibration().copy())  
+# imp2.setCalibration(img1.getCalibration().copy())  
 
 print(imp2.toString())
 nChannels = 2              # two color channels  
 nSlices = stackNew.getSize() # the number of slices of the original stack  
 nFrames = 1                # only one time point   
-imp2.setDimensions(nChannels, img1.getNSlices(), nFrames)
+imp2.setDimensions(nChannels, stackNew.getSize() / 2, nFrames)
 print(imp2.toString())
+print(imp2.getStatistics())
 
-comp = CompositeImage(imp2, CompositeImage.COMPOSITE)  
-# comp.setCalibration(img1.getCalibration().copy())
+comp = CompositeImage(imp2, CompositeImage.COLOR)  
+comp.setCalibration(img1.getCalibration().copy())
 
 from jarray import array
 lut1 = comp.createLutFromColor(Color.red)
+lut1.min = 500
+lut1.max=4000
 lut2 = comp.createLutFromColor(Color.blue)
-
+lut2.min=500
+lut2.max=4000
 
 luts = array([lut1, lut2], LUT)
 
 print(luts)
 
 print(comp.getLuts())
-
-# print comp.createLutFromColor(Color.blue)
+print(comp.getStatistics())
 
 comp.setLuts(luts)
-comp.resetDisplayRanges()
+# comp.resetDisplayRanges()
 
-# IJ.save(comp, "/home/jburdick/merged.tif")
+# XXX various attempts at saving the result
+# IJ.save(comp, "/home/jburdick/merged.png")
 fs = FileSaver(comp)
 fs.saveAsTiffStack("/home/jburdick/merged.tif")
 
 comp.show() 
+
+# IJ.runPlugIn("loci.plugins.LociExporter",
+#   "imageid=" + str(comp.getID()) + " " +
+#   "outfile=/home/jburdick/merged.tif " +
+#   "splitz=false splitc=false splitt=false saveroi=false " +
+#   "compression-type=Uncompressed")
+
+
 
