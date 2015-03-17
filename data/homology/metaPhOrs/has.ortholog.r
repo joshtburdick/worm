2c8e0ae689bfbe5f445fe8260f6d11b71bee303e
Josh Burdick <jburdick@mail.med.upenn.edu>
2015-03-17 17:10:11 -0400

    Seems to work for several species.

--- data/homology/metaPhOrs/has.ortholog.r
+++ data/homology/metaPhOrs/has.ortholog.r
@@ -2,16 +2,22 @@
 
 source("git/utils.r")
 
+# conversion table of gene names
 ce.metaphors.gene = read.tsv("git/data/homology/metaPhOrs/ceOrthologs.tsv")
-
 protid.to.gene = ce.metaphors.gene$gene.name
 names(protid.to.gene) = ce.metaphors.gene$protid
 
-
-ce.ortho = read.table(
-  "/home/jburdick/data/ftp/phylomedb.org/metaphors/release-201405/orthologs/6239.txt.gz",
+# genes orthologous to Ce
+ce.ortho = read.table(gzfile(
+  "/home/jburdick/data/ftp/phylomedb.org/metaphors/release-201405/orthologs/6239.txt.gz"),
   sep="\t", comment="", header=TRUE, as.is=TRUE)
 names(ce.ortho)[1] = "taxid1"
+
+# table for looking up species ID in Tree of Life
+species = read.table(
+  "~/data/ftp/phylomedb.org/metaphors/release-201405/species.txt.gz",
+  sep="\t", header=TRUE, quote="", comment="", as.is=TRUE)
+
 
 # Gets which genes have a Ce ortholog in another species.
 # Args:
@@ -31,12 +37,10 @@ get.ortho = function(taxid2) {
   s
 }
 
+has.ortholog = as.matrix(cbind("Pristionchus" = get.ortho(54126),
+  "fly" = get.ortho(7227),
+  "mouse" = get.ortho(10090),
+  "human" = get.ortho(9606)))
 
-r = get.ortho(7227)
-
-
-
-
-
-
+write.tsv(has.ortholog, gzfile("git/data/homology/metaPhOrs/has.ortholog.tsv.gz"))
 
