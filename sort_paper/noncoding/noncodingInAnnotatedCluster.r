8e6be7a9a60ddb4b561559cba22ac5dd61c335cf
Josh Burdick <jburdick@mail.med.upenn.edu>
2015-04-01 15:37:42 -0400

    Added a few stats.

--- sort_paper/noncoding/noncodingInAnnotatedCluster.r
+++ sort_paper/noncoding/noncodingInAnnotatedCluster.r
@@ -13,20 +13,24 @@ colnames(noncoding.cl)[1] = "non-coding gene"
 anatomy.annotated = read.tsv(
   "git/sort_paper/enrichment/clustersWithAnatomyAnnotation.tsv")
 
-noncoding.anatomy.annotated = merge(noncoding.cl, anatomy.annotated)
+a = merge(noncoding.cl, anatomy.annotated)
 
 # reads per million (for computing max. expression)
 rpm = read.tsv("git/cluster/readsPerMillion.tsv")
 rpm = rpm[ , !grepl("^(HS|RNAi)", colnames(rpm)) ]
 max.rpm = apply(rpm, 1, max)
 
-noncoding.anatomy.annotated$max.rpm =
-  max.rpm[ noncoding.anatomy.annotated$"non-coding gene" ]
-noncoding.anatomy.annotated = noncoding.anatomy.annotated[
-  order(noncoding.anatomy.annotated$max.rpm, decreasing=TRUE) , ]
-noncoding.anatomy.annotated = noncoding.anatomy.annotated[ ,
-  c("non-coding gene", "max.rpm", "cluster", "group", "p.corr", "group.name") ]
+a$max.rpm = max.rpm[ a$"non-coding gene" ]
+a = a[ order(a$max.rpm, decreasing=TRUE) , ]
+a = a[ , c("non-coding gene", "max.rpm", "cluster", "group", "p.corr", "group.name") ]
+
+# some stats on this
+cat(paste("num expressed anrRNAs with annotation =",
+  sum(a$max.rpm >= 1 & grepl("^anr-", a$"non-coding gene")), "\n"))
+cat(paste("num expressed lincRNAs with annotation =",
+  sum(a$max.rpm >= 1 & grepl("^linc-", a$"non-coding gene")), "\n"))
 
 write.tsv(noncoding.anatomy.annotated,
   "git/sort_paper/noncoding/noncodingInAnnotatedCluster.tsv")
+
 
