f772786c0a143c0417e1b6208ca0ecadcbafcc8d
Josh Burdick <jburdick@mail.med.upenn.edu>
2015-05-28 16:30:28 -0400

    Superficially seems to work.

---
+++ unmix/ept/practice/sd.r
@@ -0,0 +1,54 @@
+# A "scaled Dirichlet" ("SD") distribution.
+
+source("git/unmix/ept/gamma.r")
+source("git/unmix/ept/moment.r")
+
+
+# Converts from SD parameters to marginal mean and variance.
+sd.s2mv = function(a) {
+  mv = gamma.s2mv(a)
+  moments = mv2moment(mv)
+  s = apply(mv, 1, sum)
+  s.moments = c(mv2moment(cbind(s)))
+  moment2mv( moments / s.moments )
+}
+
+# Converts from marginal mean and variance to SD parameters.
+# XXX not currently working
+sd.mv2s = function(b) {
+  m = mv2moment(b)
+
+  a = sum( m["x2",] )
+  b = sum( m["x1",] ^ 2 )
+
+  # the first moments stay the same
+  m["x2",] = m["x2",] * ( (1-b) / (1-a) )
+
+  gamma.mv2s( moment2mv(m) )
+}
+
+
+# some simple tests. For concreteness, I'm scaling
+# the a's to sum to one. (even though it doesn't affect
+# the result of sd.s2mv())
+sd.test1 = function() {
+  a1 = rbind(a=c(1,1,1), b=c(1,1,1))
+  a1["b",] = a1["b",] * sum(gamma.s2mv(a1)["m",])
+  b1 = sd.s2mv(a1)
+
+  a2 = rbind(a=c(1,2,3), b=c(3,2,1))
+  a2["b",] = a2["b",] * sum(gamma.s2mv(a2)["m",])
+  b2 = sd.s2mv(a2)
+  list(a1, b1, a2, b2)
+}
+
+# More tests.
+sd.test2 = function(m, n) {
+  for(i in 1:n) {
+    a = rbind(a = rgamma(m,1,1), b = rgamma(m,1,1))
+    b = sd.s2mv(a)
+    b1 = sd.s2mv( sd.mv2s(b) )
+    cat("diff =", range(b1 - b), "\n")
+  }
+}
+
