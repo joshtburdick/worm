0d843d3f38832762ca390c62918b73a804224ff9
Josh Burdick <jburdick@mail.med.upenn.edu>
2015-06-24 15:22:37 -0400

    Added p-value, made fonts larger.

--- sort_paper/unmix/sortMatrix/spencerOverlapCompare.r
+++ sort_paper/unmix/sortMatrix/spencerOverlapCompare.r
@@ -53,14 +53,22 @@ spencer.overlap = overlap(spencer.on.off)
 cat(paste0("FACS mean overlap = ", mean(facs.overlap), "\n"))
 cat(paste0("Spencer mean overlap = ", mean(spencer.overlap), "\n"))
 
-pdf("git/sort_paper/unmix/sortMatrix/spencerOverlapCompare.pdf")
+wt = wilcox.test(facs.overlap, spencer.overlap, alternative="greater")
+
+
+pdf("git/sort_paper/unmix/sortMatrix/spencerOverlapCompare.pdf",
+  width=4.5, height=6)
 par(mfrow=c(2,1))
 b = seq(0, 110, 2)
 hist(facs.overlap, breaks=b, col="#ff0000a0",
   main="Pairs of markers used for flow sorting",
   xlab="Number of cells in overlap")
 hist(spencer.overlap, breaks=b, col="#ffff00a0",
-  main="Pairs of embryonic tissues from (Spencer et al. 2011)",
+  main="Pairs of embryonic tissues\nfrom (Spencer et al. 2011)",
   xlab="Number of cells in overlap")
+# XXX
+x = expression("Wilcoxon p" * 1)
+x[[1]][[3]] = format.p(wt$p.value)[[1]]
+mtext(x, line=-1, cex=0.9)
 dev.off()
 
