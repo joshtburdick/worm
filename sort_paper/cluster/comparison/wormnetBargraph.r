02b114abf84c3297dd841a0740c15d325b314a7f
Josh Burdick <jburdick@keyfitz.org>
2015-07-13 23:49:41 -0400

    Cosmetic improvements, e.g. boldface.

--- sort_paper/cluster/comparison/wormnetBargraph.r
+++ sort_paper/cluster/comparison/wormnetBargraph.r
@@ -3,17 +3,19 @@
 source("git/utils.r")
 
 w = read.tsv("git/sort_paper/cluster/comparison/wormnet.tsv")
-w = w[ order(w$times.more.than.random) , ]
-w["combined","description"] = "combined"
+w = w[ order(
+  rownames(w)=="combined",
+  w$times.more.than.random) , ]
+w["combined","description"] = expression(bold("All"))
 
 pdf("git/sort_paper/cluster/comparison/wormnetBargraph.pdf",
-  width=8, height=11)
-par(mar=c(30,4,4,1)+0.1)
+  width=11, height=6)
+par(mar=c(5,29,0.7,0.7)+0.1)
 
-barplot(w$times.more.than.random,
-  names.arg = w$description, las=2,
-    col=ifelse(w$description=="combined", "#777777", "grey"),
-  ylab="Fold enrichment over random")
+barplot(w$times.more.than.random, horiz=TRUE,
+  names.arg = w$description, las=1,
+    col=ifelse(rownames(w)=="combined", "#777777", "grey"),
+  xlab="Fold enrichment over random")
 
 dev.off()
 
