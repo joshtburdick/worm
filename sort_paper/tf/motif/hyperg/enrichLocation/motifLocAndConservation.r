4454b575128dd199119be2e9db6bf3e7816f830d
Josh Burdick <jburdick@mail.med.upenn.edu>
2015-07-15 19:43:27 -0400

    Initial version.

--- sort_paper/tf/motif/hyperg/enrichLocation/motifLocAndConservation.r
+++ sort_paper/tf/motif/hyperg/enrichLocation/motifLocAndConservation.r
@@ -19,34 +19,82 @@ colnames(a)[5] = "lp.dist"
 a[,7] = -log10(a$cons.p.corr)
 colnames(a)[7] = "lp.cons"
 
-png("git/sort_paper/tf/motif/hyperg/enrichLocation/motifLocAndConservation.png",
-  width=1200, height=600)
-par(mfrow=c(2,4))
+# Plots each of these four categories separately, compared
+# to motif enrichment.
+plot.1 = function() {
+  png("git/sort_paper/tf/motif/hyperg/enrichLocation/motifLocAndConservation.png",
+    width=1200, height=600)
+  par(mfrow=c(2,4))
 
-hist(a[a$dist.closer, "lp.dist"], main="Closer to TSS",
-  xlab="Location -log10(p)", breaks=100, col="grey")
-hist(a[!a$dist.closer, "lp.dist"], main="Further from TSS",
-  xlab="Location -log10(p)", breaks=100, col="grey")
-hist(a[a$cons.higher, "lp.cons"], main="More conserved",
-  xlab="Conservation -log10(p)", breaks=100, col="grey", xlim=c(0,40))
-hist(a[!a$cons.higher, "lp.cons"], main="Less conserved",
-  xlab="Conservation -log10(p)", breaks=100, col="grey", xlim=c(0,40))
+  hist(a[a$dist.closer, "lp.dist"], main="Closer to TSS",
+    xlab="Location -log10(p)", breaks=100, col="grey")
+  hist(a[!a$dist.closer, "lp.dist"], main="Further from TSS",
+    xlab="Location -log10(p)", breaks=100, col="grey")
+  hist(a[a$cons.higher, "lp.cons"], main="More conserved",
+    xlab="Conservation -log10(p)", breaks=100, col="grey", xlim=c(0,40))
+  hist(a[!a$cons.higher, "lp.cons"], main="Less conserved",
+    xlab="Conservation -log10(p)", breaks=100, col="grey", xlim=c(0,40))
 
-i = a$dist.closer
-plot(a[i,"lp.dist"], a[i,"lp.corr"], pch=20, col="#00000040",
-  xlab="Location -log10(p)", ylab="Enrichment -log10(p)",
-  main="Closer to TSS", ylim=c(0,70))
-plot(a[!i,"lp.dist"], a[!i,"lp.corr"], pch=20, col="#00000040",
-  xlab="Location -log10(p)", ylab="Enrichment -log10(p)",
-  main="Further from TSS", ylim=c(0,70))
+  i = a$dist.closer
+  plot(a[i,"lp.dist"], a[i,"lp.corr"], pch=20, col="#00000040",
+    xlab="Location -log10(p)", ylab="Enrichment -log10(p)",
+    main="Closer to TSS", ylim=c(0,70))
+  plot(a[!i,"lp.dist"], a[!i,"lp.corr"], pch=20, col="#00000040",
+    xlab="Location -log10(p)", ylab="Enrichment -log10(p)",
+    main="Further from TSS", ylim=c(0,70))
 
-i = a$cons.higher
-plot(a[i,"lp.cons"], a[i,"lp.corr"], pch=20, col="#00000040",
-  xlab="Conservation -log10(p)", ylab="Enrichment -log10(p)",
-  main="More conserved", xlim=c(0,40), ylim=c(0,70))
-plot(a[!i,"lp.cons"], a[!i,"lp.corr"], pch=20, col="#00000040",
-  xlab="Conservation -log10(p)", ylab="Enrichment -log10(p)",
-  main="Less conserved", xlim=c(0,40), ylim=c(0,70))
+  i = a$cons.higher
+  plot(a[i,"lp.cons"], a[i,"lp.corr"], pch=20, col="#00000040",
+    xlab="Conservation -log10(p)", ylab="Enrichment -log10(p)",
+    main="More conserved", xlim=c(0,40), ylim=c(0,70))
+  plot(a[!i,"lp.cons"], a[!i,"lp.corr"], pch=20, col="#00000040",
+    xlab="Conservation -log10(p)", ylab="Enrichment -log10(p)",
+    main="Less conserved", xlim=c(0,40), ylim=c(0,70))
 
+  dev.off()
+}
+
+# Plots each of these together, colored by motif enrichment.
+plot.2 = function(coloring) {
+  a = a[ !is.na(a$lp.cons) , ]
+  a = a[ is.finite(a$lp.dist) , ]
+  a$lp.corr.1 = pmin(a$lp.corr / 10, 1)
+  a$lp.dist.1 = ifelse(a$dist.closer, a$lp.dist, -a$lp.dist)
+  a$lp.cons.1 = ifelse(a$cons.higher, a$lp.cons, -a$lp.cons)
+# browser()
+  plot(a$lp.dist.1, a$lp.cons.1, xlim=c(-305, 305),
+    ylim=c(-40,40),
+    xlab="Distance to TSS", ylab="Conservation",
+    xaxt="n", yaxt="n",
+    pch=183, font=5, cex=0.7, col=coloring(a$lp.corr.1))
+  abline(h=0, col="#00000080")
+  abline(v=0, col="#00000080")
+  a = 100*c(-3:3)
+  axis(1, at=a, labels=abs(a), cex=0.8)
+  a = 20*c(-2:2)
+  axis(2, at=a, labels=abs(a), cex=0.8)
+
+  legend("topleft", title="Enrichment significance", pch=20,
+#    legend=c(4,6,8,10),
+    legend=c(expression(10^{-4}),
+      expression(10^{-6}),
+      expression(10^{-8}),
+      expression("" > 10^{-10})),
+      col=coloring(c(2:5)/5), cex=0.7)
+    mtext("Further", side=1, adj=0, line=2)
+    mtext("Closer", side=1, adj=1, line=2)
+    mtext("Lower", side=2, adj=0, line=2)
+    mtext("Higher", side=2, adj=1, line=2)
+}
+
+pdf("git/sort_paper/tf/motif/hyperg/enrichLocation/motifLocAndConservation_BW.pdf",
+  width=10, height=7.5)
+plot.2(function(x) hsv(0, 0, 1-x, 0.8))
 dev.off()
+
+pdf("git/sort_paper/tf/motif/hyperg/enrichLocation/motifLocAndConservation_C.pdf",
+  width=10, height=7.5)
+plot.2(function(x) hsv(1-x, 1, 1, 0.8))
+dev.off()
+
 
