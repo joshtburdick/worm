082aababc8711f14c519b1c3762b25352a48fa40
Josh Burdick <jburdick@mail.med.upenn.edu>
2015-08-28 17:42:51 -0400

    Colorized.

--- sort_paper/FACS/spencerComparison/geneEnrichComparison.r
+++ sort_paper/FACS/spencerComparison/geneEnrichComparison.r
@@ -112,43 +112,46 @@ overlap.matrix = function(a, b) {
   r
 }
 
+# a = overlap.matrix(bool.matrix.to.list(m.unnormalized > 0),
+#   bool.matrix.to.list(spencer.m))[,13:1]
+a = tissue.enrich(facs.m, spencer.m)[,13:1]
+b = t(gene.set.avg.enrich(spencer.cluster, r[ , rownames(facs.m) ]))[,13:1]
+
+
+# convert to a table
+# XXX slow but simple way of doing this
+gec = NULL
+for(facs in rownames(a))
+  for(tissue in colnames(a)) {
+    gec = rbind(gec, data.frame(tissue = tissue,
+      facs = facs,
+      tissue.enrich = a[facs,tissue],
+      gene.enrich = b[facs,tissue],
+      stringsAsFactors=FALSE))
+  }
+
 pdf("git/sort_paper/FACS/spencerComparison/geneEnrichComparison.pdf",
   width=5.5, height=5.5)
 #  width=11, height=4)
 # par(mfrow=c(1,3))
 # par(mar=c(9,9.5,3,1)+0.1)
 
-# a = overlap.matrix(bool.matrix.to.list(m.unnormalized > 0),
-#   bool.matrix.to.list(spencer.m))[,13:1]
-a = tissue.enrich(facs.m, spencer.m)[,13:1]
-b = t(gene.set.avg.enrich(spencer.cluster, r[ , rownames(facs.m) ]))[,13:1]
-
-# XXX omitting these for now
-if (FALSE) {
-  plot.heatmap(a, main="Enrichment of tissues",
-    cluster=FALSE, z.lim=c(-6, 6),
-    col=c(rgb(0,128:0/128,0), rgb(0:128/128,0,0)))
-  plot.heatmap(b, main="Enrichment of tissue-specific genes",
-    cluster=FALSE, z.lim=c(-2, 2),
-    col=c(rgb(0,128:0/128,0), rgb(0:128/128,0,0)))
-}
+tissues = sort(unique(gec$tissue))
+tissue.colors = hsv(c(1:length(tissues))/length(tissues), 0.8, 0.8)
+names(tissue.colors) = tissues
 
 par(mar=c(5,5,1,1)+0.1)
-plot(as.vector(a), as.vector(b),
+plot(gec$tissue.enrich, gec$gene.enrich,
   main="",
   xlab=expression("Enrichment of tissue " * (log[2] * " scale")),
   ylab=expression("Enrichment of tissue-specific genes " *
-    (log[2] * " scale")), 
-  pch=20, col="#00000080")
+    (log[2] * " scale")),
+  col = tissue.colors[ gec$tissue ],
+  pch=20)
 
-a1 = as.vector(a)
-b1 = as.vector(b)
-s = cor.test(a1, b1)
-
+s = cor.test(gec$tissue.enrich, gec$gene.enrich)
+# compute p more exactly, using t distribution
 p = pt(s$statistic, df=s$parameter, lower.tail=FALSE)
-
-# legend("bottomright",
-#   legend=paste("r =", round(s$estimate, 2)), bty="n")
 
 legend("bottomright",
   legend=expr.format(expression("r = " * r * ", " * italic("p") * p),
@@ -157,5 +160,12 @@ legend("bottomright",
 # add regression line
 m = lm(b1 ~ a1)
 abline(m, lwd=2, col="#00000040")
+
 dev.off()
+
+colnames(gec) = c("Tissue", "FACS sample",
+  "Tissue enrichment", "Gene enrichment")
+write.tsv(gec, file=
+  "git/sort_paper/FACS/spencerComparison/geneEnrichComparison.tsv")
+
 
