d0825fc191e39c227230cca4066791e712789339
Josh Burdick <jburdick@keyfitz.org>
2015-12-10 13:55:09 -0500

    Mostly added a description. Also temporarily disabled the unblurification.

--- tex/paper/sort_paper/make.sh
+++ tex/paper/sort_paper/make.sh
@@ -6,6 +6,6 @@ unblurify < figures.pdf > figures1.pdf
 mv figures1.pdf figures.pdf
 
 tex2pdf supplementalFigures.tex
-unblurify < supplementalFigures.pdf > supplementalFigures1.pdf
-mv supplementalFigures1.pdf supplementalFigures.pdf
+# unblurify < supplementalFigures.pdf > supplementalFigures1.pdf
+# mv supplementalFigures1.pdf supplementalFigures.pdf
 
